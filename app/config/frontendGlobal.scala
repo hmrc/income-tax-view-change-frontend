953442f78b63f990af50ca0a3c885b7a678ecfa3
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-05-08 11:50:05 +0100

    [BTAT_42] add contact frontend and DI Module

--- app/config/frontendGlobal.scala
+++ app/config/frontendGlobal.scala
@@ -14,24 +14,20 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.incometaxviewchangefrontend
-
-import java.io.File
-
+package config
 import com.typesafe.config.Config
 import net.ceedubs.ficus.Ficus._
-import play.api.Mode._
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
 import play.api.mvc.Request
 import play.api.{Application, Configuration, Play}
 import play.twirl.api.Html
 import uk.gov.hmrc.crypto.ApplicationCrypto
 import uk.gov.hmrc.play.audit.filters.FrontendAuditFilter
 import uk.gov.hmrc.play.config.{AppName, ControllerConfig, RunMode}
+import uk.gov.hmrc.play.filters.MicroserviceFilterSupport
 import uk.gov.hmrc.play.frontend.bootstrap.DefaultFrontendGlobal
 import uk.gov.hmrc.play.http.logging.filters.FrontendLoggingFilter
-import uk.gov.hmrc.play.filters.MicroserviceFilterSupport
-import play.api.i18n.Messages.Implicits._
-import play.api.Play.current
 
 
 object FrontendGlobal
@@ -47,7 +43,7 @@ object FrontendGlobal
   }
 
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit rh: Request[_]): Html =
-    uk.gov.hmrc.incometaxviewchangefrontend.views.html.error_template(pageTitle, heading, message)
+    views.html.error_template(pageTitle, heading, message)(implicitly, implicitly, new FrontendAppConfig(Play.current.configuration))
 
   override def microserviceMetricsConfig(implicit app: Application): Option[Configuration] = app.configuration.getConfig(s"microservice.metrics")
 }
