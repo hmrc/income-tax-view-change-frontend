c4091b8a87230f8af93b03b0bf3cb382c2024e01
matthew.page <matthew.page@digital.hmrc.gov.uk>
2017-05-29 20:34:38 +0100

    [BTAT-118] - Created EstimatedTaxLiability service to call connector and translate JSON to response model

---
+++ app/services/EstimatedTaxLiabilityService.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import javax.inject.{Inject, Singleton}
+
+import connectors.EstimatedTaxLiabilityConnector
+import models._
+import play.api.Logger
+import uk.gov.hmrc.play.http.HeaderCarrier
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+@Singleton
+class EstimatedTaxLiabilityService @Inject()(val estimatedTaxLiabilityConnector: EstimatedTaxLiabilityConnector) {
+
+  def getEstimatedTaxLiability(mtditid: String)(implicit headerCarrier: HeaderCarrier): Future[EstimatedTaxLiabilityResponseModel] = {
+    Logger.debug("[EstimatedTaxLiabilityService][getEstimateTaxLiability] - Requesting Estimate Liability from Backend via Connector")
+    estimatedTaxLiabilityConnector.getEstimatedTaxLiability(mtditid).map[EstimatedTaxLiabilityResponseModel] {
+      case success: SuccessResponse =>
+        Logger.debug(s"[EstimatedTaxLiabilityService][getEstimateTaxLiability] - Retrieved Estimated Tax Liability: ${success.json}")
+        success.json.as[EstimatedTaxLiability]
+      case error: ErrorResponse =>
+        Logger.debug(s"[EstimatedTaxLiabilityService][getEstimateTaxLiability] - Error Response Status: ${error.status}, Message: ${error.message}")
+        EstimatedTaxLiabilityError(error.status, error.message)
+    }
+  }
+}
