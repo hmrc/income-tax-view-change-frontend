7022ebc6d9e807944f9b136869b358f297d44e1c
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-06-07 17:56:27 +0100

    [BTAT-164] update ObligationsService unit tests

---
+++ test/utils/DateFormatterSpec.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import java.time.LocalDate
+
+class DateFormatterSpec extends TestSupport with ImplicitDateFormatter {
+
+  "The implicit date formatter" should {
+
+    "format string dates" in {
+     val date = LocalDate.of(2017, 4, 1)
+
+      date shouldBe localDate("2017-04-01")
+    }
+
+    "format months with single digit values" in {
+      val date = LocalDate.of(2017, 6, 30)
+
+      date shouldBe localDate("2017-6-30")
+    }
+
+    "format days with single digit values" in {
+      val date = LocalDate.of(2017, 6, 1)
+
+      date shouldBe localDate("2017-6-1")
+    }
+  }
+}
