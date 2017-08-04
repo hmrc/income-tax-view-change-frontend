ee6e9e63e4bd4f52b1c08ccd4e8ae4ce901d203c
samuel-raspin <samuel.raspin@digital.hmrc.gov.uk>
2017-08-04 09:27:19 +0100

    [partial-links-fix] - corrected tests

--- test/views/helpers/BtaPartialHelperSpec.scala
+++ test/views/helpers/BtaPartialHelperSpec.scala
@@ -17,6 +17,7 @@
 package views.helpers
 
 import assets.TestConstants.Estimates._
+import config.FrontendAppConfig
 import models._
 import play.twirl.api.Html
 import utils.TestSupport
@@ -26,37 +27,39 @@ import play.api.i18n.Messages.Implicits._
 
 class BtaPartialHelperSpec extends TestSupport {
 
+  lazy val mockAppConfig = fakeApplication.injector.instanceOf[FrontendAppConfig]
+
   "The BtaPartialHelper's whichStatus method" should {
     "return Html corresponding to the ObligationStatus" when {
       val openHtml = Html(
         """
            |<p id="report-due">Your next report is due by 31 October 2017</p>
-           |<a id="obligations-link" href=/report-quarterly/income-and-expenses/view/obligations>View deadlines</a>
+           |<a id="obligations-link" href=http://localhost:9081/report-quarterly/income-and-expenses/view/obligations>View deadlines</a>
          """.stripMargin.trim
       )
 
       val overdueHtml = Html(
         """
            |<p id="report-due">You have an overdue report</p>
-           |<a id="obligations-link" href=/report-quarterly/income-and-expenses/view/obligations>View deadlines</a>
+           |<a id="obligations-link" href=http://localhost:9081/report-quarterly/income-and-expenses/view/obligations>View deadlines</a>
          """.stripMargin.trim
       )
 
       val receivedHtml = Html(
         """
           |<p id="report-due">Your latest report has been received</p>
-          |<a id="obligations-link" href=/report-quarterly/income-and-expenses/view/obligations>View deadlines</a>
+          |<a id="obligations-link" href=http://localhost:9081/report-quarterly/income-and-expenses/view/obligations>View deadlines</a>
         """.stripMargin.trim
       )
 
       "passed 'Open' the Open Html is returned" in {
-        BtaPartialHelper.whichStatus(openObligation) shouldBe openHtml
+        BtaPartialHelper.whichStatus(openObligation)(applicationMessages, mockAppConfig) shouldBe openHtml
       }
       "passed 'Overdue' the Overdue Html is returned" in {
-        BtaPartialHelper.whichStatus(overdueObligation) shouldBe overdueHtml
+        BtaPartialHelper.whichStatus(overdueObligation)(applicationMessages, mockAppConfig) shouldBe overdueHtml
       }
       "passed 'Received' the Received Html is returned" in {
-        BtaPartialHelper.whichStatus(receivedObligation) shouldBe receivedHtml
+        BtaPartialHelper.whichStatus(receivedObligation)(applicationMessages, mockAppConfig) shouldBe receivedHtml
       }
     }
   }
@@ -67,12 +70,12 @@ class BtaPartialHelperSpec extends TestSupport {
       val successHtml: Html = Html(
         """
           |<p id="current-estimate-2018">Your estimated tax amount is &pound;543.21</p>
-          |<a id="estimates-link-2018" href=/report-quarterly/income-and-expenses/view/estimated-tax-liability/2018>View details</a>
+          |<a id="estimates-link-2018" href=http://localhost:9081/report-quarterly/income-and-expenses/view/estimated-tax-liability/2018>View details</a>
         """.stripMargin.trim
       )
 
       "passed a number the Html is returned" in {
-        BtaPartialHelper.showLastEstimate(List(lastTaxCalcSuccessWithYear)) shouldBe List(successHtml)
+        BtaPartialHelper.showLastEstimate(List(lastTaxCalcSuccessWithYear))(applicationMessages, mockAppConfig) shouldBe List(successHtml)
       }
     }
 
@@ -81,18 +84,18 @@ class BtaPartialHelperSpec extends TestSupport {
       val successHtml1: Html = Html(
         """
           |<p id="current-estimate-2018">Your estimated tax amount is &pound;543.21</p>
-          |<a id="estimates-link-2018" href=/report-quarterly/income-and-expenses/view/estimated-tax-liability/2018>View details</a>
+          |<a id="estimates-link-2018" href=http://localhost:9081/report-quarterly/income-and-expenses/view/estimated-tax-liability/2018>View details</a>
         """.stripMargin.trim
       )
       val successHtml2: Html = Html(
         """
           |<p id="current-estimate-2019">Your estimated tax amount is &pound;6,543.21</p>
-          |<a id="estimates-link-2019" href=/report-quarterly/income-and-expenses/view/estimated-tax-liability/2019>View details</a>
+          |<a id="estimates-link-2019" href=http://localhost:9081/report-quarterly/income-and-expenses/view/estimated-tax-liability/2019>View details</a>
         """.stripMargin.trim
       )
 
       "return Html corresponding to the estimated tax for misaligned tax years" in {
-        BtaPartialHelper.showLastEstimate(List(lastTaxCalcSuccessWithYear, LastTaxCalculationWithYear(LastTaxCalculation("CALCID","2018-07-06T12:34:56.789Z", 6543.21), 2019))) shouldBe List(successHtml1,successHtml2)
+        BtaPartialHelper.showLastEstimate(List(lastTaxCalcSuccessWithYear, LastTaxCalculationWithYear(LastTaxCalculation("CALCID","2018-07-06T12:34:56.789Z", 6543.21), 2019)))(applicationMessages, mockAppConfig) shouldBe List(successHtml1,successHtml2)
       }
     }
 
@@ -101,7 +104,7 @@ class BtaPartialHelperSpec extends TestSupport {
       val successHtml: Html = Html(
         """
           |<p id="current-estimate-2018">Your estimated tax amount is &pound;543.21</p>
-          |<a id="estimates-link-2018" href=/report-quarterly/income-and-expenses/view/estimated-tax-liability/2018>View details</a>
+          |<a id="estimates-link-2018" href=http://localhost:9081/report-quarterly/income-and-expenses/view/estimated-tax-liability/2018>View details</a>
         """.stripMargin.trim
       )
 
@@ -112,7 +115,7 @@ class BtaPartialHelperSpec extends TestSupport {
       )
 
       "" in {
-        BtaPartialHelper.showLastEstimate(List(lastTaxCalcSuccessWithYear, LastTaxCalculationWithYear(NoLastTaxCalculation, 2019))) shouldBe List(successHtml,noCalcHtml)
+        BtaPartialHelper.showLastEstimate(List(lastTaxCalcSuccessWithYear, LastTaxCalculationWithYear(NoLastTaxCalculation, 2019)))(applicationMessages, mockAppConfig) shouldBe List(successHtml,noCalcHtml)
       }
     }
   }
