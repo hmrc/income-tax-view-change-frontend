7212b3de386d50f9475691caabd36bb79e44ef21
matthew.page <matthew.page@digital.hmrc.gov.uk>
2017-08-10 15:59:36 +0100

    [BTAT-437] Added ExitSurveyModel and Form (with validation)

---
+++ test/forms/ExitSurveyFormSpec.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import assets.{Messages => messageLookup}
+import models.ExitSurveyModel
+import play.api.data.Form
+import play.api.i18n.MessagesApi
+import utils.TestSupport
+
+class ExitSurveyFormSpec extends TestSupport {
+
+  object TestExitSurveyForm extends ExitSurveyForm()(fakeApplication.injector.instanceOf[MessagesApi])
+
+  val improvementsMaxLength = 1200
+
+  "The ExitSurveyForm" should {
+
+    "have no errors" when {
+
+      "the length of 'improvements' is equal to the maxLength" should {
+
+        lazy val formInput: Map[String, String] = Map("improvements" -> "a" * improvementsMaxLength)
+        lazy val result = TestExitSurveyForm.exitSurveyForm.bind(formInput)
+
+        "have no errors" in {
+          result.hasErrors shouldBe false
+        }
+      }
+
+      "the length of 'improvements' is less than the maxLength" should {
+
+        lazy val formInput: Map[String, String] = Map("improvements" -> "a" * (improvementsMaxLength - 1))
+        lazy val result = TestExitSurveyForm.exitSurveyForm.bind(formInput)
+
+        "have no errors" in {
+          result.hasErrors shouldBe false
+        }
+      }
+
+      "valid numeric is supplied for 'satisfaction'" should {
+
+        lazy val formInput: Map[String, String] = Map("satisfaction" -> "1")
+        lazy val result = TestExitSurveyForm.exitSurveyForm.bind(formInput)
+
+        "have no errors" in {
+          result.hasErrors shouldBe false
+        }
+
+        "can fold a valid form into a ExitSurveyModel" in {
+          result.fold(
+            _ => false shouldBe true,
+            success => success.satisfaction shouldBe Some(1)
+          )
+        }
+
+        "should take a ExitSurveyModel and unapply to form" in {
+          val model = ExitSurveyModel(Some(3), Some("Awesome Site"))
+          TestExitSurveyForm.exitSurveyForm.fill(model).fold(
+            _ => false shouldBe true,
+            success => success shouldBe model
+          )
+        }
+      }
+    }
+
+    "have errors" when {
+
+      "the max length of 'improvements' is exceeded" should {
+
+        lazy val formInput: Map[String, String] = Map("improvements" -> "a" * (improvementsMaxLength + 1))
+        lazy val result = TestExitSurveyForm.exitSurveyForm.bind(formInput)
+
+        "have an error" in {
+          result.hasErrors shouldBe true
+        }
+
+        s"has the error message '${messageLookup.ExitSurvey.maxImprovementsError}'" in {
+          result.errors.head.message shouldBe messageLookup.ExitSurvey.maxImprovementsError
+        }
+      }
+
+
+      "a non-numeric value is supplied for 'satisfaction'" should {
+
+        lazy val formInput: Map[String, String] = Map("satisfaction" -> "a")
+        lazy val result = TestExitSurveyForm.exitSurveyForm.bind(formInput)
+
+        "have an error" in {
+          result.hasErrors shouldBe true
+        }
+      }
+    }
+  }
+}
---
+++ test/forms/constraints/ValidationSpec.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.constraints
+
+import assets.{Messages => messageLookup}
+import play.api.data.validation.{Invalid, Valid}
+import utils.TestSupport
+
+class ValidationSpec extends Validation with TestSupport {
+
+  val maxLength = 2
+  val errMsg = "Too Long"
+
+  "The Validation.optMaxLength method" when {
+
+    "supplied with a some string which exceeds the max length" should {
+      "return invalid with the correct message" in {
+        optMaxLength(maxLength, errMsg)(Some("abc")) shouldBe Invalid(errMsg)
+      }
+    }
+
+    "supplied with a some string which equals the max length" should {
+      "return valid" in {
+        optMaxLength(maxLength, errMsg)(Some("ab")) shouldBe Valid
+      }
+    }
+
+    "supplied with a some string which is less than the max length" should {
+      "return valid" in {
+        optMaxLength(maxLength, errMsg)(Some("a")) shouldBe Valid
+      }
+    }
+
+    "supplied with no value" should {
+      "return valid" in {
+        optMaxLength(maxLength, errMsg)(None) shouldBe Valid
+      }
+    }
+  }
+}
