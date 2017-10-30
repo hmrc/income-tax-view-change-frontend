eb29e33178ad5ac3167897588bffe5a8fd556ce3
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-10-30 16:11:05 +0000

    [BTAT-1009] add custom error handler

---
+++ app/config/ItvcErrorHandler.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import javax.inject.Inject
+
+import play.api.i18n.MessagesApi
+import play.api.mvc.{Request, Result}
+import play.api.mvc.Results.InternalServerError
+import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.play.bootstrap.http.FrontendErrorHandler
+import views.html.templates.error_template
+
+class ItvcErrorHandler @Inject()(val messagesApi: MessagesApi,
+                                 implicit val config: AppConfig) extends FrontendErrorHandler {
+  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: Request[_]): HtmlFormat.Appendable =
+    error_template(pageTitle, heading, message)
+
+  def showInternalServerError(implicit request: Request[_]): Result = InternalServerError(internalServerErrorTemplate)
+
+}
