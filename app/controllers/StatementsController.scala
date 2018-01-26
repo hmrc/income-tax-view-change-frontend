22077892d0c051dcc5f9b05619bfe8043e927c9f
samuel-raspin <samuel.raspin@digital.hmrc.gov.uk>
2018-01-26 15:30:09 +0000

    [BTAT-1580] - WIP: statements

---
+++ app/controllers/StatementsController.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import javax.inject.{Inject, Singleton}
+
+import auth.MtdItUser
+import config.FrontendAppConfig
+import controllers.predicates.{AuthenticationPredicate, IncomeSourceDetailsPredicate, NinoPredicate, SessionTimeoutPredicate}
+import models.{FinancialTransactionsErrorModel, FinancialTransactionsModel, TransactionModelWithYear}
+import play.api.i18n.MessagesApi
+import play.api.mvc.{Action, AnyContent}
+import services.FinancialTransactionsService
+import utils.ImplicitDateFormatter._
+import uk.gov.hmrc.http.HeaderCarrier
+
+@Singleton
+class StatementsController @Inject()(implicit val config: FrontendAppConfig,
+                                     implicit val messagesApi: MessagesApi,
+                                     val checkSessionTimeout: SessionTimeoutPredicate,
+                                     val authenticate: AuthenticationPredicate,
+                                     val retrieveNino: NinoPredicate,
+                                     val retrieveIncomeSources: IncomeSourceDetailsPredicate,
+                                     val financialTransactionsService: FinancialTransactionsService
+                                    ) extends BaseController {
+
+  val getStatements: Action[AnyContent] = (checkSessionTimeout andThen authenticate andThen retrieveNino andThen retrieveIncomeSources).async {
+    implicit user =>
+//      auditReportDeadlines(user)
+      for {
+        financialTransactionsResponse <- financialTransactionsService.getFinancialTransactions(user.nino)
+      } yield financialTransactionsResponse match {
+        case model: FinancialTransactionsModel => Ok(views.html.statements(withYears(model)))
+        case _: FinancialTransactionsErrorModel => InternalServerError
+      }
+
+  }
+
+  def withYears(model: FinancialTransactionsModel): Seq[TransactionModelWithYear] = {
+    model.financialTransactions.map { ft =>
+      if(ft.taxPeriodTo.isDefined){
+        ft.taxPeriodTo.get match {
+          case endDate if endDate isBefore s"${endDate.getYear}-4-6" => TransactionModelWithYear(ft, endDate.getYear)
+          case endDate => TransactionModelWithYear(ft, endDate.getYear + 1)
+        }
+      } else TransactionModelWithYear(ft, -1)
+    }
+  }
+
+//  private def auditReportDeadlines[A](user: MtdItUser[A])(implicit hc: HeaderCarrier): Unit =
+//    auditingService.audit(StatementsAuditModel(user), controllers.routes.StatementsController.getStatements().url)
+
+}
