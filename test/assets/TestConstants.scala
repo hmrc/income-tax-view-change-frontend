74e16d80c5be519828a21e42e53c17f800407093
matthew.page <matthew.page@digital.hmrc.gov.uk>
2018-02-12 15:25:41 +0000

    [BTAT-1710] Remove unecessary test constant

--- test/assets/TestConstants.scala
+++ test/assets/TestConstants.scala
@@ -186,7 +186,6 @@ object TestConstants extends ImplicitDateFormatter {
     val obligationsEOPSDataSuccessJson = Json.parse(obligationsEOPSDataSuccessString)
 
     val obligationsDataErrorModel = ReportDeadlinesErrorModel(testErrorStatus, testErrorMessage)
-    val eopsAndQuarterlyObligationsError = ReportDeadlinesErrorModel(Status.INTERNAL_SERVER_ERROR, "Quarterly and EOPS Deadlines both returned errors")
     val obligationsDataErrorString =
       s"""
          |{
