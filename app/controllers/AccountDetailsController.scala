f37cc3e1c24cca8d1ce90a9f879f6b43bc231bff
samuel-raspin <samuel.raspin@digital.hmrc.gov.uk>
2018-02-19 16:13:52 +0000

    [BTAT-1831] - WIP: AccountDetailsController

---
+++ app/controllers/AccountDetailsController.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import javax.inject.{Inject, Singleton}
+
+import config.FrontendAppConfig
+import controllers.predicates.{AuthenticationPredicate, IncomeSourceDetailsPredicate, NinoPredicate, SessionTimeoutPredicate}
+import models.IncomeSourcesModel
+import play.api.i18n.MessagesApi
+import play.api.mvc.{Action, AnyContent}
+
+@Singleton
+class AccountDetailsController @Inject()(implicit val config: FrontendAppConfig,
+                                         implicit val messagesApi: MessagesApi,
+                                         val checkSessionTimeout: SessionTimeoutPredicate,
+                                         val authenticate: AuthenticationPredicate,
+                                         val retrieveNino: NinoPredicate,
+                                         val retrieveIncomeSources: IncomeSourceDetailsPredicate
+                                        ) extends BaseController {
+
+  val getAccountDetails: Action[AnyContent] = (checkSessionTimeout andThen authenticate andThen retrieveNino andThen retrieveIncomeSources).async {
+    implicit user =>
+      val ids: Seq[String] = user.incomeSources.businessIncomeSources.map { business => business.selfEmploymentId}
+      
+  }
+
+}
