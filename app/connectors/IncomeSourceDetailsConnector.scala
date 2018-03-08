bccaf36a3eb3124bdc21b646f14242223b469972
samuel-raspin <samuel.raspin@digital.hmrc.gov.uk>
2018-03-08 11:06:20 +0000

    [BTAT-1959] - added IncomeSourceDetailsConnector + tests

---
+++ app/connectors/IncomeSourceDetailsConnector.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import javax.inject.{Inject, Singleton}
+
+import config.FrontendAppConfig
+import models.{IncomeSourcesError, IncomeSourcesModel, IncomeSourcesResponseModel}
+import play.api.Logger
+import play.api.http.Status.OK
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext._
+
+import scala.concurrent.Future
+
+@Singleton
+class IncomeSourceDetailsConnector @Inject()(val http: HttpClient,
+                                             val config: FrontendAppConfig) extends RawResponseReads {
+
+  private[connectors] lazy val getIncomeSourcesUrl: String => String = nino =>
+    s"${config.itvcProtectedService}/income-tax-view-change/income-sources/$nino"
+
+  def getIncomeSources(nino: String)(implicit headerCarrier: HeaderCarrier): Future[IncomeSourcesResponseModel] = {
+
+    val url = getIncomeSourcesUrl(nino)
+    Logger.debug(s"[IncomeSourceDetailsConnector][getIncomeSources] - GET $url")
+
+    http.GET[HttpResponse](url) map {
+      response =>
+        response.status match {
+          case OK =>
+            Logger.debug(s"[IncomeSourceDetailsConnector][getIncomeSources] - RESPONSE status: ${response.status}, json: ${response.json}")
+            response.json.validate[IncomeSourcesModel].fold(
+              invalid => {
+                Logger.warn(s"[IncomeSourceDetailsConnector][getIncomeSources] - Json Validation Error. Parsing Latest Calc Response")
+                IncomeSourcesError
+              },
+              valid => valid
+            )
+          case _ =>
+            Logger.debug(s"[IncomeSourceDetailsConnector][getIncomeSources] - RESPONSE status: ${response.status}, body: ${response.body}")
+            Logger.warn(s"[IncomeSourceDetailsConnector][getIncomeSources] - Response status: [${response.status}] from Latest Calc call")
+            IncomeSourcesError
+        }
+    } recover {
+      case _ =>
+        Logger.warn(s"[IncomeSourceDetailsConnector][getIncomeSources] - Unexpected future failed error")
+        IncomeSourcesError
+    }
+
+  }
+
+}
---
+++ test/connectors/IncomeSourceDetailsConnectorSpec.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import mocks.MockHttp
+import play.api.libs.json.Json
+import play.mvc.Http.Status
+import uk.gov.hmrc.http.HttpResponse
+import utils.TestSupport
+import assets.TestConstants.IncomeSourceDetails._
+import assets.TestConstants.testNino
+import models.{IncomeSourcesError, IncomeSourcesResponseModel}
+
+import scala.concurrent.Future
+
+class IncomeSourceDetailsConnectorSpec extends TestSupport with MockHttp {
+
+  val successResponse = HttpResponse(Status.OK, Some(Json.toJson(bothIncomeSourcesSuccessBusinessAligned)))
+  val successResponseBadJson = HttpResponse(Status.OK, Some(Json.parse("{}")))
+  val badResponse = HttpResponse(Status.BAD_REQUEST, responseString = Some("Error Message"))
+
+  object TestIncomeSourceDetailsConnector extends IncomeSourceDetailsConnector(mockHttpGet, frontendAppConfig)
+
+  "IncomeSourceDetailsConnector.getIncomeSources" should {
+
+    lazy val testUrl = TestIncomeSourceDetailsConnector.getIncomeSourcesUrl(testNino)
+    def result: Future[IncomeSourcesResponseModel] = TestIncomeSourceDetailsConnector.getIncomeSources(testNino)
+
+    "return a IncomeSourcesModel when successful JSON is received" in {
+      setupMockHttpGet(testUrl)(successResponse)
+      await(result) shouldBe bothIncomeSourcesSuccessBusinessAligned
+    }
+
+    "return IncomeSourcesError in case of bad/malformed JSON response" in {
+      setupMockHttpGet(testUrl)(successResponseBadJson)
+      await(result) shouldBe IncomeSourcesError
+    }
+
+    "return IncomeSourcesError model in case of failure" in {
+      setupMockHttpGet(testUrl)(badResponse)
+      await(result) shouldBe IncomeSourcesError
+    }
+
+    "return IncomeSourcesError model in case of future failed scenario" in {
+      setupMockFailedHttpGet(testUrl)(badResponse)
+      await(result) shouldBe IncomeSourcesError
+    }
+  }
+
+}
