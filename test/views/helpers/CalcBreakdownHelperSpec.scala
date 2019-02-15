89edfff10072863bea80712bfb5011808dba0418
“ashwini.rudrappa” <ashwini.rudrappa@digital.hmrc.gov.uk>
2019-02-15 10:45:32 +0000

    MIT-93:update the call to calk breakdown is from  Estimate Page

--- test/views/helpers/CalcBreakdownHelperSpec.scala
+++ test/views/helpers/CalcBreakdownHelperSpec.scala
@@ -58,7 +58,7 @@ class CalcBreakdownHelperSpec extends TestSupport {
   }
 
 
-  "The Calculation Breakdown View" should {
+  "The Calculation Breakdown VIew on the Estimate Page" should {
 
     val messages = new Messages.Calculation(testYear)
 
