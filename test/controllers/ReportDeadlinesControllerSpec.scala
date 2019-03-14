ec0bd17261881ca04c056dd7c048c45bf2f0cc18
dwalters <david-walters-HMRC@.noreply.github.com>
2019-03-14 15:35:47 +0000

    MIT-177 corrected test

--- test/controllers/ReportDeadlinesControllerSpec.scala
+++ test/controllers/ReportDeadlinesControllerSpec.scala
@@ -199,7 +199,7 @@ class ReportDeadlinesControllerSpec extends MockAuthenticationPredicate with Moc
         val result = TestReportDeadlinesController.getReportDeadlines()(fakeRequestWithActiveSession)
         val document = Jsoup.parse(bodyOf(result))
 
-        document.getElementById("page-heading").text shouldBe "Reports"
+        document.getElementById("page-heading").text shouldBe "Returns"
 
       }
     }
