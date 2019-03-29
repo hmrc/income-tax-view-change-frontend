f63e5f93692bfff567f77594d8c61433fc0bfddb
dwalters <david-walters-HMRC@.noreply.github.com>
2019-03-29 15:55:26 +0000

    MIT-229 added fixes for html issue

--- it/assets/ReportDeadlinesIntegrationTestConstants.scala
+++ it/assets/ReportDeadlinesIntegrationTestConstants.scala
@@ -160,5 +160,15 @@ object ReportDeadlinesIntegrationTestConstants {
     )
   ))
 
+  val crystallisedEOPSModel = ReportDeadlinesModel(List(
+    ReportDeadlineModel(
+      start = "2017-04-06",
+      end = "2018-04-05",
+      due = "2019-01-31",
+      "Crystallised",
+      periodKey = "#003"
+    )
+  ))
+
   val emptyModel = ReportDeadlinesModel(List())
 }
