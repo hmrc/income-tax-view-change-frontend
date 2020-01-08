c48af99431f847ab633e5cc3486209a62f72a0ba
James Forster <jameshforster@users.noreply.github.com>
2020-01-08 09:30:28 +0000

    MISUV-112 - Added test routes to select test scenarios for calculations.

---
+++ app/testOnly/models/TestHeadersModel.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package testOnly.models
+
+import play.api.libs.json.{Json, OFormat}
+
+case class TestHeadersModel(headerName: String)
+
+object TestHeadersModel {
+  implicit val formats: OFormat[TestHeadersModel] = Json.format[TestHeadersModel]
+
+  val validTestHeaders: Map[String, String] = Map(
+    "DEFAULT" -> "Default",
+    "UK_MULTIPLE_INCOMES_EXAMPLE" -> "Multiple Incomes",
+    "UK_PROP_DIVIDENDS_EXAMPLE" -> "Property with Dividends",
+    "UK_PROP_GIFTAID_EXAMPLE" -> "Property with Giftaid",
+    "UK_PROP_SAVINGS_EXAMPLE" -> "Property with Savings",
+    "SCOT_SE_DIVIDENDS_EXAMPLE" -> "Scottish Self Employed with Dividends",
+    "UK_SE_GIFTAID_EXAMPLE" -> "Self-Employed with Giftaid",
+    "UK_SE_SAVINGS_EXAMPLE" -> "Self Employed with Savings",
+    "ERROR_MESSAGES_EXIST" -> "Errored Calculation",
+    "NOT_FOUND" -> "Calculation Not Found"
+  )
+
+}
