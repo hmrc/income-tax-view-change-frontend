ab95156eeb79acb3c0b22dc38aef2e6fe3b8cd64
Chandra Andem <36305926+chandraAndem@users.noreply.github.com>
2020-04-30 15:18:27 +0100

    Tax due controller and FS changes

---
+++ it/controllers/TaxDueSummaryControllerISpec.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import java.time.LocalDateTime
+
+import assets.BaseIntegrationTestConstants._
+import assets.CalcDataIntegrationTestConstants._
+import assets.IncomeSourceIntegrationTestConstants._
+import assets.messages.{TaxDueSummaryMessages => messages}
+import config.featureswitch.{TaxDue, FeatureSwitching}
+import helpers.ComponentSpecBase
+import helpers.servicemocks._
+import models.calculation.{CalculationItem, ListCalculationItems}
+import play.api.http.Status._
+
+class TaxDueSummaryControllerISpec extends ComponentSpecBase with FeatureSwitching {
+
+  "Calling the TaxDueSummaryController.showTaxDueSummary(taxYear)" when {
+
+    "isAuthorisedUser with an active enrolment, valid nino and tax year, valid CalcDisplayModel response, " +
+      "feature switch TaxDue is enabled" should {
+      enable(TaxDue)
+      "return the correct tax due summary page" in {
+
+        And("I wiremock stub a successful TaxDue Details response with single Business and Property income")
+        IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, businessAndPropertyResponse)
+
+        And("I stub a successful calculation response for 2017-18")
+        IndividualCalculationStub.stubGetCalculationList(testNino, "2017-18")(
+          status = OK,
+          body = ListCalculationItems(Seq(CalculationItem("idOne", LocalDateTime.now())))
+        )
+        IndividualCalculationStub.stubGetCalculation(testNino, "idOne")(
+          status = OK,
+          body = estimatedCalculationFullJson
+        )
+
+        When(s"I call GET /report-quarterly/income-and-expenses/view/calculation/$testYear/tax-due")
+        val res = IncomeTaxViewChangeFrontend.getTaxDueSummary(testYear)
+
+        verifyIncomeSourceDetailsCall(testMtditid)
+        IndividualCalculationStub.verifyGetCalculationList(testNino, "2017-18")
+        IndividualCalculationStub.verifyGetCalculation(testNino, "idOne")
+
+        res should have(
+          httpStatus(OK),
+          pageTitle(messages.taxDueSummaryTitle),
+          elementTextBySelector("h1")(messages.taxDueSummaryHeading ++ " " + messages.taxDueSummaryTitle)
+        )
+      }
+    }
+
+    unauthorisedTest("/calculation/" + testYear + "/tax-due")
+
+  }
+}
