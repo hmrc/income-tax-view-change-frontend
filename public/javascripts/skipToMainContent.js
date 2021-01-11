3112ee71d70b35c93126e3ee101dd1774ce9f7ba
Alex Rimmer <31283208+AlexRimmerHMRC@users.noreply.github.com>
2021-01-11 13:04:05 +0000

    MISUV-1313 - Updated the skip link script to only focus on the heading

--- public/javascripts/skipToMainContent.js
+++ public/javascripts/skipToMainContent.js
@@ -1,7 +1,7 @@
 $(document).ready($(function () {
     document.getElementById('skip-link').addEventListener('click', function(e) {
         e.preventDefault();
-        var target = document.querySelectorAll('.content__body, .report-error')[0];
+        var target = document.querySelectorAll('h1')[0];
         target.setAttribute('tabindex', '-1');
         target.focus();
     });
