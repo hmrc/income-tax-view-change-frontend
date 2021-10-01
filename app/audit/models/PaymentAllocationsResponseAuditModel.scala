ae169832112e50f56a51698cb5ab8bcdb8d71466
Joseph Sheridan <joesheridan@users.noreply.github.com>
2021-10-01 14:17:46 +0100

    Misuv 2473 Add Tax Year Overview Late Payment Details to Audit Events (#906)
    * fixed paymentallocationresponse audit message description
    
    * MISUV-2473 add late payment interest details to audit events
    
    * PR comments

--- app/audit/models/PaymentAllocationsResponseAuditModel.scala
+++ app/audit/models/PaymentAllocationsResponseAuditModel.scala
@@ -48,13 +48,27 @@ case class PaymentAllocationsResponseAuditModel(mtdItUser: MtdItUser[_],
     }
   }
 
+  private def getAllocationDescriptionFromKey(key: String): String = key match {
+    case "paymentAllocation.paymentAllocations.poa1.incomeTax" => "Income Tax for payment on account 1 of 2"
+    case "paymentAllocation.paymentAllocations.poa1.nic4" => "Class 4 National Insurance for payment on account 1 of 2"
+    case "paymentAllocation.paymentAllocations.poa2.incomeTax" => "Income Tax for payment on account 2 of 2"
+    case "paymentAllocation.paymentAllocations.poa2.nic4" => "Class 4 National Insurance for payment on account 2 of 2"
+    case "paymentAllocation.paymentAllocations.bcd.incomeTax" => "Income Tax for remaining balance"
+    case "paymentAllocation.paymentAllocations.bcd.nic2" => "Class 2 National Insurance for remaining balance"
+    case "paymentAllocation.paymentAllocations.bcd.vcnic2" => "Voluntary Class 2 National Insurance for remaining balance"
+    case "paymentAllocation.paymentAllocations.bcd.nic4" => "Class 4 National Insurance for remaining balance"
+    case "paymentAllocation.paymentAllocations.bcd.sl" => "Student Loans for remaining balance"
+    case "paymentAllocation.paymentAllocations.bcd.cgt" => "Capital Gains Tax for remaining balance"
+  }
+
   private def paymentAllocationDetail(): JsObject = Json.obj() ++
     ("paymentMadeDate", paymentAllocations.paymentAllocationChargeModel.financialDetails.head.items.flatMap(_.head.dueDate)) ++
     ("paymentMadeAmount", getPaymentMadeAmount) ++
     Json.obj("paymentAllocations" -> paymentAllocations.originalPaymentAllocationWithClearingDate.map {
         case (_, allocationDetail: Option[AllocationDetail], dateAllocated) =>
           Json.obj() ++
-            ("paymentAllocationDescription", allocationDetail.map(_.getPaymentAllocationKeyInPaymentAllocations)) ++
+            ("paymentAllocationDescription", allocationDetail.map(ad =>
+              getAllocationDescriptionFromKey(ad.getPaymentAllocationKeyInPaymentAllocations))) ++
             ("dateAllocated", dateAllocated) ++
             ("amount", allocationDetail.flatMap { _.amount }) ++
             ("taxYear", allocationDetail.flatMap { _.to }.map(getTaxYearString))
