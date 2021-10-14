183ad5b1af4e739c0c30ac2651ea198d9ccda957
Deng Nyuar <60218795+dnyuar@users.noreply.github.com>
2021-10-14 17:41:30 +0100

    Sass 1176 - Updating agent poller to direct to tax year overview page (#914)
    * calculation poller for agents updated
    
    * integration tests added
    
    * working agent links on final tax overview page
    
    * integration test errors fixed
    
    * changing agent taxYearOverview to use breakdownPartial
    
    * integrations test errors fixed
    
    * missing tests readded
    
    * rebase test errors fix and adding it test for calculationPollingControllerISpec

--- it/controllers/agent/FinalTaxCalculationControllerISpec.scala
+++ it/controllers/agent/FinalTaxCalculationControllerISpec.scala
@@ -89,18 +89,18 @@ class FinalTaxCalculationControllerISpec extends ComponentSpecBase with SessionC
 
     val incomeText = "Income"
     val incomeAmount = "£199,505.00"
-    val incomeLink = "/report-quarterly/income-and-expenses/view/calculation/2018/income"
+    val incomeLink = "/report-quarterly/income-and-expenses/view/agents/calculation/2018/income"
 
     val allowanceText = "Allowances and deductions"
     val allowanceAmount = "−£500.00"
-    val allowanceLink = "/report-quarterly/income-and-expenses/view/calculation/2018/deductions"
+    val allowanceLink = "/report-quarterly/income-and-expenses/view/agents/calculation/2018/deductions"
 
     val taxIsDueText = "Total taxable income"
     val taxIsDueAmount = "£198,500.00"
 
     val contributionText = "Income Tax and National Insurance contributions"
     val contributionAmount = "£90,500.00"
-    val contributionLink = "/report-quarterly/income-and-expenses/view/calculation/2018/tax-due"
+    val contributionLink = "/report-quarterly/income-and-expenses/view/agents/calculation/2018/tax-due"
 
     val chargeInformationParagraph: String = "The amount your client needs to pay might be different if there are other charges or payments on their account."
 
