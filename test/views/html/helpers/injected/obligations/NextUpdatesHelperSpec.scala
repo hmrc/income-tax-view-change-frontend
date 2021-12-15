d5c4678d2aeb8f5f85e2da446c47f8fe26fbfd6c
andriikondiuk <94916773+andriikondiuk@users.noreply.github.com>
2021-12-15 17:25:47 +0000

    MISUV-2856 Add nextUpdates.business message for Welsh and test refactoring

--- test/views/html/helpers/injected/obligations/NextUpdatesHelperSpec.scala
+++ test/views/html/helpers/injected/obligations/NextUpdatesHelperSpec.scala
@@ -17,7 +17,7 @@
 package views.html.helpers.injected.obligations
 
 import testConstants.BaseTestConstants.testMtdItUser
-import testConstants.BusinessDetailsTestConstants.business1
+import testConstants.BusinessDetailsTestConstants.{business1, testTradeName}
 import testConstants.NextUpdatesTestConstants.{crystallisedObligation, twoObligationsSuccessModel}
 import models.nextUpdates.{NextUpdatesModel, ObligationsModel}
 import org.jsoup.Jsoup
@@ -65,7 +65,7 @@ class NextUpdatesHelperSpec extends TestSupport {
 
       section.select("tbody tr").size() shouldBe 1
       section.select("tbody tr td:nth-of-type(1)").text() shouldBe "Quarterly"
-      section.select("tbody tr td:nth-of-type(2)").text() shouldBe "business"
+      section.select("tbody tr td:nth-of-type(2)").text() shouldBe testTradeName
     }
 
     "display the later due date" in new Setup(obligationsModel) {
@@ -77,7 +77,7 @@ class NextUpdatesHelperSpec extends TestSupport {
 
       section.select("tbody tr").size() shouldBe 1
       section.select("tbody tr td:nth-of-type(1)").text() shouldBe "Annual Update"
-      section.select("tbody tr td:nth-of-type(2)").text() shouldBe "business"
+      section.select("tbody tr td:nth-of-type(2)").text() shouldBe testTradeName
     }
 
     "display the correct due date text for a quarterly date" in new Setup(obligationsModel) {
