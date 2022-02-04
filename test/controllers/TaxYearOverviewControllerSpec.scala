e77f265729fa78a4fa7844d8e891c099b00a4624
Andrii Kondiuk <94916773+andriikondiuk@users.noreply.github.com>
2022-02-04 14:52:54 +0000

    MISUV-2940 Make .get throw an exception if data doesn't exist in Tax year template files  (#1072)
    * MISUV-2940 refactor by using .map TaxYears view
    
    * MISUV-2940 Refactor .get to map or throw exception when needed
    
    * MISUV-2940 Fix merge conflict and broken test
    
    * MISUV-2940 Addressing PR comments
    
    * MISUV-2940 Addressing PR comments

--- test/controllers/TaxYearOverviewControllerSpec.scala
+++ test/controllers/TaxYearOverviewControllerSpec.scala
@@ -27,6 +27,7 @@ import models.calculation.CalcOverview
 import models.financialDetails.DocumentDetailWithDueDate
 import models.liabilitycalculation.viewmodels.TaxYearOverviewViewModel
 import models.nextUpdates.{NextUpdatesErrorModel, ObligationsModel}
+import org.jsoup.Jsoup
 import play.api.http.Status
 import play.api.mvc.MessagesControllerComponents
 import play.api.test.Helpers._
@@ -688,19 +689,19 @@ class TaxYearOverviewControllerSpec extends TestSupport with MockCalculationServ
             response = testObligtionsModel
           )
 
-          val expectedContent: String = taxYearOverviewViewOld(
+          val expectedContent: String =  Jsoup.parse(taxYearOverviewViewOld(
             testYear,
             None,
             testChargesList,
             testObligtionsModel,
             taxYearsBackLink,
             codingOutEnabled = true
-          ).toString
+          ).toString()).text()
 
           val result = TestTaxYearOverviewController.renderTaxYearOverviewPage(testYear)(fakeRequestWithActiveSessionWithReferer(referer = taxYearsBackLink))
 
           status(result) shouldBe Status.OK
-          contentAsString(result) shouldBe expectedContent
+          Jsoup.parse(contentAsString(result)).text() shouldBe expectedContent
           contentType(result) shouldBe Some("text/html")
           result.futureValue.session.get(SessionKeys.chargeSummaryBackPage) shouldBe Some("taxYearOverview")
         }
