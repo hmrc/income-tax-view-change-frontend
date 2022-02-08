05af4855f71d58f3d160db527d56af9d08f600fa
Roheel Arshad <48284720+RoheelArshad@users.noreply.github.com>
2022-02-08 12:27:14 +0000

    Misuv 2976-Changed Next Updates Tables to Summary Links (#1082)
    * MISUV-2976: Summary list update
    
    * MISUV-2976 updated unit/it tests
    
    * MISUV-2976 removed println
    
    Co-authored-by: AIbrahimQA <57941383+AIbrahimQA@users.noreply.github.com>

--- test/views/NextUpdatesViewSpec.scala
+++ test/views/NextUpdatesViewSpec.scala
@@ -113,9 +113,9 @@ class NextUpdatesViewSpec extends TestSupport {
 		s"have the correct TradeName" in new Setup(obligationsModel) {
 			val section = pageDocument.select(".govuk-accordion__section:nth-of-type(2)")
 
-			section.select("tbody tr").size() shouldBe 1
-			section.select("tbody tr td:nth-of-type(1)").text() shouldBe "Annual Update"
-			section.select("tbody tr td:nth-of-type(2)").text() shouldBe testTradeName
+			section.select("dl").size() shouldBe 1
+			section.select("dl dt").text() shouldBe "Annual Update"
+			section.select("dl dd").text() shouldBe testTradeName
 		}
 	}
 }
