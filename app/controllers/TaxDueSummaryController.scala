4e0a85054059bb7b932c17cacfc7d0a965fbcd05
AIbrahimQA <57941383+AIbrahimQA@users.noreply.github.com>
2022-03-21 13:47:52 +0000

    MISUV-3167: Unify TaxDueSummaryController and Unit tests (#1162)
    * MISUV-3167: Unify TaxDueSummaryController and Unit tests
    
    * Removing unused variable from spec

--- app/controllers/TaxDueSummaryController.scala
+++ app/controllers/TaxDueSummaryController.scala
@@ -20,62 +20,91 @@ import audit.AuditingService
 import audit.models._
 import auth.MtdItUser
 import config.featureswitch.{Class4UpliftEnabled, FeatureSwitching}
-import config.{FrontendAppConfig, ItvcErrorHandler}
+import config.{AgentItvcErrorHandler, FrontendAppConfig, ItvcErrorHandler, ShowInternalServerError}
+import controllers.agent.predicates.ClientConfirmedController
 import controllers.predicates._
 import implicits.ImplicitDateFormatter
+import javax.inject.{Inject, Singleton}
 import models.liabilitycalculation.viewmodels._
 import models.liabilitycalculation.{LiabilityCalculationError, LiabilityCalculationResponse}
 import play.api.Logger
 import play.api.i18n.I18nSupport
 import play.api.mvc._
-import services.CalculationService
+import services.{CalculationService, IncomeSourceDetailsService}
+import uk.gov.hmrc.auth.core.AuthorisedFunctions
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.language.LanguageUtils
 import views.html.TaxCalcBreakdown
 
-import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class TaxDueSummaryController @Inject()(checkSessionTimeout: SessionTimeoutPredicate,
-                                        authenticate: AuthenticationPredicate,
-                                        retrieveNino: NinoPredicate,
-                                        retrieveIncomeSources: IncomeSourceDetailsPredicate,
-                                        calculationService: CalculationService,
-                                        itvcErrorHandler: ItvcErrorHandler,
-                                        retrieveBtaNavPartial: BtaNavBarPredicate,
-                                        taxCalcBreakdown: TaxCalcBreakdown,
-                                        val auditingService: AuditingService)
+class TaxDueSummaryController @Inject()(val authorisedFunctions: AuthorisedFunctions,
+                                        val checkSessionTimeout: SessionTimeoutPredicate,
+                                        val authenticate: AuthenticationPredicate,
+                                        val retrieveNino: NinoPredicate,
+                                        val retrieveIncomeSources: IncomeSourceDetailsPredicate,
+                                        val calculationService: CalculationService,
+                                        val incomeSourceDetailsService: IncomeSourceDetailsService,
+                                        val itvcErrorHandler: ItvcErrorHandler,
+                                        implicit val itvcErrorHandlerAgent: AgentItvcErrorHandler,
+                                        val taxCalcBreakdown: TaxCalcBreakdown,
+                                        val auditingService: AuditingService,
+                                        val retrieveBtaNavBar: BtaNavBarPredicate)
                                        (implicit val appConfig: FrontendAppConfig,
                                         val languageUtils: LanguageUtils,
                                         mcc: MessagesControllerComponents,
-                                        val executionContext: ExecutionContext
-                                       ) extends BaseController with ImplicitDateFormatter with FeatureSwitching with I18nSupport {
+                                        implicit val ec: ExecutionContext
+                                       ) extends ClientConfirmedController with ImplicitDateFormatter with FeatureSwitching with I18nSupport {
 
+  def handleRequest(backUrl: String,
+                    itcvErrorHandler: ShowInternalServerError,
+                    taxYear: Int,
+                    isAgent: Boolean
 
-  val action: ActionBuilder[MtdItUser, AnyContent] = checkSessionTimeout andThen authenticate andThen retrieveNino andThen
-    retrieveIncomeSources andThen retrieveBtaNavPartial
+                   )
+                   (implicit user: MtdItUser[_], hc: HeaderCarrier): Future[Result] = {
 
-  def showTaxDueSummary(taxYear: Int): Action[AnyContent] = {
-    action.async {
-      implicit user => {
-        calculationService.getLiabilityCalculationDetail(user.mtditid, user.nino, taxYear).map {
-          case liabilityCalc: LiabilityCalculationResponse =>
-            val viewModel = TaxDueSummaryViewModel(liabilityCalc)
-            auditingService.extendedAudit(TaxDueResponseAuditModel(user, viewModel, taxYear))
-            Ok(taxCalcBreakdown(viewModel, taxYear, backUrl(taxYear), false, user.btaNavPartial, isEnabled(Class4UpliftEnabled)))
-          case calcErrorResponse: LiabilityCalculationError if calcErrorResponse.status == NOT_FOUND =>
-            Logger("application").info("[TaxDueController][showTaxDueSummary] No calculation data returned from downstream. Not Found.")
-            itvcErrorHandler.showInternalServerError()
-          case _: LiabilityCalculationError =>
-            Logger("application").error(
-              "[TaxDueController][showTaxDueSummary[" + taxYear +
-                "]] No new calc deductions data error found. Downstream error")
-            itvcErrorHandler.showInternalServerError()
-        }
-      }
+    calculationService.getLiabilityCalculationDetail(user.mtditid, user.nino, taxYear).map {
+      case liabilityCalc: LiabilityCalculationResponse =>
+        val viewModel = TaxDueSummaryViewModel(liabilityCalc)
+        auditingService.extendedAudit(TaxDueResponseAuditModel(user, viewModel, taxYear))
+        Ok(taxCalcBreakdown(viewModel, taxYear, backUrl, isAgent = isAgent, btaNavPartial = user.btaNavPartial, isEnabled(Class4UpliftEnabled)))
+      case calcErrorResponse: LiabilityCalculationError if calcErrorResponse.status == NOT_FOUND =>
+        Logger("application").info("[TaxDueController][showTaxDueSummary] No calculation data returned from downstream. Not Found.")
+        itvcErrorHandler.showInternalServerError()
+      case _: LiabilityCalculationError =>
+        Logger("application").error(
+          "[TaxDueController][showTaxDueSummary[" + taxYear +
+            "]] No new calc deductions data error found. Downstream error")
+        itvcErrorHandler.showInternalServerError()
     }
   }
 
-  def backUrl(taxYear: Int): String = controllers.routes.TaxYearOverviewController.renderTaxYearOverviewPage(taxYear).url
 
+  def showTaxDueSummary(taxYear: Int): Action[AnyContent] = {
+    (checkSessionTimeout andThen authenticate andThen retrieveNino andThen retrieveIncomeSources andThen retrieveBtaNavBar).async {
+      implicit user =>
+        handleRequest(
+          backUrl = controllers.routes.TaxYearOverviewController.renderTaxYearOverviewPage(taxYear).url,
+          itcvErrorHandler = itvcErrorHandler,
+          taxYear = taxYear,
+          isAgent = false
+        )
+    }
+  }
+
+  def showTaxDueSummaryAgent(taxYear: Int): Action[AnyContent] = {
+    Authenticated.async { implicit request =>
+      implicit user =>
+        getMtdItUserWithIncomeSources(incomeSourceDetailsService, useCache = true) flatMap { implicit mtdItUser =>
+          handleRequest(
+            backUrl = controllers.agent.routes.TaxYearOverviewController.show(taxYear).url,
+            itcvErrorHandler = itvcErrorHandlerAgent,
+            taxYear = taxYear,
+            isAgent = true
+          )
+        }
+    }
+  }
 }
