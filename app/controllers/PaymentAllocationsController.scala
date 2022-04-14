2f8f064592a535011b7c709852c0bbd6b4c44c5d
Jack Malik <jack.malik@hotmail.com>
2022-04-14 10:23:09 +0200

    Misuv 3190 1 (#1201)
    * Modified handleRequest method. Added unit test to cover the new logic.
    
    * Modified handleRequest with updated call to paymentAllocationView.

--- app/controllers/PaymentAllocationsController.scala
+++ app/controllers/PaymentAllocationsController.scala
@@ -32,8 +32,9 @@ import services.{IncomeSourceDetailsService, PaymentAllocationsService}
 import uk.gov.hmrc.auth.core.AuthorisedFunctions
 import uk.gov.hmrc.http.HeaderCarrier
 import views.html.PaymentAllocation
-
 import javax.inject.{Inject, Singleton}
+import play.api.Logger
+
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
@@ -56,6 +57,7 @@ class PaymentAllocationsController @Inject()(val paymentAllocationView: PaymentA
   private lazy val redirectUrlIndividual: String = controllers.errors.routes.NotFoundDocumentIDLookupController.show().url
   private lazy val redirectUrlAgent: String = controllers.agent.errors.routes.AgentNotFoundDocumentIDLookupController.show().url
 
+
   def handleRequest(backUrl: String,
                     itvcErrorHandler: ShowInternalServerError,
                     documentNumber: String,
@@ -64,10 +66,18 @@ class PaymentAllocationsController @Inject()(val paymentAllocationView: PaymentA
                     origin: Option[String] = None)
                    (implicit user: MtdItUser[_], hc: HeaderCarrier, ec: ExecutionContext, messages: Messages): Future[Result] = {
 
+
     paymentAllocations.getPaymentAllocation(Nino(user.nino), documentNumber) map {
       case Right(paymentAllocations) =>
-        auditingService.extendedAudit(PaymentAllocationsResponseAuditModel(user, paymentAllocations))
-        Ok(paymentAllocationView(paymentAllocations, backUrl = backUrl, user.saUtr, CutOverCreditsEnabled=isEnabled(CutOverCredits), btaNavPartial = user.btaNavPartial, isAgent = isAgent, origin = origin)(implicitly, messages))
+
+        if (!isEnabled(CutOverCredits) && paymentAllocations.paymentAllocationChargeModel.documentDetails.exists(_.credit.isDefined)){
+          Logger("application").warn(s"[PaymentAllocationsController][handleRequest] CutOverCredits is disabled and redirected to not found page")
+          Redirect(controllers.errors.routes.NotFoundDocumentIDLookupController.show().url)
+        } else {
+          auditingService.extendedAudit(PaymentAllocationsResponseAuditModel(user, paymentAllocations))
+          Ok(paymentAllocationView(paymentAllocations, backUrl = backUrl, user.saUtr, CutOverCreditsEnabled=isEnabled(CutOverCredits), btaNavPartial = user.btaNavPartial, isAgent = isAgent, origin = origin)(implicitly, messages))
+        }
+
       case Left(PaymentAllocationError(Some(Http.Status.NOT_FOUND))) =>
         Redirect(redirectUrl)
       case _ => itvcErrorHandler.showInternalServerError()
