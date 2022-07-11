cfcbe1da34a39af80b148b456232a1a653efde6e
Joseph Sheridan <joesheridan@users.noreply.github.com>
2022-07-11 14:15:27 +0100

    MISUV-3719 refactor and tests for payment history refunds

--- app/models/repaymentHistory/RepaymentHistoryUtils.scala
+++ app/models/repaymentHistory/RepaymentHistoryUtils.scala
@@ -28,9 +28,9 @@ object RepaymentHistoryUtils {
 
   private def getControllerHref(transactionId: Option[String], isAgent: Boolean) = {
     if (isAgent) {
-      controllers.routes.PaymentAllocationsController.viewPaymentAllocationAgent(transactionId.getOrElse(throw MissingFieldException("Document ID"))).url
+      controllers.routes.PaymentAllocationsController.viewPaymentAllocationAgent(transactionId.getOrElse(throw MissingFieldException("Transaction ID"))).url
     } else {
-      controllers.routes.PaymentAllocationsController.viewPaymentAllocation(transactionId.getOrElse(throw MissingFieldException("Document ID"))).url
+      controllers.routes.PaymentAllocationsController.viewPaymentAllocation(transactionId.getOrElse(throw MissingFieldException("Transaction ID"))).url
     }
   }
 
@@ -43,9 +43,41 @@ object RepaymentHistoryUtils {
     }
   }
 
-  def combinePaymentHistoryData(payments: List[Payment], repayments: List[RepaymentHistory], isAgent: Boolean,
-                               MFACreditsEnabled: Boolean, CutOverCreditsEnabled: Boolean, languageUtils: LanguageUtils
-                               )(implicit messages: Messages): List[PaymentHistoryEntry] = {
+  private def getRefundsLink(isAgent: Boolean) = {
+    if (isAgent) {
+      "agents/refund-to-taxpayer"
+    } else {
+      "refund-to-taxpayer"
+    }
+  }
+
+  private def groupedPayments(payments: List[PaymentHistoryEntry]): List[(Int, List[PaymentHistoryEntry])] = {
+    def sortPayments(payments: List[PaymentHistoryEntry]) = {
+      payments
+        .map(payment => (LocalDate.parse(payment.date).toEpochDay, payment))
+        .sortWith((left, right) => left._1 < right._1)
+        .map { case (_, payments) => payments }
+    }
+
+    payments.groupBy[Int] { payment => {
+      LocalDate.parse(payment.date).getYear
+    }
+    }.toList.sortBy(_._1).reverse
+      .map { case (year, payments) => (year, sortPayments(payments)) }
+  }
+
+  def getGroupedPaymentHistoryData(payments: List[Payment], repayments: List[RepaymentHistory], isAgent: Boolean,
+                                   MFACreditsEnabled: Boolean, CutOverCreditsEnabled: Boolean, languageUtils: LanguageUtils
+                                  )(implicit messages: Messages): List[(Int, List[PaymentHistoryEntry])] = {
+    val combinedPayments = combinePaymentHistoryData(payments, repayments, isAgent,
+      MFACreditsEnabled, CutOverCreditsEnabled, languageUtils
+    )
+    groupedPayments(combinedPayments)
+  }
+
+  private def combinePaymentHistoryData(payments: List[Payment], repayments: List[RepaymentHistory], isAgent: Boolean,
+                                        MFACreditsEnabled: Boolean, CutOverCreditsEnabled: Boolean, languageUtils: LanguageUtils
+                                       )(implicit messages: Messages): List[PaymentHistoryEntry] = {
     val filteredPayments = payments.flatMap(payment => {
       if (payment.credit.isEmpty) {
         Some(PaymentHistoryEntry(
@@ -54,7 +86,7 @@ object RepaymentHistoryUtils {
           transactionId = payment.transactionId,
           amount = payment.amount,
           linkUrl = getControllerHref(payment.transactionId, isAgent),
-          visuallyHiddenText = s"${payment.dueDate.get} ${payment.amount.map(_.abs.toCurrency)}"
+          visuallyHiddenText = s"${payment.dueDate.get} ${payment.amount.getOrElse(throw MissingFieldException("Amount")).abs.toCurrency}"
         ))
       } else {
         if (payment.validMFACreditDescription()) {
@@ -64,7 +96,7 @@ object RepaymentHistoryUtils {
               description = "paymentHistory.mfaCredit",
               amount = payment.amount,
               linkUrl = getMFACreditsLink(payment.documentDate, isAgent),
-              visuallyHiddenText = s"${payment.transactionId.getOrElse(throw MissingFieldException("Document ID"))}"
+              visuallyHiddenText = s"${payment.transactionId.getOrElse(throw MissingFieldException("Transaction ID"))}"
             ))
           } else None
         } else {
@@ -86,7 +118,7 @@ object RepaymentHistoryUtils {
         date = languageUtils.Dates.shortDate(repayment.estimatedRepaymentDate)(messages),
         description = "paymentHistory.refund",
         amount = Some(repayment.totalRepaymentAmount),
-        linkUrl = "refund-to-taxpayer",
+        linkUrl = getRefundsLink(isAgent),
         visuallyHiddenText = s"${repayment.repaymentRequestNumber}"
       ))
     })
