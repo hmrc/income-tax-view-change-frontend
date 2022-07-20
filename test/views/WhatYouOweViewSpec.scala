d50147b220b2a01be6fd3136b58b1a8fc189cd04
Edward Dennis <98972365+edward-dennis-ey@users.noreply.github.com>
2022-07-20 11:28:51 +0100

    revert to previous WYO view test

--- test/views/WhatYouOweViewSpec.scala
+++ test/views/WhatYouOweViewSpec.scala
@@ -1082,7 +1082,7 @@ class WhatYouOweViewSpec extends TestSupport with FeatureSwitching with Implicit
     "have an HMRC adjustment payment due" in new Setup(charges = whatYouOweDataWithMFADebits, MFADebitsEnabled = true) {
       pageDocument.title() shouldBe whatYouOweTitle
       pageDocument.selectFirst("h1").text shouldBe whatYouOweHeading
-      pageDocument.select("#due-0-late-link2").text.shouldBe(s"$hmrcAdjustment ${LocalDate.now().getYear.toString}")
+      pageDocument.getElementById("due-0").text.contains(hmrcAdjustment)
       pageDocument.select("#payments-due-table tbody > tr").size() shouldBe 1
     }
     "display the payment details content" in new Setup(charges = whatYouOweDataWithMFADebits, MFADebitsEnabled = true) {
