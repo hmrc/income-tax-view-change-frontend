248542c9ea224c54b875a31ae1ac3998c23f4bb9
Sarina Salamon <sarina.salamon@gmail.com>
2022-09-06 10:21:27 +0100

    MISUV-3931 sorting out signing issue

--- test/testUtils/CurrencyFormatterSpec.scala
+++ test/testUtils/CurrencyFormatterSpec.scala
@@ -36,7 +36,7 @@ class CurrencyFormatterSpec extends TestSupport with ImplicitCurrencyFormatter {
       }
     }
 
-    "The CurrencyFormatterInt.toNegativeCurrency" should {
+    "The new CurrencyFormatterInt.toNegativeCurrency" should {
       "return a value as negative" when {
         "the number is more than 0" in {
           val amount: Int = 1
