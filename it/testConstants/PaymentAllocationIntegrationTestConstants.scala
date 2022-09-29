3ae569960d629f4c0abac7aaf6516d191cea586f
avelx <3528155+avelx@users.noreply.github.com>
2022-09-29 09:48:02 +0100

    MISUV-4129:   * fix compilation errors[2];

--- it/testConstants/PaymentAllocationIntegrationTestConstants.scala
+++ it/testConstants/PaymentAllocationIntegrationTestConstants.scala
@@ -262,7 +262,7 @@ object PaymentAllocationIntegrationTestConstants {
   )
 
   val testValidPaymentAllocationsModel: PaymentAllocations = PaymentAllocations(
-    Some(110.10), Some("Payment by Card"), Some("2019-05-27"), Some("reference"),
+    Some(110.10), Some("Payment by Card"), Some(LocalDate.parse("2019-05-27")), Some("reference"),
     Seq(
       AllocationDetail(Some("1040000872"), Some(LocalDate.parse("2019-06-27")), Some(LocalDate.parse("2019-08-27")), Some("NIC4 Wales"), Some("SA Payment on Account 1"), Some(10.10), Some(5.50), Some("chargeReference1")),
       AllocationDetail(Some("1040000873"), Some(LocalDate.parse("2019-07-28")), Some(LocalDate.parse("2019-09-28")), Some("NIC4 Wales"), Some("SA Payment on Account 1"), Some(10.90), Some(5.90), Some("chargeReference2"))
