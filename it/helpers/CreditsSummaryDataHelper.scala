e868ea9176e6b6a1864e25f227bb9c56c07d3fe3
avelx <3528155+avelx@users.noreply.github.com>
2022-12-23 10:16:21 +0000

    MISUV-4694:  * fix failing IT tests;

--- it/helpers/CreditsSummaryDataHelper.scala
+++ it/helpers/CreditsSummaryDataHelper.scala
@@ -8,6 +8,20 @@ import java.time.LocalDate
 trait CreditsSummaryDataHelper {
   val chargesList: Seq[CreditDetailModel] = Seq(
     CreditDetailModel(
+      date = LocalDate.of(2018, 3, 29),
+      documentDetail = DocumentDetail(
+        taxYear = "2023",
+        transactionId = "transId",
+        documentDescription = Some("docId"),
+        documentText = Some("text"),
+        outstandingAmount = Some(BigDecimal("1400")),
+        originalAmount = Some(BigDecimal("1400")),
+        documentDate = LocalDate.now()
+      ),
+      creditType = MfaCreditType,
+      balanceDetails = None
+    ),
+    CreditDetailModel(
       date = LocalDate.of(2023, 12, 21),
       documentDetail = DocumentDetail(
         taxYear = "2023",
