7e1dce7b3c4d9620430ff978acfe428529f20ae2
Tharun Prasath <65491423+tharunprasath@users.noreply.github.com>
2023-01-13 09:53:46 +0000

    MISUV-4790 To handle date format in dd/MM/yyyy (#1574)
    * MISUV-4790 To handle date format in dd/MM/yyyy
    
    * MISUV-4790 To handle date format in dd/MM/yyyy in test cases
    
    * MISUV-4790 To handle date format in dd/MM/yyyy in test cases
    
    * MISUV-4790 To handle date format in dd/MM/yyyy in test cases

--- test/models/liabilitycalculation/viewmodels/TaxYearSummaryViewModelSpec.scala
+++ test/models/liabilitycalculation/viewmodels/TaxYearSummaryViewModelSpec.scala
@@ -156,7 +156,7 @@ class TaxYearSummaryViewModelSpec extends UnitSpec with ImplicitDateParser {
             periodTo = None,
             messages = Some(Messages(
               errors = Some(List(
-                Message("C55012", "the update must align to the accounting period end date of 5 January 2023."),
+                Message("C55012", "the update must align to the accounting period end date of 05/01/2023."),
                 Message("C15507", "you’ve claimed £2000 in Property Income Allowance but this is more than turnover for your UK property."),
                 Message("C15510", "the Rent a Room relief claimed for a jointly let property cannot be more than 10% of the Rent a Room limit."),
                 Message("C55009", "updates cannot include gaps.")
