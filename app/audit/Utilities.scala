20d2c77ff7b93ca0e75028b8ec38aa5d6d4ca1d1
jordhemsworth <88255447+JordHemsworth@users.noreply.github.com>
2023-02-06 09:45:22 +0000

    MISUV-4885 - Added match for rejected by NPS charge type

--- app/audit/Utilities.scala
+++ app/audit/Utilities.scala
@@ -39,9 +39,10 @@ object Utilities {
     case None => Json.obj()
   }
 
-  def getChargeType(docDetail: DocumentDetail, latePaymentCharge: Boolean = false): Option[String] = (docDetail.documentDescription, docDetail.documentText) match {
+  def getChargeType(docDetail: DocumentDetail, latePaymentCharge: Boolean = false): Option[String] =
+    (docDetail.documentDescription, docDetail.documentText) match {
     case (_, Some(documentDescription)) if (documentDescription.contains("Class 2 National Insurance")) => Some("Class 2 National Insurance")
-    case(_, Some(documentDescription)) if (documentDescription.contains("Cancelled PAYE Self Assessment (through your PAYE tax code)")) => Some("Cancelled PAYE Self Assessment (through your PAYE tax code)")
+    case(_, Some(documentDescription)) if (documentDescription.contains("Cancelled PAYE Self Assessment")) => Some("Cancelled PAYE Self Assessment (through your PAYE tax code)")
     case (Some("ITSA- POA 1"), _) => if (latePaymentCharge) Some("Late payment interest for payment on account 1 of 2") else Some("Payment on account 1 of 2")
     case (Some("ITSA - POA 2"),_) => if (latePaymentCharge) Some("Late payment interest for payment on account 2 of 2") else Some("Payment on account 2 of 2")
     case (Some("TRM New Charge") | Some("TRM Amend Charge"),_ ) => if (latePaymentCharge) Some("Late payment interest for remaining balance") else Some("Remaining balance")
