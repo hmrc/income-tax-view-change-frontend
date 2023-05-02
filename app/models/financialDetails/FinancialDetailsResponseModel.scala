8a65b08d6d22c0543270fe56dfab2a3f197ca01f
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2023-05-02 11:15:18 +0100

    removed calls to path: financialDetails[].items[].dueDate

--- app/models/financialDetails/FinancialDetailsResponseModel.scala
+++ app/models/financialDetails/FinancialDetailsResponseModel.scala
@@ -35,7 +35,7 @@ case class FinancialDetailsModel(balanceDetails: BalanceDetails,
       financialDetails.find { fd =>
         fd.transactionId.contains(documentDetail.transactionId) &&
           fd.taxYear.toInt == documentDetail.taxYear
-      } flatMap (_.items.flatMap(_.headOption.flatMap(_.dueDate)))
+      }.flatMap(_ => documentDetail.effectiveDateOfPayment)
     }
   }
 
