d96fb44f2eec7c51a90341868e5f0d42658ee2c7
tharunprasath <65491423+tharunprasath@users.noreply.github.com>
2023-05-09 10:11:01 +0100

    MISUV-5438 TxM event - Forecast Income page

---
+++ app/audit/models/ForecastIncomeAuditModel.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package audit.models
+
+import audit.Utilities.userAuditDetails
+import auth.MtdItUserWithNino
+import implicits.ImplicitDateParser
+import models.liabilitycalculation.{EndOfYearEstimate, IncomeSource}
+import play.api.libs.json.{JsNumber, JsObject, JsValue, Json}
+import utils.Utilities._
+
+
+case class ForecastIncomeAuditModel(user: MtdItUserWithNino[_], endOfYearEstimate: EndOfYearEstimate)
+  extends ExtendedAuditModel with ImplicitDateParser {
+
+  override val transactionName: String = enums.TransactionName.ForecastIncome
+  override val auditType: String = enums.AuditType.ForecastIncome
+
+  private val totalEstimatedIncome: Option[Int] = endOfYearEstimate.totalEstimatedIncome
+  private val incomeSource: Option[List[IncomeSource]] = endOfYearEstimate.incomeSource
+
+  private var allOtherIncomeType: JsObject = Json.obj()
+  private var profitFromIncomeType: List[JsObject] = List()
+  private var payFromIncomeType: List[JsObject] = List()
+
+  val incomeTypeValues = Map(
+    "02" -> "profitFromUKLandandProperty",
+    "03" -> "profitfromEEAholidayPropertyLettings",
+    "04" -> "profitFromUKFurnishedHolidayLettings",
+    "06" -> "foreignIncome",
+    "07" -> "dividentsFromForeignCompanies",
+    "08" -> "profitsFromTrustsAndEstates",
+    "09" -> "Interest from UK banks, building societies and securities",
+    "10" -> "Dividends from UK companies",
+    "11" -> "stateBenefitIncome",
+    "12" -> "Gains on life insurance policies",
+    "13" -> "Share schemes",
+    "14" -> "profitFromPartnership",
+    "15" -> "Profit from foreign land and property",
+    "16" -> "foreignInterest",
+    "17" -> "Other dividends",
+    "18" -> "UK securities",
+    "19" -> "otherIncome",
+    "20" -> "foreignPension",
+    "21" -> "nonPayeIncome",
+    "22" -> "capitalGains",
+    "98" -> "giftAidAndPayrollGiving"
+  )
+
+  incomeSource.map(incomeSources => {
+    incomeSources.map(incomeSource => {
+      val amount = incomeSource.taxableIncome
+      incomeSource.incomeSourceType match {
+        case "01" =>
+          val incomeType = incomeSource.incomeSourceName.getOrElse("self-employment")
+          profitFromIncomeType = profitFromIncomeType.appended(Json.obj("name" -> incomeType , "amount" -> amount))
+        case "05" =>
+          val incomeType = incomeSource.incomeSourceName.getOrElse("employment")
+          payFromIncomeType = payFromIncomeType.appended(Json.obj("name" -> incomeType , "amount" -> amount))
+        case _ => allOtherIncomeType = allOtherIncomeType + (incomeTypeValues.getOrElse(incomeSource.incomeSourceType , "") -> JsNumber(amount))
+      }
+    })
+  })
+
+  override val detail: JsValue = {
+    userAuditDetails(user) ++
+      Json.obj() ++
+      ("totalEstimatedIncome", totalEstimatedIncome) ++
+      Json.obj("profitFrom" -> profitFromIncomeType) ++
+      Json.obj("payFrom" -> payFromIncomeType) ++
+      allOtherIncomeType
+  }
+
+}
