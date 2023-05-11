6dc2861b457c99f728471cd63534ea49b8073093
Tharun Prasath <65491423+tharunprasath@users.noreply.github.com>
2023-05-11 16:23:28 +0100

    Misuv 5467 TxM event forecast income page (#1709)
    * MISUV-5438 TxM event - Forecast Income page
    
    * MISUV-5438 JSON key name changes.
    
    * MISUV-5438 JSON key name changes and model test cases
    
    * MISUV-5438 Unused imports

--- it/controllers/ForecastIncomeSummaryControllerISpec.scala
+++ it/controllers/ForecastIncomeSummaryControllerISpec.scala
@@ -16,19 +16,62 @@
 
 package controllers
 
+import audit.models.ForecastIncomeAuditModel
+import auth.MtdItUserWithNino
 import config.featureswitch.ForecastCalculation
 import helpers.ComponentSpecBase
 import helpers.servicemocks._
+import models.liabilitycalculation.{EndOfYearEstimate, IncomeSource}
 import play.api.http.Status._
+import play.api.test.FakeRequest
 import testConstants.BaseIntegrationTestConstants._
 import testConstants.NewCalcBreakdownItTestConstants.liabilityCalculationModelSuccessful
+import uk.gov.hmrc.auth.core.AffinityGroup.Individual
+
+object ForecastIncomeSummaryControllerTestConstants {
+  val mtdItUser: MtdItUserWithNino[_] = MtdItUserWithNino(testMtditid, testNino, None, None, Some("1234567890"),
+    Some("12345-credId"), Some(Individual), None)(FakeRequest())
+
+  val taxableIncome = 12500
+
+  val endOfYearEstimate: EndOfYearEstimate = EndOfYearEstimate(
+    incomeSource = Some(List(
+      IncomeSource("01", Some("self-employment1"), taxableIncome),
+      IncomeSource("01", Some("self-employment2"), taxableIncome),
+      IncomeSource("02", None, taxableIncome),
+      IncomeSource("03", None, taxableIncome),
+      IncomeSource("04", None, taxableIncome),
+      IncomeSource("05", Some("employment1"), taxableIncome),
+      IncomeSource("05", Some("employment2"), taxableIncome),
+      IncomeSource("06", None, taxableIncome),
+      IncomeSource("07", None, taxableIncome),
+      IncomeSource("08", None, taxableIncome),
+      IncomeSource("09", None, taxableIncome),
+      IncomeSource("10", None, taxableIncome),
+      IncomeSource("11", None, taxableIncome),
+      IncomeSource("12", None, taxableIncome),
+      IncomeSource("13", None, taxableIncome),
+      IncomeSource("14", None, taxableIncome),
+      IncomeSource("15", None, taxableIncome),
+      IncomeSource("16", None, taxableIncome),
+      IncomeSource("17", None, taxableIncome),
+      IncomeSource("18", None, taxableIncome),
+      IncomeSource("19", None, taxableIncome),
+      IncomeSource("20", None, taxableIncome),
+      IncomeSource("21", None, taxableIncome),
+      IncomeSource("22", None, taxableIncome),
+      IncomeSource("98", None, taxableIncome)
+    )),
+    totalEstimatedIncome = Some(taxableIncome),
+  )
+}
 
 class ForecastIncomeSummaryControllerISpec extends ComponentSpecBase {
 
   "Calling the ForecastIncomeSummaryController.show(taxYear)" when {
 
     "isAuthorisedUser with an active enrolment, valid nino and tax year, valid CalcDisplayModel response, " should {
-      "return the correct forecast income summary page" in {
+      "return the correct forecast income summary page and audit event" in {
 
         Given("I enable forecast calculation display")
         enable(ForecastCalculation)
@@ -43,6 +86,9 @@ class ForecastIncomeSummaryControllerISpec extends ComponentSpecBase {
         val res = IncomeTaxViewChangeFrontend.getForecastIncomeSummary(testYear)
 
         IncomeTaxCalculationStub.verifyGetCalculationResponse(testNino, "2018")
+
+        AuditStub.verifyAuditEvent(ForecastIncomeAuditModel(ForecastIncomeSummaryControllerTestConstants.mtdItUser,
+          ForecastIncomeSummaryControllerTestConstants.endOfYearEstimate))
 
         res should have(
           httpStatus(OK),
