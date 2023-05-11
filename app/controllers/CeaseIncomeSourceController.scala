f35ca3ee53e46f4eff335eef85db1a2c1cffcb9e
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2023-05-11 15:30:24 +0100

    moved routes and created show and handle request methods

--- app/controllers/CeaseIncomeSourceController.scala
+++ app/controllers/CeaseIncomeSourceController.scala
@@ -16,19 +16,22 @@
 
 package controllers
 
-import auth.FrontendAuthorisedFunctions
+import auth.{FrontendAuthorisedFunctions, MtdItUser}
 import config.{AgentItvcErrorHandler, FrontendAppConfig}
 import config.featureswitch.FeatureSwitching
 import controllers.agent.predicates.ClientConfirmedController
-import controllers.predicates.{AuthenticationPredicate, SessionTimeoutPredicate}
+import controllers.predicates.{AuthenticationPredicate, IncomeSourceDetailsPredicate, NavBarPredicate, NinoPredicate, SessionTimeoutPredicate}
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
 
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class CeaseIncomeSourceController @Inject()(val authenticate: AuthenticationPredicate,
                                             val authorisedFunctions: FrontendAuthorisedFunctions,
+                                            val retrieveNino: NinoPredicate,
+                                            val retrieveBtaNavBar: NavBarPredicate,
+                                            val retrieveIncomeSources: IncomeSourceDetailsPredicate,
                                             val checkSessionTimeout: SessionTimeoutPredicate)
                                            (implicit val appConfig: FrontendAppConfig,
                                             mcc: MessagesControllerComponents,
@@ -36,11 +39,19 @@ class CeaseIncomeSourceController @Inject()(val authenticate: AuthenticationPred
                                             val itvcErrorHandlerAgent: AgentItvcErrorHandler)
   extends ClientConfirmedController with FeatureSwitching with I18nSupport {
 
-  def show(): Action[AnyContent] = Action {
-    Ok("")
+  def show(): Action[AnyContent] = (checkSessionTimeout andThen authenticate andThen retrieveNino
+    andThen retrieveIncomeSources andThen retrieveBtaNavBar).async {
+    implicit user =>
+      handleRequest()
   }
 
   def showAgent(): Action[AnyContent] = Action {
     Ok("")
+  }
+
+  def handleRequest()(implicit user: MtdItUser[_], ec: ExecutionContext): Future[Result] = {
+    Future {
+      Ok("Hello World")
+    }
   }
 }
