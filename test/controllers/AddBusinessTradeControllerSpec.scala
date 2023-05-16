641a80b4541286199f1cd5992f6dd8df5e2bc8df
danielmaltz1 <117353102+danielmaltz1@users.noreply.github.com>
2023-05-16 14:41:52 +0100

    Began work on testing

---
+++ test/controllers/AddBusinessTradeControllerSpec.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.featureswitch.FeatureSwitch.switches
+import config.featureswitch.FeatureSwitching
+import mocks.MockItvcErrorHandler
+import mocks.auth.MockFrontendAuthorisedFunctions
+import mocks.controllers.predicates.{MockAuthenticationPredicate, MockIncomeSourceDetailsPredicate, MockNavBarEnumFsPredicate}
+import mocks.services.MockClientDetailsService
+import testUtils.TestSupport
+
+
+
+class AddBusinessTradeControllerSpec extends TestSupport
+  with MockFrontendAuthorisedFunctions
+  with MockIncomeSourceDetailsPredicate
+  with MockAuthenticationPredicate
+  with MockItvcErrorHandler
+  with MockNavBarEnumFsPredicate
+  with MockClientDetailsService
+  with FeatureSwitching {
+
+
+  def disableAllSwitches(): Unit = {
+    switches.foreach(switch => disable(switch))
+  }
+
+}
