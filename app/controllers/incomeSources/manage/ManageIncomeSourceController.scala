2cc5659e927fc97604ae1586f5cb29cd6c269311
jordhemsworth <88255447+JordHemsworth@users.noreply.github.com>
2023-05-18 13:25:11 +0100

    MISUV-5560 - Created View & Controller, added Eng

--- app/controllers/incomeSources/manage/ManageIncomeSourceController.scala
+++ app/controllers/incomeSources/manage/ManageIncomeSourceController.scala
@@ -16,31 +16,85 @@
 
 package controllers.incomeSources.manage
 
-import auth.FrontendAuthorisedFunctions
-import config.featureswitch.FeatureSwitching
-import config.{AgentItvcErrorHandler, FrontendAppConfig}
+import auth.MtdItUser
+import config.featureswitch.{FeatureSwitching, IncomeSources}
+import config.{AgentItvcErrorHandler, FrontendAppConfig, ItvcErrorHandler}
 import controllers.agent.predicates.ClientConfirmedController
-import controllers.predicates.{AuthenticationPredicate, SessionTimeoutPredicate}
-import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import controllers.predicates.{AuthenticationPredicate, IncomeSourceDetailsPredicate, NavBarPredicate, NinoPredicate, SessionTimeoutPredicate}
+import models.incomeSourceDetails.IncomeSourceDetailsModel
+import play.api.Logger
+import play.api.mvc._
+import services.IncomeSourceDetailsService
+import uk.gov.hmrc.auth.core.AuthorisedFunctions
+import views.html.incomeSources.manage.ManageIncomeSources
 
-import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
 
-class ManageIncomeSourceController @Inject()(val authenticate: AuthenticationPredicate,
-                                             val authorisedFunctions: FrontendAuthorisedFunctions,
-                                             val checkSessionTimeout: SessionTimeoutPredicate)
-                                            (implicit val appConfig: FrontendAppConfig,
-                                             mcc: MessagesControllerComponents,
-                                             val ec: ExecutionContext,
-                                             val itvcErrorHandlerAgent: AgentItvcErrorHandler)
-  extends ClientConfirmedController with FeatureSwitching with I18nSupport {
+@Singleton
+class ManageIncomeSourceController @Inject()(val manageIncomeSources: ManageIncomeSources,
+                                          val checkSessionTimeout: SessionTimeoutPredicate,
+                                          val authenticate: AuthenticationPredicate,
+                                          val authorisedFunctions: AuthorisedFunctions,
+                                          val retrieveNino: NinoPredicate,
+                                          val retrieveIncomeSources: IncomeSourceDetailsPredicate,
+                                          val itvcErrorHandler: ItvcErrorHandler,
+                                          implicit val itvcErrorHandlerAgent: AgentItvcErrorHandler,
+                                          val incomeSourceDetailsService: IncomeSourceDetailsService,
+                                          val retrieveBtaNavBar: NavBarPredicate)
+                                         (implicit val ec: ExecutionContext,
+                                          implicit override val mcc: MessagesControllerComponents,
+                                          val appConfig: FrontendAppConfig) extends ClientConfirmedController
+  with FeatureSwitching {
 
-  def show(): Action[AnyContent] = Action {
-    Ok("")
+  def show(): Action[AnyContent] = (checkSessionTimeout andThen authenticate andThen retrieveNino
+    andThen retrieveIncomeSources andThen retrieveBtaNavBar).async {
+    implicit user =>
+      handleRequest(
+        sources = user.incomeSources,
+        isAgent = false,
+        backUrl = controllers.routes.HomeController.show().url
+      )
   }
 
-  def showAgent(): Action[AnyContent] = Action {
-    Ok("")
+  def showAgent(): Action[AnyContent] = Authenticated.async {
+    implicit request =>
+      implicit user =>
+        getMtdItUserWithIncomeSources(incomeSourceDetailsService) flatMap {
+          implicit mtdItUser =>
+            handleRequest(
+              sources = mtdItUser.incomeSources,
+              isAgent = true,
+              backUrl = controllers.routes.HomeController.showAgent.url
+            )
+        }
+  }
+
+  def handleRequest(sources: IncomeSourceDetailsModel, isAgent: Boolean, backUrl: String)
+                   (implicit user: MtdItUser[_]): Future[Result] = {
+    Future.successful(
+      if (isDisabled(IncomeSources)) {
+        Redirect(controllers.routes.HomeController.show())
+      } else {
+        incomeSourceDetailsService.getAddIncomeSourceViewModel(sources) match {
+          case Right(viewModel) =>
+            Ok(manageIncomeSources(
+              viewModel,
+              isAgent = isAgent,
+              backUrl = backUrl
+            ))
+          case Left(ex) =>
+            if (isAgent) {
+              Logger("application").error(
+                s"[Agent][AddIncomeSourceController][handleRequest] - Error: ${ex.getMessage}")
+              itvcErrorHandlerAgent.showInternalServerError()
+            } else {
+              Logger("application").error(
+                s"[AddIncomeSourceController][handleRequest] - Error: ${ex.getMessage}")
+              itvcErrorHandler.showInternalServerError()
+            }
+        }
+      }
+    )
   }
 }
