113518236d352a226d20fc638e2f2d2b13a8ab3c
danielmaltz1 <117353102+danielmaltz1@users.noreply.github.com>
2023-06-26 10:55:33 +0100

    Added banner text

--- app/connectors/AddressLookupConnector.scala
+++ app/connectors/AddressLookupConnector.scala
@@ -51,8 +51,13 @@ class AddressLookupConnector @Inject()(val appConfig: FrontendAppConfig,
   lazy val individualFeedbackUrl: String = controllers.feedback.routes.FeedbackController.show.url
   lazy val agentFeedbackUrl: String = controllers.feedback.routes.FeedbackController.showAgent.url
 
+  val individualEnglishBanner: String = messagesApi.preferred(Seq(Lang("en")))("header.serviceName")
+  val agentEnglishBanner: String = messagesApi.preferred(Seq(Lang("en")))("agent.header.serviceName")
 
-  def addressJson(continueUrl: String, feedbackUrl: String): JsValue = {
+  val individualWelshBanner: String = messagesApi.preferred(Seq(Lang("cy")))("header.serviceName")
+  val agentWelshBanner: String = messagesApi.preferred(Seq(Lang("cy")))("agent.header.serviceName")
+
+  def addressJson(continueUrl: String, feedbackUrl: String, headerEnglish: String, headerWelsh: String): JsValue = {
     JsObject(
       Seq(
         "version" -> JsNumber(2),
@@ -83,6 +88,7 @@ class AddressLookupConnector @Inject()(val appConfig: FrontendAppConfig,
             "phaseFeedbackLink" -> JsString(appConfig.itvcFrontendEnvironment + feedbackUrl),
             "deskProServiceName" -> JsString("cds-reimbursement-claim"),
             "showPhaseBanner" -> JsBoolean(true),
+            "phaseBannerHtml" -> JsString("CLIENT"),
             "ukMode" -> JsBoolean(true)
           )
         ),
@@ -90,6 +96,11 @@ class AddressLookupConnector @Inject()(val appConfig: FrontendAppConfig,
           Seq(
             "en" -> JsObject(
               Seq(
+                "appLevelLabels" -> JsObject(
+                  Seq(
+                    "navTitle" -> JsString(headerEnglish),
+                  )
+                ),
                 "selectPageLabels" -> JsObject(
                   Seq(
                     "heading" -> JsString(messagesApi.preferred(Seq(Lang("en")))("add-business-address.select.heading"))
@@ -114,6 +125,11 @@ class AddressLookupConnector @Inject()(val appConfig: FrontendAppConfig,
             ),
             "cy" -> JsObject(
               Seq(
+                "appLevelLabels" -> JsObject(
+                  Seq(
+                    "navTitle" -> JsString(headerWelsh)
+                  )
+                ),
                 "selectPageLabels" -> JsObject(
                   Seq(
                     "heading" -> JsString(messagesApi.preferred(Seq(Lang("cy")))("add-business-address.select.heading"))
@@ -145,7 +161,8 @@ class AddressLookupConnector @Inject()(val appConfig: FrontendAppConfig,
 
   def initialiseAddressLookup(isAgent: Boolean)(implicit hc: HeaderCarrier, request: RequestHeader): Future[PostAddressLookupResponse] = {
     Logger("application").info(s"[AddressLookupConnector] - URL: $addressLookupInitializeUrl")
-    val payload = if (isAgent) addressJson(agentContinueUrl, agentFeedbackUrl) else addressJson(individualContinueUrl, individualFeedbackUrl)
+    val payload = if (isAgent) addressJson(agentContinueUrl, agentFeedbackUrl, agentEnglishBanner, agentWelshBanner)
+      else addressJson(individualContinueUrl, individualFeedbackUrl, individualEnglishBanner, individualWelshBanner)
     Logger("application").info(s"AddressLookupPayload: $payload")
     http.POST[JsValue, PostAddressLookupResponse](
       url = addressLookupInitializeUrl,
