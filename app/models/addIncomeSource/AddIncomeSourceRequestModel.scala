72437f8be5c7720075c846c2b95c215579aff94a
Pavel <3528155+avelx@users.noreply.github.com>
2023-07-03 10:28:52 +0100

    MISUV-5768: FE - 1566 connector (#1782)
    * MISUV-5768:
      * add required connector and models;
    
    * MISUV-5768:
      * add required connector and models [WIP];
    
    * MISUV-5768:
      * add minimum test coverage;
    
    * MISUV-5768:
      * WIP ...
    
    * MISUV-5768:
      * WIP: add more unit tests
    
    * MISUV-5768:
      * refactoring + add unit tests for the service;
    
    * MISUV-5768:
      * tidy up;
    
    * MISUV-5768:
      * tidy up;
    
    * MISUV-5768:
      * tidy up [v3];
    
    * MISUV-5768:
      * tidy up [v4];
    
    * MISUV-5768:
      * fix broken test;
    
    * MISUV-5768:
      * code tidy up;
    
    * MISUV-5768:
      * fix broken unit tests
    
    * MISUV-5768:
      * integration tidy up
    
    * MISUV-5768:
      * integration tidy up [v7]
    
    * MISUV-5768:
      * integration tidy up [v8]
    
    * MISUV-5768:
      * integration tidy up [v8]
    
    * MISUV-5768:
      * integration tidy up [v8]
    
    * MISUV-5768:
      * re-enable CSRF;
      * add empty form
      * update tests respectively

---
+++ app/models/addIncomeSource/AddIncomeSourceRequestModel.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.addIncomeSource
+
+import play.api.libs.json.{Format, Json}
+
+import java.time.LocalDate
+
+sealed trait AddIncomeSourceRequest
+
+case class AddBusinessIncomeSourcesRequest(businessDetails: List[BusinessDetails]) extends AddIncomeSourceRequest
+
+
+case class BusinessDetails(accountingPeriodStartDate: String,
+                           accountingPeriodEndDate: String,
+                           tradingName: String,
+                           addressDetails: AddressDetails,
+                           typeOfBusiness: Option[String],
+                           tradingStartDate: String,
+                           cashOrAccrualsFlag: String,
+                           cessationDate: Option[String],
+                           cessationReason: Option[String]
+                          )
+
+case class AddressDetails(addressLine1: String,
+                          addressLine2: Option[String],
+                          addressLine3: Option[String],
+                          addressLine4: Option[String],
+                          countryCode: Option[String],
+                          postalCode: Option[String]
+                         )
+
+object AddBusinessIncomeSourcesRequest {
+  implicit val format: Format[AddBusinessIncomeSourcesRequest] = Json.format
+}
+
+object BusinessDetails {
+  implicit val format: Format[BusinessDetails] = Json.format
+}
+
+object AddressDetails {
+  implicit val format: Format[AddressDetails] = Json.format
+}
--- it/controllers/incomeSources/add/CheckBusinessDetailsControllerISpec.scala
+++ it/controllers/incomeSources/add/CheckBusinessDetailsControllerISpec.scala
@@ -1,14 +1,15 @@
 package controllers.incomeSources.add
 
 import config.featureswitch.IncomeSources
-import forms.utils.SessionKeys.{addBusinessAccountingMethod, addBusinessAddressLine1, addBusinessPostalCode, businessName, businessStartDate, businessTrade}
+import forms.utils.SessionKeys.{addBusinessAccountingMethod, addBusinessAccountingPeriodEndDate, addBusinessAddressLine1, addBusinessPostalCode, businessName, businessStartDate, businessTrade}
 import helpers.ComponentSpecBase
 import helpers.servicemocks.IncomeTaxViewChangeStub
+import models.addIncomeSource.AddIncomeSourceResponse
 import play.api.http.Status.{BAD_REQUEST, OK, SEE_OTHER}
 import testConstants.BaseIntegrationTestConstants.testMtditid
-import testConstants.IncomeSourceIntegrationTestConstants.{noPropertyOrBusinessResponse}
+import testConstants.IncomeSourceIntegrationTestConstants.noPropertyOrBusinessResponse
 
-class CheckBusinessDetailsControllerISpec extends ComponentSpecBase{
+class CheckBusinessDetailsControllerISpec extends ComponentSpecBase {
 
   val checkBusinessDetailsShowUrl: String = controllers.incomeSources.add.routes.CheckBusinessDetailsController.show().url
   val checkBusinessDetailsSubmitUrl: String = controllers.incomeSources.add.routes.CheckBusinessDetailsController.submit().url
@@ -19,7 +20,9 @@ class CheckBusinessDetailsControllerISpec extends ComponentSpecBase{
                                             businessTrade -> "Plumbing",
                                             addBusinessAddressLine1 -> "Test Road",
                                             addBusinessPostalCode -> "B32 1PQ",
-                                            addBusinessAccountingMethod -> "Quarterly")
+                                            addBusinessAccountingMethod -> "Quarterly",
+                                            addBusinessAccountingPeriodEndDate -> "2023-11-11")
+
   val testBusinessName: String = "Test Business"
   val testBusinessStartDate: String = "1 January 2022"
   val testBusinessTrade: String = "Plumbing"
@@ -37,9 +40,11 @@ class CheckBusinessDetailsControllerISpec extends ComponentSpecBase{
         enable(IncomeSources)
         IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, noPropertyOrBusinessResponse)
 
+        val response = List(AddIncomeSourceResponse("123"))
+        IncomeTaxViewChangeStub.stubCreateBusinessDetailsResponse(testMtditid)(OK, response)
+
         When(s"I call GET $checkBusinessDetailsShowUrl")
         val result = IncomeTaxViewChangeFrontend.get("/income-sources/add/business-check-details", sessionData)
-        println("LOOOOK" + result)
 
         result should have(
           httpStatus(OK),
@@ -60,12 +65,15 @@ class CheckBusinessDetailsControllerISpec extends ComponentSpecBase{
       "user selects 'confirm and continue'" in {
         val formData: Map[String,Seq[String]] = Map("addBusinessName" -> Seq("Test Business Name"),
           "addBusinessTrade" -> Seq("Test Business Name"),
-          "addBusinessStartDate" -> Seq("Test Business Name"),
+          "addBusinessStartDate" -> Seq("2011-11-11"),
           "addBusinessAddressLine1" -> Seq("Test Business Name"),
-          "addBusinessPostalCode" -> Seq("Test Business Name"),
-          "addBusinessAccountingMethod" -> Seq("Test Business Name"))
+          "addBusinessPostalCode" -> Seq("SE15 1WR"),
+          "addBusinessAccountingMethod" -> Seq("Test Business Name"),
+          "addBusinessAccountingPeriodEndDate" -> Seq("2023-11-11"))
         enable(IncomeSources)
+        val response = List(AddIncomeSourceResponse("123"))
         IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, noPropertyOrBusinessResponse)
+        IncomeTaxViewChangeStub.stubCreateBusinessDetailsResponse(testMtditid)(OK, response)
 
         val result = IncomeTaxViewChangeFrontend.post(s"/income-sources/add/business-check-details", sessionData)(formData)
 
