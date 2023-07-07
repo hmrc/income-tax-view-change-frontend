1ae25c8357277be97801a8fcd012c514442246b5
danielmaltz1 <117353102+danielmaltz1@users.noreply.github.com>
2023-07-07 15:53:07 +0100

    Small fix

--- app/controllers/incomeSources/add/BusinessAddedObligationsController.scala
+++ app/controllers/incomeSources/add/BusinessAddedObligationsController.scala
@@ -83,7 +83,7 @@ class BusinessAddedObligationsController @Inject()(authenticate: AuthenticationP
       business match {
         case None => Logger("application").error(
           s"[BusinessAddedObligationsController][handleRequest] - No business with provided id")
-          itvcErrorHandler.showInternalServerError()
+          Future(itvcErrorHandler.showInternalServerError())
         case Some(addedBusiness) =>
           if (addedBusiness.tradingName.isEmpty) {
             Logger("application").error(
@@ -101,21 +101,22 @@ class BusinessAddedObligationsController @Inject()(authenticate: AuthenticationP
           obligationsService.getObligationDates(id) map {
             datesList: Seq[DatesModel] =>
 
-            val (finalDeclarationDates, otherObligationDates) = datesList.partition(x => x.isFinalDec)
+              val (finalDeclarationDates, otherObligationDates) = datesList.partition(x => x.isFinalDec)
 
-            val quarterlyDates: Seq[DatesModel] = otherObligationDates.filter(x => x.periodKey.contains("00")).sortBy(_.inboundCorrespondenceFrom)
-            val quarterlyDatesByYear: (Seq[DatesModel], Seq[DatesModel]) = quarterlyDates.partition(x => dateService.getAccountingPeriodEndDate(x.inboundCorrespondenceTo) == dateService.getAccountingPeriodEndDate(quarterlyDates.head.inboundCorrespondenceTo))
-            val quarterlyDatesYearOne = quarterlyDatesByYear._1.sortBy(_.periodKey)
-            val quarterlyDatesYearTwo = quarterlyDatesByYear._2.sortBy(_.periodKey)
+              val quarterlyDates: Seq[DatesModel] = otherObligationDates.filter(x => x.periodKey.contains("00")).sortBy(_.inboundCorrespondenceFrom)
+              val quarterlyDatesByYear: (Seq[DatesModel], Seq[DatesModel]) = quarterlyDates.partition(x => dateService.getAccountingPeriodEndDate(x.inboundCorrespondenceTo) == dateService.getAccountingPeriodEndDate(quarterlyDates.head.inboundCorrespondenceTo))
+              val quarterlyDatesYearOne = quarterlyDatesByYear._1.sortBy(_.periodKey)
+              val quarterlyDatesYearTwo = quarterlyDatesByYear._2.sortBy(_.periodKey)
 
-            val eopsDates: Seq[DatesModel] = otherObligationDates.filter(x => x.periodKey == "EOPS")
+              val eopsDates: Seq[DatesModel] = otherObligationDates.filter(x => x.periodKey == "EOPS")
 
-            val showPreviousTaxYears: Boolean = startDate.isBefore(dateService.getCurrentTaxYearStart())
+              val showPreviousTaxYears: Boolean = startDate.isBefore(dateService.getCurrentTaxYearStart())
 
-            if (isAgent) Ok(obligationsView(businessName, ObligationsViewModel(quarterlyDatesYearOne, quarterlyDatesYearTwo, eopsDates, finalDeclarationDates, dateService.getCurrentTaxYearEnd(), showPrevTaxYears = showPreviousTaxYears),
-              controllers.incomeSources.add.routes.BusinessAddedObligationsController.agentSubmit(), agentBackUrl, isAgent = true))
-            else Ok(obligationsView(businessName, ObligationsViewModel(quarterlyDatesYearOne, quarterlyDatesYearTwo, eopsDates, finalDeclarationDates, dateService.getCurrentTaxYearEnd(), showPrevTaxYears = showPreviousTaxYears),
-              controllers.incomeSources.add.routes.BusinessAddedObligationsController.submit(), backUrl, isAgent = false))
+              if (isAgent) Ok(obligationsView(businessName, ObligationsViewModel(quarterlyDatesYearOne, quarterlyDatesYearTwo, eopsDates, finalDeclarationDates, dateService.getCurrentTaxYearEnd(), showPrevTaxYears = showPreviousTaxYears),
+                controllers.incomeSources.add.routes.BusinessAddedObligationsController.agentSubmit(), agentBackUrl, isAgent = true))
+              else Ok(obligationsView(businessName, ObligationsViewModel(quarterlyDatesYearOne, quarterlyDatesYearTwo, eopsDates, finalDeclarationDates, dateService.getCurrentTaxYearEnd(), showPrevTaxYears = showPreviousTaxYears),
+                controllers.incomeSources.add.routes.BusinessAddedObligationsController.submit(), backUrl, isAgent = false))
+          }
       }
     }
   }
