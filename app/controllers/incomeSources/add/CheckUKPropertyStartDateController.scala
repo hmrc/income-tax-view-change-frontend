e8a80d2a866d1660b66fd240e8514720526ed835
Tharun Prasath <65491423+tharunprasath@users.noreply.github.com>
2023-07-10 15:55:43 +0100

    Misuv 5803 uk property business accounting method (#1791)
    * MISUV-5107 SCP025 | Add a UK property | Build - UK property Business Accounting Method page
    
    * MISUV-5107 Review comments changes.

--- app/controllers/incomeSources/add/CheckUKPropertyStartDateController.scala
+++ app/controllers/incomeSources/add/CheckUKPropertyStartDateController.scala
@@ -125,7 +125,7 @@ class CheckUKPropertyStartDateController @Inject()(val authenticate: Authenticat
             Future.successful(Redirect(controllers.incomeSources.add.routes.AddUKPropertyStartDateController.show())
               .removingFromSession("addUkPropertyStartDate"))
           } else {
-            Future.successful(Redirect(controllers.incomeSources.add.routes.UKPropertyAccountingMethod.show()))
+            Future.successful(Redirect(controllers.incomeSources.add.routes.UKPropertyBusinessAccountingMethodController.show()))
           }
         }
       )
@@ -152,7 +152,7 @@ class CheckUKPropertyStartDateController @Inject()(val authenticate: Authenticat
                   Future.successful(Redirect(controllers.incomeSources.add.routes.AddUKPropertyStartDateController.showAgent())
                     .removingFromSession("addUkPropertyStartDate"))
                 } else {
-                  Future.successful(Redirect(controllers.incomeSources.add.routes.UKPropertyAccountingMethod.showAgent()))
+                  Future.successful(Redirect(controllers.incomeSources.add.routes.UKPropertyBusinessAccountingMethodController.showAgent()))
                 }
               }
             )
