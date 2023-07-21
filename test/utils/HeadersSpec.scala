6bfa155f515fb3ac86956eafbd43c66fa3cd382a
Tharun Prasath <65491423+tharunprasath@users.noreply.github.com>
2023-07-21 10:18:23 +0100

    Misuv 5914 dynamic stub creation (#1809)
    * MISUV-5516 SCP025 | Add UK property | Build - Select reporting method
    
    * MISUV-5516 Review comment changes
    
    * MISUV-5914 Dynamic Stub Creation
    
    * MISUV-5914 Dynamic Stub Creation
    
    * MISUV-5914 Review comment changes

---
+++ test/utils/HeadersSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import testUtils.TestSupport
+import utils.Headers.checkAndAddTestHeader
+
+class HeadersSpec extends TestSupport {
+
+  val ukSelectReportingMethod: String = "uk-property-reporting-method"
+  val incomeSourceCreatedJourney: String = "afterIncomeSourceCreated"
+  val testHeader: String = "Gov-Test-Scenario"
+
+  "Return updated Gov-Test-Scenario headers" when {
+    "the action scenario matches the govUKTestHeaderValuesMap" in {
+        val updatedHeaders = checkAndAddTestHeader(ukSelectReportingMethod, headerCarrier)
+        val hc = headerCarrier.withExtraHeaders(testHeader -> incomeSourceCreatedJourney)
+        updatedHeaders shouldBe hc
+    }
+  }
+
+  "Return empty Gov-Test-Scenario headers" when {
+    "the action scenario does not matches the govUKTestHeaderValuesMap" in {
+      val updatedHeaders = checkAndAddTestHeader("otherAction", headerCarrier)
+      val hc = headerCarrier.withExtraHeaders(testHeader -> "")
+      updatedHeaders shouldBe hc
+    }
+  }
+
+}
