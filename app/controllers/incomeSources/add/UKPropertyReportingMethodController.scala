ecf2056ff6cb4a158d77ed49265219193c430b51
tharunprasath <65491423+tharunprasath@users.noreply.github.com>
2023-07-21 15:10:32 +0100

    MISUV-5516 Redirect to error page upon failure

--- app/controllers/incomeSources/add/UKPropertyReportingMethodController.scala
+++ app/controllers/incomeSources/add/UKPropertyReportingMethodController.scala
@@ -63,7 +63,7 @@ class UKPropertyReportingMethodController @Inject()(val authenticate: Authentica
     case _ => None
   }
   private def getUKPropertyReportingMethodDetails(incomeSourceId: String)(implicit user: MtdItUser[_], hc: HeaderCarrier, ec: ExecutionContext): Future[Option[UKPropertyReportingMethodViewModel]] = {
-    val latencyDetails: Option[LatencyDetails] = user.incomeSources.properties.find(_.incomeSourceId.getOrElse("").equals(incomeSourceId)).flatMap(_.latencyDetails)
+    val latencyDetails: Option[LatencyDetails] = user.incomeSources.properties.filter(_.isUkProperty).find(_.incomeSourceId.getOrElse("").equals(incomeSourceId)).flatMap(_.latencyDetails)
     latencyDetails match {
       case Some(x) =>
         val currentTaxYearEnd = dateService.getCurrentTaxYearEnd(isEnabled(TimeMachineAddYear))
@@ -124,6 +124,7 @@ class UKPropertyReportingMethodController @Inject()(val authenticate: Authentica
     val incomeSourcesEnabled: Boolean = isEnabled(IncomeSources)
     val errorHandler: ShowInternalServerError = if (isAgent) itvcErrorHandlerAgent else itvcErrorHandler
     val redirectUrl: Call = if (isAgent) routes.UKPropertyAddedController.showAgent(id) else routes.UKPropertyAddedController.show(id)
+    val redirectErrorUrl: Call = if (isAgent) routes.UKPropertyReportingMethodErrorController.showAgent() else routes.UKPropertyReportingMethodErrorController.show()
     val submitUrl: Call = if (isAgent) controllers.incomeSources.add.routes.UKPropertyReportingMethodController.submitAgent(id) else
       controllers.incomeSources.add.routes.UKPropertyReportingMethodController.submit(id)
 
@@ -162,7 +163,7 @@ class UKPropertyReportingMethodController @Inject()(val authenticate: Authentica
                 Redirect(redirectUrl)
               case err: UpdateIncomeSourceResponseError =>
                 Logger("application").error(s"${if (isAgent) "[Agent]"}" + s" Failed to Updated tax year specific reporting method : $err")
-                errorHandler.showInternalServerError()
+                Redirect(redirectErrorUrl)
             }
           } else {
             Logger("application").info(s"${if (isAgent) "[Agent]"}" + s" Updating the tax year specific reporting method not required.")
