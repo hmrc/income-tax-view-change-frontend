912e1d89a09b93d801a2ae5ea850a31003efd5c9
Tharun Prasath <65491423+tharunprasath@users.noreply.github.com>
2023-08-09 13:52:28 +0100

    MISUV-6042 SCP0025 | Add | API#1171 - spec updates uk/foreign property (#1847)

--- it/testConstants/PropertyDetailsIntegrationTestConstants.scala
+++ it/testConstants/PropertyDetailsIntegrationTestConstants.scala
@@ -32,8 +32,8 @@ object PropertyDetailsIntegrationTestConstants {
   val propertyAccounringEndLocalDate = LocalDate.of(startYear, 12, 31)
   val propertyIncomeType = Some("property-unspecified")
   val propertyTradingStartDate = Some(LocalDate.parse((startYear - 1).toString + "-01-01"))
-  val ukPropertyIncomeType = Some("uk-property")
-  val foreignPropertyIncomeType = Some("foreign-property")
+  val ukPropertyIncomeType = Some("02-uk-property")
+  val foreignPropertyIncomeType = Some("03-foreign-property")
 
   def propertyAccountingStartLocalDateOfCurrentYear(year: Int) = LocalDate.of(year, 1, 1)
 
