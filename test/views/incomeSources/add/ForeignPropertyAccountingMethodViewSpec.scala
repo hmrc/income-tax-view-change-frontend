043f375ce526ae5ab11e6ab117d70928c2ff6e14
AyubAz <110174399+ayubAz@users.noreply.github.com>
2023-08-15 10:38:41 +0100

    un-comment required tests

--- test/views/incomeSources/add/ForeignPropertyAccountingMethodViewSpec.scala
+++ test/views/incomeSources/add/ForeignPropertyAccountingMethodViewSpec.scala
@@ -14,164 +14,148 @@
  * limitations under the License.
  */
 
-///*
-// * Copyright 2023 HM Revenue & Customs
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package views.incomeSources.add
-//
-//import auth.MtdItUser
-//import forms.incomeSources.add.ForeignPropertyAccountingMethodForm
-//import org.jsoup.Jsoup
-//import org.jsoup.nodes.Document
-//import play.api.data.Form
-//import play.api.mvc.Call
-//import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout}
-//import play.twirl.api.HtmlFormat
-//import services.DateService
-//import testConstants.BaseTestConstants.{testMtditid, testNino}
-//import testConstants.incomeSources.IncomeSourceDetailsTestConstants.noIncomeDetails
-//import testUtils.TestSupport
-//import uk.gov.hmrc.auth.core.AffinityGroup.Individual
-//import views.html.incomeSources.add.ForeignPropertyAccountingMethod
-//
-//class ForeignPropertyAccountingMethodViewSpec extends TestSupport {
-//  val foreignPropertyAccountingMethodView: ForeignPropertyAccountingMethod = app.injector.instanceOf[ForeignPropertyAccountingMethod]
-//
-//  val testUser: MtdItUser[_] = MtdItUser(
-//    mtditid = testMtditid,
-//    nino = testNino,
-//    userName = None,
-//    btaNavPartial = None,
-//    saUtr = None,
-//    credId = Some("12345-credId"),
-//    userType = Some(Individual),
-//    arn = None,
-//    incomeSources = noIncomeDetails
-//  )(fakeRequestCeaseUKPropertyDeclarationComplete)
-//
-//
-//  class Setup(isAgent: Boolean, error: Boolean = false) extends TestSupport {
-//
-//    val form: Form[_] = ForeignPropertyAccountingMethodForm.form
-//
-//    val backUrl: String = if (isAgent) controllers.incomeSources.add.routes.ForeignPropertyStartDateCheckController.showAgent().url else
-//      controllers.incomeSources.add.routes.ForeignPropertyStartDateCheckController.show().url
-//    val postAction: Call = if (isAgent) controllers.incomeSources.add.routes.ForeignPropertyAccountingMethodController.submitAgent() else
-//      controllers.incomeSources.add.routes.ForeignPropertyAccountingMethodController.submit()
-//
-//    val prefix: String = "incomeSources.add.foreignPropertyAccountingMethod"
-//
-//    lazy val view: HtmlFormat.Appendable = if (isAgent) {
-//      foreignPropertyAccountingMethodView(
-//        form,
-//        postAction = postAction,
-//        isAgent = true,
-//        backUrl = backUrl)
-//    } else {
-//      foreignPropertyAccountingMethodView(
-//        form = form,
-//        postAction = postAction,
-//        isAgent = false,
-//        backUrl = backUrl)
-//    }
-//
-//    lazy val viewWithInputErrors: HtmlFormat.Appendable = if (isAgent) {
-//      foreignPropertyAccountingMethodView(
-//        form = form.withError(s"$prefix", s"$prefix.no-selection"),
-//        postAction = postAction,
-//        isAgent = true,
-//        backUrl = controllers.incomeSources.add.routes.ForeignPropertyStartDateCheckController.showAgent().url)
-//
-//    } else {
-//      foreignPropertyAccountingMethodView(
-//        form = form.withError(s"$prefix", s"$prefix.no-selection"),
-//        postAction = postAction,
-//        isAgent = false,
-//        backUrl = controllers.incomeSources.add.routes.ForeignPropertyStartDateCheckController.show().url)
-//    }
-//
-//    lazy val document: Document = if (error) Jsoup.parse(contentAsString(viewWithInputErrors)) else Jsoup.parse(contentAsString(view))
-//  }
-//
-//  "ForeignPropertyAccountingMethod - Individual" should {
-//    "render the heading" in new Setup(false) {
-//      document.getElementsByClass("govuk-fieldset__legend").text() shouldBe messages(s"$prefix.heading")
-//    }
-//    "render the dropdown" in new Setup(false) {
-//      document.getElementsByClass("govuk-details__summary").text() shouldBe messages(s"$prefix.example")
-//      document.getElementsByClass("govuk-body").eq(0).text() shouldBe messages(s"$prefix.drop-down-text")
-//    }
-//
-//
-//    "render the radio form" in new Setup(false) {
-//      document.getElementsByClass("govuk-label govuk-radios__label").eq(0).text() shouldBe messages(s"$prefix.radio-1-title")
-//      document.getElementsByClass("govuk-label govuk-radios__label").eq(1).text() shouldBe messages(s"$prefix.radio-2-title")
-//      document.getElementsByClass("govuk-hint govuk-radios__hint govuk-hint govuk-radios__hint").eq(0).text() shouldBe messages(s"$prefix.radio-1-hint")
-//      document.getElementsByClass("govuk-hint govuk-radios__hint govuk-hint govuk-radios__hint").eq(1).text() shouldBe messages(s"$prefix.radio-2-hint")
-//      document.getElementsByClass("govuk-radios").size() shouldBe 1
-//    }
-//    "render the back link with the correct URL" in new Setup(false) {
-//      document.getElementById("back").text() shouldBe messages("base.back")
-//      document.getElementById("back").attr("href") shouldBe backUrl
-//    }
-//    "render the continue button" in new Setup(false) {
-//      document.getElementById("continue-button").text() shouldBe messages("base.continue")
-//    }
-//    "render the input error" in new Setup(false, true) {
-//      document.getElementById("incomeSources.add.foreignPropertyAccountingMethod-error").text() shouldBe messages("base.error-prefix") + " " +
-//        messages(s"$prefix.no-selection")
-//    }
-//    "render the error summary" in new Setup(false, true) {
-//      document.getElementById("error-summary-heading").text() shouldBe messages("base.error_summary.heading")
-//      document.getElementsByClass("govuk-list govuk-error-summary__list").text() shouldBe messages(s"$prefix.no-selection")
-//    }
-//    "render the back url" in new Setup(true, true) {
-//      document.getElementById("back").attr("href") shouldBe backUrl
-//    }
-//  }
-//
-//  "ForeignPropertyAccountingMethod - Agent" should {
-//    "render the heading" in new Setup(true) {
-//      document.getElementsByClass("govuk-fieldset__legend").text() shouldBe messages(s"$prefix.heading")
-//    }
-//
-//    "render the radio form" in new Setup(true) {
-//      document.getElementsByClass("govuk-label govuk-radios__label").eq(0).text() shouldBe messages(s"$prefix.radio-1-title")
-//      document.getElementsByClass("govuk-label govuk-radios__label").eq(1).text() shouldBe messages(s"$prefix.radio-2-title")
-//      document.getElementsByClass("govuk-radios").size() shouldBe 1
-//    }
-//    "render the back link with the correct URL" in new Setup(true) {
-//      document.getElementById("back").text() shouldBe messages("base.back")
-//      document.getElementById("back").attr("href") shouldBe backUrl
-//    }
-//    "render the continue button" in new Setup(true) {
-//      document.getElementById("continue-button").text() shouldBe messages("base.continue")
-//    }
-//    "render the input error" in new Setup(true, true) {
-//      document.getElementById(s"$prefix-error").text() shouldBe messages("base.error-prefix") + " " +
-//        messages(s"$prefix.no-selection")
-//    }
-//    "render the error summary" in new Setup(true, true) {
-//      document.getElementById("error-summary-heading").text() shouldBe messages("base.error_summary.heading")
-//      document.getElementsByClass("govuk-list govuk-error-summary__list").text() shouldBe messages(s"$prefix.no-selection")
-//    }
-//    "render the back url" in new Setup(true, true) {
-//      document.getElementById("back").attr("href") shouldBe backUrl
-//    }
-//  }
-//
-//
-//}
+package views.incomeSources.add
+
+import auth.MtdItUser
+import forms.incomeSources.add.ForeignPropertyAccountingMethodForm
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.data.Form
+import play.api.mvc.Call
+import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout}
+import play.twirl.api.HtmlFormat
+import services.DateService
+import testConstants.BaseTestConstants.{testMtditid, testNino}
+import testConstants.incomeSources.IncomeSourceDetailsTestConstants.noIncomeDetails
+import testUtils.TestSupport
+import uk.gov.hmrc.auth.core.AffinityGroup.Individual
+import views.html.incomeSources.add.ForeignPropertyAccountingMethod
+
+class ForeignPropertyAccountingMethodViewSpec extends TestSupport {
+  val foreignPropertyAccountingMethodView: ForeignPropertyAccountingMethod = app.injector.instanceOf[ForeignPropertyAccountingMethod]
+
+  val testUser: MtdItUser[_] = MtdItUser(
+    mtditid = testMtditid,
+    nino = testNino,
+    userName = None,
+    btaNavPartial = None,
+    saUtr = None,
+    credId = Some("12345-credId"),
+    userType = Some(Individual),
+    arn = None,
+    incomeSources = noIncomeDetails
+  )(fakeRequestCeaseUKPropertyDeclarationComplete)
+
+
+  class Setup(isAgent: Boolean, error: Boolean = false) extends TestSupport {
+
+    val form: Form[_] = ForeignPropertyAccountingMethodForm.form
+
+    val backUrl: String = if (isAgent) controllers.incomeSources.add.routes.AddIncomeSourceStartDateCheckController.showForeignPropertyAgent.url else
+      controllers.incomeSources.add.routes.AddIncomeSourceStartDateCheckController.showForeignProperty.url
+    val postAction: Call = if (isAgent) controllers.incomeSources.add.routes.ForeignPropertyAccountingMethodController.submitAgent() else
+      controllers.incomeSources.add.routes.ForeignPropertyAccountingMethodController.submit()
+
+    val prefix: String = "incomeSources.add.foreignPropertyAccountingMethod"
+
+    lazy val view: HtmlFormat.Appendable = if (isAgent) {
+      foreignPropertyAccountingMethodView(
+        form,
+        postAction = postAction,
+        isAgent = true,
+        backUrl = backUrl)
+    } else {
+      foreignPropertyAccountingMethodView(
+        form = form,
+        postAction = postAction,
+        isAgent = false,
+        backUrl = backUrl)
+    }
+
+    lazy val viewWithInputErrors: HtmlFormat.Appendable = if (isAgent) {
+      foreignPropertyAccountingMethodView(
+        form = form.withError(s"$prefix", s"$prefix.no-selection"),
+        postAction = postAction,
+        isAgent = true,
+        backUrl = controllers.incomeSources.add.routes.AddIncomeSourceStartDateCheckController.showForeignPropertyAgent.url)
+
+    } else {
+      foreignPropertyAccountingMethodView(
+        form = form.withError(s"$prefix", s"$prefix.no-selection"),
+        postAction = postAction,
+        isAgent = false,
+        backUrl = controllers.incomeSources.add.routes.AddIncomeSourceStartDateCheckController.showForeignProperty.url)
+    }
+
+    lazy val document: Document = if (error) Jsoup.parse(contentAsString(viewWithInputErrors)) else Jsoup.parse(contentAsString(view))
+  }
+
+  "ForeignPropertyAccountingMethod - Individual" should {
+    "render the heading" in new Setup(false) {
+      document.getElementsByClass("govuk-fieldset__legend").text() shouldBe messages(s"$prefix.heading")
+    }
+    "render the dropdown" in new Setup(false) {
+      document.getElementsByClass("govuk-details__summary").text() shouldBe messages(s"$prefix.example")
+      document.getElementsByClass("govuk-body").eq(0).text() shouldBe messages(s"$prefix.drop-down-text")
+    }
+
+
+    "render the radio form" in new Setup(false) {
+      document.getElementsByClass("govuk-label govuk-radios__label").eq(0).text() shouldBe messages(s"$prefix.radio-1-title")
+      document.getElementsByClass("govuk-label govuk-radios__label").eq(1).text() shouldBe messages(s"$prefix.radio-2-title")
+      document.getElementsByClass("govuk-hint govuk-radios__hint govuk-hint govuk-radios__hint").eq(0).text() shouldBe messages(s"$prefix.radio-1-hint")
+      document.getElementsByClass("govuk-hint govuk-radios__hint govuk-hint govuk-radios__hint").eq(1).text() shouldBe messages(s"$prefix.radio-2-hint")
+      document.getElementsByClass("govuk-radios").size() shouldBe 1
+    }
+    "render the back link with the correct URL" in new Setup(false) {
+      document.getElementById("back").text() shouldBe messages("base.back")
+      document.getElementById("back").attr("href") shouldBe backUrl
+    }
+    "render the continue button" in new Setup(false) {
+      document.getElementById("continue-button").text() shouldBe messages("base.continue")
+    }
+    "render the input error" in new Setup(false, true) {
+      document.getElementById("incomeSources.add.foreignPropertyAccountingMethod-error").text() shouldBe messages("base.error-prefix") + " " +
+        messages(s"$prefix.no-selection")
+    }
+    "render the error summary" in new Setup(false, true) {
+      document.getElementById("error-summary-heading").text() shouldBe messages("base.error_summary.heading")
+      document.getElementsByClass("govuk-list govuk-error-summary__list").text() shouldBe messages(s"$prefix.no-selection")
+    }
+    "render the back url" in new Setup(true, true) {
+      document.getElementById("back").attr("href") shouldBe backUrl
+    }
+  }
+
+  "ForeignPropertyAccountingMethod - Agent" should {
+    "render the heading" in new Setup(true) {
+      document.getElementsByClass("govuk-fieldset__legend").text() shouldBe messages(s"$prefix.heading")
+    }
+
+    "render the radio form" in new Setup(true) {
+      document.getElementsByClass("govuk-label govuk-radios__label").eq(0).text() shouldBe messages(s"$prefix.radio-1-title")
+      document.getElementsByClass("govuk-label govuk-radios__label").eq(1).text() shouldBe messages(s"$prefix.radio-2-title")
+      document.getElementsByClass("govuk-radios").size() shouldBe 1
+    }
+    "render the back link with the correct URL" in new Setup(true) {
+      document.getElementById("back").text() shouldBe messages("base.back")
+      document.getElementById("back").attr("href") shouldBe backUrl
+    }
+    "render the continue button" in new Setup(true) {
+      document.getElementById("continue-button").text() shouldBe messages("base.continue")
+    }
+    "render the input error" in new Setup(true, true) {
+      document.getElementById(s"$prefix-error").text() shouldBe messages("base.error-prefix") + " " +
+        messages(s"$prefix.no-selection")
+    }
+    "render the error summary" in new Setup(true, true) {
+      document.getElementById("error-summary-heading").text() shouldBe messages("base.error_summary.heading")
+      document.getElementsByClass("govuk-list govuk-error-summary__list").text() shouldBe messages(s"$prefix.no-selection")
+    }
+    "render the back url" in new Setup(true, true) {
+      document.getElementById("back").attr("href") shouldBe backUrl
+    }
+  }
+
+
+}
--- test/views/incomeSources/cease/CheckCeaseBusinessDetailsViewSpec.scala
+++ test/views/incomeSources/cease/CheckCeaseBusinessDetailsViewSpec.scala
@@ -14,119 +14,103 @@
  * limitations under the License.
  */
 
-///*
-// * Copyright 2023 HM Revenue & Customs
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package views.incomeSources.cease
-//
-//import auth.MtdItUser
-//import models.incomeSourceDetails.viewmodels.CheckCeaseBusinessDetailsViewModel
-//import org.jsoup.nodes.Document
-//import play.api.test.FakeRequest
-//import play.twirl.api.HtmlFormat
-//import testConstants.BaseTestConstants.{testArn, testCredId, testMtditid, testNino, testRetrievedUserName, testSaUtr, testSelfEmploymentId, testUserTypeAgent, testUserTypeIndividual}
-//import testConstants.BusinessDetailsTestConstants.{address, businessIncomeSourceId, testEndDate, testStartDate, testTradeName}
-//import testConstants.incomeSources.IncomeSourceDetailsTestConstants.{businessAndPropertyAligned, businessesAndPropertyIncome}
-//import testUtils.{TestSupport, ViewSpec}
-//import uk.gov.hmrc.auth.core.retrieve.Name
-//import views.html.incomeSources.cease.CheckCeaseBusinessDetails
-//
-//import java.time.LocalDate
-//
-//class CheckCeaseBusinessDetailsViewSpec extends TestSupport with ViewSpec{
-//  val checkCeaseBusinessDetailsView = app.injector.instanceOf[CheckCeaseBusinessDetails]
-//  val businessEndDate = "2022-04-23"
-//  val businessEndShortLongDate = "23 April 2022"
-//  val individualUseWithSession: MtdItUser[_] = MtdItUser(
-//    mtditid = testMtditid,
-//    nino = testNino,
-//    userName = Some(testRetrievedUserName),
-//    incomeSources = businessAndPropertyAligned,
-//    btaNavPartial = None,
-//    saUtr = Some(testSaUtr),
-//    credId = Some(testCredId),
-//    userType = Some(testUserTypeIndividual),
-//    arn = None
-//  )(FakeRequest().withSession(forms.utils.SessionKeys.ceaseBusinessEndDate -> businessEndDate)
-//    .withSession(forms.utils.SessionKeys.ceaseBusinessIncomeSourceId -> businessIncomeSourceId))
-//
-// def agentUserConfirmedClientWithSession(clientNino: String = "AA111111A"): MtdItUser[_] = MtdItUser(
-//    mtditid = "XAIT00000000015",
-//    nino = clientNino,
-//    userName = Some(Name(Some("Test"), Some("User"))),
-//    incomeSources = businessesAndPropertyIncome,
-//    btaNavPartial = None,
-//    saUtr = Some("1234567890"),
-//    credId = Some(testCredId),
-//    userType = Some(testUserTypeAgent),
-//    arn = Some(testArn)
-//  )(FakeRequest().withSession(forms.utils.SessionKeys.ceaseBusinessEndDate -> businessEndDate)
-//   .withSession(forms.utils.SessionKeys.ceaseBusinessIncomeSourceId -> businessIncomeSourceId))
-//
-//  val ceaseBusinessDetailsViewModel = CheckCeaseBusinessDetailsViewModel(
-//    incomeSourceId = testSelfEmploymentId,
-//    tradingName = Some(testTradeName),
-//    address = Some(address),
-//    businessEndDate = LocalDate.parse(businessEndDate)
-//  )
-//
-//  lazy val viewAgent: HtmlFormat.Appendable = checkCeaseBusinessDetailsView(ceaseBusinessDetailsViewModel, true)(agentUserConfirmedClientWithSession(), implicitly)
-//  lazy val view: HtmlFormat.Appendable = checkCeaseBusinessDetailsView(ceaseBusinessDetailsViewModel, false)(individualUseWithSession, implicitly)
-//
-//  val caption = messages("incomeSources.ceaseBusiness.checkDetails.soloTrader")
-//  val heading = messages("incomeSources.ceaseBusiness.checkDetails.heading")
-//  val businessStopDateLabel = messages("incomeSources.ceaseBusiness.checkDetails.dateStopped")
-//  val businessNameLabel = messages("incomeSources.ceaseBusiness.checkDetails.businessName")
-//  val businessAddressLabel = messages("incomeSources.ceaseBusiness.checkDetails.businessAddress")
-//  val buttonLabel = messages("incomeSources.ceaseBusiness.checkDetails.confirm")
-//  val changeUrl = controllers.incomeSources.cease.routes.BusinessEndDateController.show(testSelfEmploymentId).url
-//  val changeUrlAgent = controllers.incomeSources.cease.routes.BusinessEndDateController.showAgent(testSelfEmploymentId).url
-//  val formAction = controllers.incomeSources.cease.routes.CheckCeaseBusinessDetailsController.submit().url
-//  val formActionAgent = controllers.incomeSources.cease.routes.CheckCeaseBusinessDetailsController.submitAgent().url
-//
-//  def validateHtmlValues(isAgent: Boolean, document: Document): Unit = {
-//    document.hasPageHeading(heading)
-//    document.getElementById("caption").text() shouldBe caption
-//    document.getElementById("businessStopDateLabel").text() shouldBe businessStopDateLabel
-//    document.getElementById("businessStopDate").text() shouldBe businessEndShortLongDate
-//    document.getElementById("businessNameLabel").text() shouldBe businessNameLabel
-//    document.getElementById("businessName").text() shouldBe testTradeName
-//    document.getElementById("businessAddressLabel").text() shouldBe businessAddressLabel
-//    document.getElementById("businessAddress").text() shouldBe address.addressLine1+" "+
-//      address.addressLine2.get+" "+
-//      address.addressLine3.get+" "+
-//      address.addressLine4.get+" "+
-//      address.postCode.get+" "+
-//      address.countryCode
-//    document.getElementById("change").attr("href") shouldBe (if(isAgent) changeUrlAgent else changeUrl)
-//    document.getElementById("continue-button").text() shouldBe buttonLabel
-//    document.form.attr("action") shouldBe (if(isAgent) formActionAgent else formAction)
-//  }
-//
-//  "CheckCeaseBusinessDetailsView - Individual" should {
-//
-//    "render the page " in new Setup(view) {
-//      validateHtmlValues(false, document)
-//    }
-//  }
-//
-//  "CheckCeaseBusinessDetailsView - Agent" should {
-//
-//    "render the page " in new Setup(viewAgent) {
-//      validateHtmlValues(true, document)
-//    }
-//  }
-//}
+package views.incomeSources.cease
+
+import auth.MtdItUser
+import models.incomeSourceDetails.viewmodels.CheckCeaseBusinessDetailsViewModel
+import org.jsoup.nodes.Document
+import play.api.test.FakeRequest
+import play.twirl.api.HtmlFormat
+import testConstants.BaseTestConstants.{testArn, testCredId, testMtditid, testNino, testRetrievedUserName, testSaUtr, testSelfEmploymentId, testUserTypeAgent, testUserTypeIndividual}
+import testConstants.BusinessDetailsTestConstants.{address, businessIncomeSourceId, testEndDate, testStartDate, testTradeName}
+import testConstants.incomeSources.IncomeSourceDetailsTestConstants.{businessAndPropertyAligned, businessesAndPropertyIncome}
+import testUtils.{TestSupport, ViewSpec}
+import uk.gov.hmrc.auth.core.retrieve.Name
+import views.html.incomeSources.cease.CheckCeaseBusinessDetails
+
+import java.time.LocalDate
+
+class CheckCeaseBusinessDetailsViewSpec extends TestSupport with ViewSpec{
+  val checkCeaseBusinessDetailsView = app.injector.instanceOf[CheckCeaseBusinessDetails]
+  val businessEndDate = "2022-04-23"
+  val businessEndShortLongDate = "23 April 2022"
+  val individualUseWithSession: MtdItUser[_] = MtdItUser(
+    mtditid = testMtditid,
+    nino = testNino,
+    userName = Some(testRetrievedUserName),
+    incomeSources = businessAndPropertyAligned,
+    btaNavPartial = None,
+    saUtr = Some(testSaUtr),
+    credId = Some(testCredId),
+    userType = Some(testUserTypeIndividual),
+    arn = None
+  )(FakeRequest().withSession(forms.utils.SessionKeys.ceaseBusinessEndDate -> businessEndDate)
+    .withSession(forms.utils.SessionKeys.ceaseBusinessIncomeSourceId -> businessIncomeSourceId))
+
+ def agentUserConfirmedClientWithSession(clientNino: String = "AA111111A"): MtdItUser[_] = MtdItUser(
+    mtditid = "XAIT00000000015",
+    nino = clientNino,
+    userName = Some(Name(Some("Test"), Some("User"))),
+    incomeSources = businessesAndPropertyIncome,
+    btaNavPartial = None,
+    saUtr = Some("1234567890"),
+    credId = Some(testCredId),
+    userType = Some(testUserTypeAgent),
+    arn = Some(testArn)
+  )(FakeRequest().withSession(forms.utils.SessionKeys.ceaseBusinessEndDate -> businessEndDate)
+   .withSession(forms.utils.SessionKeys.ceaseBusinessIncomeSourceId -> businessIncomeSourceId))
+
+  val ceaseBusinessDetailsViewModel = CheckCeaseBusinessDetailsViewModel(
+    incomeSourceId = testSelfEmploymentId,
+    tradingName = Some(testTradeName),
+    address = Some(address),
+    businessEndDate = LocalDate.parse(businessEndDate)
+  )
+
+  lazy val viewAgent: HtmlFormat.Appendable = checkCeaseBusinessDetailsView(ceaseBusinessDetailsViewModel, true)(agentUserConfirmedClientWithSession(), implicitly)
+  lazy val view: HtmlFormat.Appendable = checkCeaseBusinessDetailsView(ceaseBusinessDetailsViewModel, false)(individualUseWithSession, implicitly)
+
+  val caption = messages("incomeSources.ceaseBusiness.checkDetails.soloTrader")
+  val heading = messages("incomeSources.ceaseBusiness.checkDetails.heading")
+  val businessStopDateLabel = messages("incomeSources.ceaseBusiness.checkDetails.dateStopped")
+  val businessNameLabel = messages("incomeSources.ceaseBusiness.checkDetails.businessName")
+  val businessAddressLabel = messages("incomeSources.ceaseBusiness.checkDetails.businessAddress")
+  val buttonLabel = messages("incomeSources.ceaseBusiness.checkDetails.confirm")
+  val changeUrl = controllers.incomeSources.cease.routes.BusinessEndDateController.show(testSelfEmploymentId).url
+  val changeUrlAgent = controllers.incomeSources.cease.routes.BusinessEndDateController.showAgent(testSelfEmploymentId).url
+  val formAction = controllers.incomeSources.cease.routes.CheckCeaseBusinessDetailsController.submit().url
+  val formActionAgent = controllers.incomeSources.cease.routes.CheckCeaseBusinessDetailsController.submitAgent().url
+
+  def validateHtmlValues(isAgent: Boolean, document: Document): Unit = {
+    document.hasPageHeading(heading)
+    document.getElementById("caption").text() shouldBe caption
+    document.getElementById("businessStopDateLabel").text() shouldBe businessStopDateLabel
+    document.getElementById("businessStopDate").text() shouldBe businessEndShortLongDate
+    document.getElementById("businessNameLabel").text() shouldBe businessNameLabel
+    document.getElementById("businessName").text() shouldBe testTradeName
+    document.getElementById("businessAddressLabel").text() shouldBe businessAddressLabel
+    document.getElementById("businessAddress").text() shouldBe address.addressLine1+" "+
+      address.addressLine2.get+" "+
+      address.addressLine3.get+" "+
+      address.addressLine4.get+" "+
+      address.postCode.get+" "+
+      address.countryCode
+    document.getElementById("change").attr("href") shouldBe (if(isAgent) changeUrlAgent else changeUrl)
+    document.getElementById("continue-button").text() shouldBe buttonLabel
+    document.form.attr("action") shouldBe (if(isAgent) formActionAgent else formAction)
+  }
+
+  "CheckCeaseBusinessDetailsView - Individual" should {
+
+    "render the page " in new Setup(view) {
+      validateHtmlValues(false, document)
+    }
+  }
+
+  "CheckCeaseBusinessDetailsView - Agent" should {
+
+    "render the page " in new Setup(viewAgent) {
+      validateHtmlValues(true, document)
+    }
+  }
+}
