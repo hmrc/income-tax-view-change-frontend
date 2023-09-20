b22dac23c0b4ce7eae6b61ca983d4f9b031d4e26
avelx <3528155+avelx@users.noreply.github.com>
2023-09-20 12:31:19 +0100

    MISUV-6366:   * resolve PR-bot warnings / step-B;

--- app/audit/AuditingService.scala
+++ app/audit/AuditingService.scala
@@ -30,15 +30,16 @@ import uk.gov.hmrc.play.audit.model.{DataEvent, ExtendedDataEvent}
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
-@Singleton
-class AuditingService @Inject()(appConfig: FrontendAppConfig, auditConnector: AuditConnector) {
 
-  implicit val dataEventWrites: Writes[DataEvent] = Json.writes[DataEvent]
-  implicit val extendedDataEventWrites: Writes[ExtendedDataEvent] = Json.writes[ExtendedDataEvent]
+@Singleton
+class AuditingService @Inject()(appConfig: FrontendAppConfig, auditConnector: AuditConnector)  {
+
+  //implicit val extendedDataEventWrites: Writes[ExtendedDataEvent] = Json.writes[ExtendedDataEvent]
+  //implicit val dataEventWrites: Writes[DataEvent] = Json.writes[DataEvent]
 
   def audit(auditModel: AuditModel, path: Option[String] = None)(implicit hc: HeaderCarrier, request: Request[_], ec: ExecutionContext): Unit = {
     val dataEvent = toDataEvent(appConfig.appName, auditModel, path.fold(request.path)(x => x))
-    Logger("application").debug(s"Splunk Audit Event:\n\n${Json.toJson(dataEvent)}")
+    //Logger("application").debug(s"Splunk Audit Event:\n\n${Json.toJson(dataEvent)}")
     auditConnector.sendEvent(dataEvent).map {
       case Success =>
         Logger("application").debug("[AuditingService][audit] - Splunk Audit Successful")
@@ -62,7 +63,7 @@ class AuditingService @Inject()(appConfig: FrontendAppConfig, auditConnector: Au
                                                                                  request: Request[_],
                                                                                  ec: ExecutionContext): Unit = {
     val extendedDataEvent = toExtendedDataEvent(appConfig.appName, auditModel, path.fold(request.path)(x => x))
-    Logger("application").debug(s"Splunk Audit Event:\n\n${Json.toJson(extendedDataEvent)}")
+    //Logger("application").debug(s"Splunk Audit Event:\n\n${Json.toJson(extendedDataEvent)}")
     auditConnector.sendExtendedEvent(extendedDataEvent).map {
       case Success =>
         Logger("application").debug("[AuditingService][extendedAudit] - Splunk Audit Successful")
