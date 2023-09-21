03712810592b8cbcf075084748bf07b3ce5f0ad3
danielmaltz1 <117353102+danielmaltz1@users.noreply.github.com>
2023-09-21 14:36:09 +0100

    Added submit and invalid access tests

--- test/controllers/incomeSources/add/IncomeSourceAddedControllerSpec.scala
+++ test/controllers/incomeSources/add/IncomeSourceAddedControllerSpec.scala
@@ -1,17 +1,44 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.incomeSources.add
 
 import config.{AgentItvcErrorHandler, FrontendAppConfig, ItvcErrorHandler}
-import config.featureswitch.FeatureSwitching
+import config.featureswitch.{FeatureSwitching, IncomeSources}
 import controllers.predicates.{NinoPredicate, SessionTimeoutPredicate}
+import enums.IncomeSourceJourney._
 import mocks.MockItvcErrorHandler
 import mocks.auth.MockFrontendAuthorisedFunctions
 import mocks.controllers.predicates.{MockAuthenticationPredicate, MockIncomeSourceDetailsPredicate, MockNavBarEnumFsPredicate}
 import mocks.services.{MockClientDetailsService, MockNextUpdatesService}
+import models.nextUpdates.{NextUpdateModel, NextUpdatesModel, ObligationsModel}
 import org.mockito.Mockito.mock
-import play.api.mvc.MessagesControllerComponents
+import play.api.mvc.{MessagesControllerComponents, Result}
+import play.api.test.Helpers.{redirectLocation, status}
 import services.DateService
+import testConstants.BaseTestConstants.{testAgentAuthRetrievalSuccess, testSelfEmploymentId}
 import testUtils.TestSupport
 import views.html.incomeSources.add.IncomeSourceAddedObligations
+import play.api.http.Status.SEE_OTHER
+import play.api.test.Helpers.defaultAwaitTimeout
+import testConstants.BaseTestConstants
+import testConstants.incomeSources.IncomeSourceDetailsTestConstants.businessesAndPropertyIncome
+
+import java.time.LocalDate
+import scala.concurrent.Future
 
 class IncomeSourceAddedControllerSpec extends TestSupport
   with MockFrontendAuthorisedFunctions
@@ -44,4 +71,98 @@ class IncomeSourceAddedControllerSpec extends TestSupport
     mockDateService
   )
 
+  val testObligationsModel: ObligationsModel = ObligationsModel(Seq(
+    NextUpdatesModel(testSelfEmploymentId, List(NextUpdateModel(
+      LocalDate.of(2022, 7, 1),
+      LocalDate.of(2022, 7, 2),
+      LocalDate.of(2022, 8, 2),
+      "Quarterly",
+      None,
+      "#001"
+    ),
+      NextUpdateModel(
+        LocalDate.of(2022, 7, 1),
+        LocalDate.of(2022, 7, 2),
+        LocalDate.of(2022, 8, 2),
+        "Quarterly",
+        None,
+        "#002"
+      )
+    ))
+  ))
+
+
+  "IncomeSourceAddedController" should {
+    "redirect a user back to the custom error page" when {
+      "the user is not authenticated" should {
+        "redirect them to sign in" in {
+          setupMockAuthorisationException()
+          val result = TestIncomeSourceAddedController.show("", SelfEmployment)(fakeRequestWithActiveSession)
+          status(result) shouldBe SEE_OTHER
+          redirectLocation(result) shouldBe Some(controllers.routes.SignInController.signIn.url)
+        }
+      }
+    }
+    "the user has timed out" should {
+      "redirect to the session timeout page" in {
+        setupMockAuthorisationException()
+
+        val result = TestIncomeSourceAddedController.submit()(fakeRequestWithTimeoutSession)
+
+        status(result) shouldBe SEE_OTHER
+        redirectLocation(result) shouldBe Some(controllers.timeout.routes.SessionTimeoutController.timeout.url)
+      }
+    }
+
+    "feature switch is disabled" should {
+      "redirect to home page" in {
+        disableAllSwitches()
+
+        setupMockAuthRetrievalSuccess(BaseTestConstants.testIndividualAuthSuccessWithSaUtrResponse())
+        setupMockGetIncomeSourceDetails()(businessesAndPropertyIncome)
+
+        val result: Future[Result] = TestIncomeSourceAddedController.show("123", UkProperty)(fakeRequestWithActiveSession)
+        status(result) shouldBe SEE_OTHER
+        redirectLocation(result) shouldBe Some(controllers.routes.HomeController.show().url)
+      }
+      "redirect to home page (agent)" in {
+        disableAllSwitches()
+
+        setupMockAgentAuthRetrievalSuccess(testAgentAuthRetrievalSuccess)
+        setupMockGetIncomeSourceDetails()(businessesAndPropertyIncome)
+
+        val result: Future[Result] = TestIncomeSourceAddedController.showAgent("123", ForeignProperty)(fakeRequestConfirmedClient())
+        status(result) shouldBe SEE_OTHER
+        redirectLocation(result) shouldBe Some(controllers.routes.HomeController.showAgent.url)
+      }
+    }
+
+
+
+
+    ".submit" should {
+      "take the individual back to add income sources" in {
+        disableAllSwitches()
+        enable(IncomeSources)
+
+        setupMockAuthRetrievalSuccess(BaseTestConstants.testIndividualAuthSuccessWithSaUtrResponse())
+        setupMockGetIncomeSourceDetails()(businessesAndPropertyIncome)
+
+        val result: Future[Result] = TestIncomeSourceAddedController.submit(fakeRequestWithActiveSession)
+        status(result) shouldBe SEE_OTHER
+        redirectLocation(result) shouldBe Some(controllers.incomeSources.add.routes.AddIncomeSourceController.show().url)
+      }
+      "take the agent back to add income sources" in {
+        disableAllSwitches()
+        enable(IncomeSources)
+
+        setupMockAgentAuthRetrievalSuccess(testAgentAuthRetrievalSuccess, withClientPredicate = false)
+        setupMockGetIncomeSourceDetails()(businessesAndPropertyIncome)
+
+        val result: Future[Result] = TestIncomeSourceAddedController.agentSubmit(fakeRequestConfirmedClient())
+        status(result) shouldBe SEE_OTHER
+        redirectLocation(result) shouldBe Some(controllers.incomeSources.add.routes.AddIncomeSourceController.showAgent().url)
+      }
+    }
+  }
 }
