4d0da0ab696b5baa3f4d02abf5319611051a7b4b
danielmaltz1 <117353102+danielmaltz1@users.noreply.github.com>
2023-09-26 10:36:08 +0100

    Almost finished Json

--- app/audit/models/ObligationsAuditModel.scala
+++ app/audit/models/ObligationsAuditModel.scala
@@ -3,7 +3,7 @@ package audit.models
 import audit.Utilities
 import auth.MtdItUser
 import enums.IncomeSourceJourney.{ForeignProperty, IncomeSourceType, SelfEmployment, UkProperty}
-import models.incomeSourceDetails.viewmodels.{CeasedBusinessDetailsViewModel, ObligationsViewModel, ViewBusinessDetailsViewModel, ViewPropertyDetailsViewModel}
+import models.incomeSourceDetails.viewmodels.{CeasedBusinessDetailsViewModel, DatesModel, ObligationsViewModel, ViewBusinessDetailsViewModel, ViewPropertyDetailsViewModel}
 import play.api.libs.json.{JsValue, Json}
 
 case class ObligationsAuditModel(incomeSourceType: IncomeSourceType,
@@ -23,8 +23,23 @@ case class ObligationsAuditModel(incomeSourceType: IncomeSourceType,
 
   val name: String = incomeSourceType match {
     case SelfEmployment => businessName.getOrElse("Self Employment Business")
-    case UkProperty => ???
-    case ForeignProperty => ???
+    case UkProperty => "UK property"
+    case ForeignProperty => "Foreign property"
+  }
+
+  val quarterly: Seq[(Int, Seq[DatesModel])] = {
+    ((if (obligations.quarterlyObligationsDatesYearOne.nonEmpty)
+      Seq((obligations.quarterlyObligationsDatesYearOne.head.inboundCorrespondenceFrom.getYear, obligations.quarterlyObligationsDatesYearOne))
+    else
+      Seq()
+      )
+    ++
+    (if (obligations.quarterlyObligationsDatesYearTwo.nonEmpty)
+      Seq((obligations.quarterlyObligationsDatesYearTwo.head.inboundCorrespondenceFrom.getYear, obligations.quarterlyObligationsDatesYearTwo))
+    else
+      Seq()
+      ))
+
   }
 
   override val detail: JsValue = {
@@ -36,11 +51,37 @@ case class ObligationsAuditModel(incomeSourceType: IncomeSourceType,
             "businessName" -> name,
             "reportingMethod" -> "",
             "taxYear" -> s"${obligations.currentTaxYear}-${obligations.currentTaxYear+1}"
-          )
+          ),
+        "quarterlyUpdates" ->
+          quarterly.map { value =>
+            Json.obj(
+              "taxYear" -> s"${value._1}-${value._1+1}",
+              "quarter" -> value._2.map { entry =>
+                Json.obj(
+                  "startDate" -> entry.inboundCorrespondenceFrom,
+                  "endDate" -> entry.inboundCorrespondenceTo,
+                  "deadline" -> entry.inboundCorrespondenceDue
+                )
+              }
+            )
+          },
+        "EOPstatement" ->
+          obligations.eopsObligationsDates.map { eops =>
+            Json.obj(
+              "taxYearStartDate" -> eops.inboundCorrespondenceFrom,
+              "taxYearEndDate" ->  eops.inboundCorrespondenceDue,
+              "deadline" -> eops.inboundCorrespondenceDue
+            )
+          },
+        "finalDeclaration" ->
+          obligations.finalDeclarationDates.map { finalDec =>
+            Json.obj(
+              "taxYearStartDate" -> finalDec.inboundCorrespondenceFrom,
+              "taxYearEndDate" -> finalDec.inboundCorrespondenceDue,
+              "deadline" -> finalDec.inboundCorrespondenceDue
+            )
+          }
       )
-    //quarterlyUpdates
-    //EOPStatement
-    //finalDeclaration
+    //reportingMethod
   }
-
 }
