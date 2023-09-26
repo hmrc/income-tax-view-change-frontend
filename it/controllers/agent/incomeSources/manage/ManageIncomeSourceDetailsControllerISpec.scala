651e6c204415b5813651962b218b8658836ea8b9
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2023-09-26 13:54:44 +0100

    agent SE business test passing

--- it/controllers/agent/incomeSources/manage/ManageIncomeSourceDetailsControllerISpec.scala
+++ it/controllers/agent/incomeSources/manage/ManageIncomeSourceDetailsControllerISpec.scala
@@ -1,69 +1,74 @@
-///*
-// * Copyright 2023 HM Revenue & Customs
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package controllers.agent.incomeSources.manage
-//
-//import helpers.agent.ComponentSpecBase
-//import config.featureswitch.IncomeSources
-//import helpers.servicemocks.{CalculationListStub, ITSAStatusDetailsStub, IncomeTaxViewChangeStub}
-//import models.incomeSourceDetails.LatencyDetails
-//import play.api.http.Status.OK
-//import testConstants.BaseIntegrationTestConstants.{clientDetailsWithConfirmation, testMtditid, testNino, testSelfEmploymentId, testTaxYearRange}
-//import testConstants.CalculationListIntegrationTestConstants
-//import testConstants.IncomeSourceIntegrationTestConstants.{foreignPropertyOnlyResponse, singleBusinessResponse, singleBusinessResponse2, singleBusinessResponseInLatencyPeriod, singleBusinessResponseInLatencyPeriod2, singleBusinessResponseWithUnknownsInLatencyPeriod, singleForeignPropertyResponseInLatencyPeriod, singleForeignPropertyResponseWithUnknownsInLatencyPeriod, singleUKPropertyResponseInLatencyPeriod, singleUKPropertyResponseWithUnknownsInLatencyPeriod, ukPropertyOnlyResponse}
-//
-//import java.time.LocalDate
-//import java.time.Month.APRIL
-//
-//class ManageIncomeSourceDetailsControllerISpec extends ComponentSpecBase {
-//
-//  val manageSelfEmploymentShowAgentUrl: String = controllers.incomeSources.manage.routes.ManageIncomeSourceDetailsController.showSoleTraderBusinessAgent(testSelfEmploymentId).url
-//  val manageUKPropertyShowAgentUrl: String = controllers.incomeSources.manage.routes.ManageIncomeSourceDetailsController.showUkPropertyAgent().url
-//  val manageForeignPropertyShowAgentUrl: String = controllers.incomeSources.manage.routes.ManageIncomeSourceDetailsController.showForeignPropertyAgent().url
-//  val currentTaxYear: Int = dateService.getCurrentTaxYearEnd()
-//  val lastDayOfCurrentTaxYear: LocalDate = LocalDate.of(currentTaxYear, APRIL, 5)
-//  val taxYear1: Int = currentTaxYear
-//  val taxYear2: Int = (currentTaxYear + 1)
-//  val quarterlyIndicator: String = "Q"
-//  val annuallyIndicator: String = "A"
-//  val latencyDetails: LatencyDetails = LatencyDetails(
-//    latencyEndDate = lastDayOfCurrentTaxYear.plusYears(1),
-//    taxYear1 = taxYear1.toString,
-//    latencyIndicator1 = quarterlyIndicator,
-//    taxYear2 = taxYear2.toString,
-//    latencyIndicator2 = annuallyIndicator
-//  )
-//  val latencyDetails2: LatencyDetails = LatencyDetails(
-//    latencyEndDate = lastDayOfCurrentTaxYear.plusYears(1),
-//    taxYear1 = taxYear1.toString,
-//    latencyIndicator1 = annuallyIndicator,
-//    taxYear2 = taxYear2.toString,
-//    latencyIndicator2 = quarterlyIndicator
-//  )
-//  val addressAsString: String = "64 Zoo Lane Happy Place Magical Land England ZL1 064 United Kingdom"
-//  val businessTradingName: String = "business"
-//  val businessStartDate: String = "1 January 2017"
-//  val businessAccountingMethod: String = "Cash basis accounting"
-//  val thisTestSelfEmploymentId = "ABC123456789"
-//  val messagesAnnually: String = messagesAPI("incomeSources.manage.business-manage-details.annually")
-//  val messagesQuarterly: String = messagesAPI("incomeSources.manage.business-manage-details.quarterly")
-//  val messagesChangeLinkText: String = messagesAPI("incomeSources.manage.business-manage-details.change")
-//  val messagesUnknown: String = messagesAPI("incomeSources.generic.unknown")
-//
-//  s"calling GET $manageSelfEmploymentShowAgentUrl" should {
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.agent.incomeSources.manage
+
+import audit.models.ManageYourDetailsResponseAuditModel
+import helpers.agent.ComponentSpecBase
+import auth.MtdItUser
+import config.featureswitch.IncomeSources
+import helpers.servicemocks.{AuditStub, CalculationListStub, ITSAStatusDetailsStub, IncomeTaxViewChangeStub}
+import models.incomeSourceDetails.{IncomeSourceDetailsModel, LatencyDetails}
+import play.api.http.Status.OK
+import play.api.test.FakeRequest
+import testConstants.BaseIntegrationTestConstants.{clientDetailsWithConfirmation, credId, manageIncomeSourceDetailsViewModel, testMtditid, testNino, testSaUtr, testSelfEmploymentId, testTaxYearRange}
+import testConstants.BusinessDetailsIntegrationTestConstants.business1
+import testConstants.CalculationListIntegrationTestConstants
+import testConstants.IncomeSourceIntegrationTestConstants.{foreignPropertyOnlyResponse, singleBusinessResponse, singleBusinessResponse2, singleBusinessResponseInLatencyPeriod, singleBusinessResponseInLatencyPeriod2, singleBusinessResponseManageYourDetailsAudit, singleBusinessResponseWithUnknownsInLatencyPeriod, singleForeignPropertyResponseInLatencyPeriod, singleForeignPropertyResponseWithUnknownsInLatencyPeriod, singleUKPropertyResponseInLatencyPeriod, singleUKPropertyResponseWithUnknownsInLatencyPeriod, ukPropertyOnlyResponse}
+import uk.gov.hmrc.auth.core.AffinityGroup.{Agent, Individual}
+
+import java.time.LocalDate
+import java.time.Month.APRIL
+
+class ManageIncomeSourceDetailsControllerISpec extends ComponentSpecBase {
+
+  val manageSelfEmploymentShowAgentUrl: String = controllers.incomeSources.manage.routes.ManageIncomeSourceDetailsController.showSoleTraderBusinessAgent(testSelfEmploymentId).url
+  val manageUKPropertyShowAgentUrl: String = controllers.incomeSources.manage.routes.ManageIncomeSourceDetailsController.showUkPropertyAgent().url
+  val manageForeignPropertyShowAgentUrl: String = controllers.incomeSources.manage.routes.ManageIncomeSourceDetailsController.showForeignPropertyAgent().url
+  val currentTaxYear: Int = dateService.getCurrentTaxYearEnd()
+  val lastDayOfCurrentTaxYear: LocalDate = LocalDate.of(currentTaxYear, APRIL, 5)
+  val taxYear1: Int = currentTaxYear
+  val taxYear2: Int = (currentTaxYear + 1)
+  val quarterlyIndicator: String = "Q"
+  val annuallyIndicator: String = "A"
+  val latencyDetails: LatencyDetails = LatencyDetails(
+    latencyEndDate = lastDayOfCurrentTaxYear.plusYears(1),
+    taxYear1 = taxYear1.toString,
+    latencyIndicator1 = quarterlyIndicator,
+    taxYear2 = taxYear2.toString,
+    latencyIndicator2 = annuallyIndicator
+  )
+  val latencyDetails2: LatencyDetails = LatencyDetails(
+    latencyEndDate = lastDayOfCurrentTaxYear.plusYears(1),
+    taxYear1 = taxYear1.toString,
+    latencyIndicator1 = annuallyIndicator,
+    taxYear2 = taxYear2.toString,
+    latencyIndicator2 = quarterlyIndicator
+  )
+  val addressAsString: String = "64 Zoo Lane Happy Place Magical Land England ZL1 064 United Kingdom"
+  val businessTradingName: String = "business"
+  val businessStartDate: String = "1 January 2017"
+  val businessAccountingMethod: String = "Cash basis accounting"
+  val thisTestSelfEmploymentId = "ABC123456789"
+  val messagesAnnually: String = messagesAPI("incomeSources.manage.business-manage-details.annually")
+  val messagesQuarterly: String = messagesAPI("incomeSources.manage.business-manage-details.quarterly")
+  val messagesChangeLinkText: String = messagesAPI("incomeSources.manage.business-manage-details.change")
+  val messagesUnknown: String = messagesAPI("incomeSources.generic.unknown")
+
+  s"calling GET $manageSelfEmploymentShowAgentUrl" should {
 //    "render the Manage Self Employment business page for your client" when {
 //      "URL contains a valid income source ID and agent's authorised user has no latency information" in {
 //        Given("Income Sources FS is enabled")
@@ -174,8 +179,52 @@
 //        )
 //      }
 //    }
-//  }
-//
+    "return the audit event" when {
+      "User is authorised" in {
+        stubAuthorisedAgentUser(authorised = true)
+        enable(IncomeSources)
+
+        And("API 1525 getIncomeSourceDetails returns a success response")
+        IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, singleBusinessResponseManageYourDetailsAudit)
+
+        And("API 1878 getITSAStatus returns a success response with a valid status (MTD Mandated or MTD Voluntary)")
+        ITSAStatusDetailsStub.stubGetITSAStatusDetails("MTD Mandated")
+
+        And("API 1404 getCalculationList returns a success response")
+        CalculationListStub.stubGetLegacyCalculationList(testNino, taxYear1.toString)(CalculationListIntegrationTestConstants.successResponseNonCrystallised.toString())
+
+        And("API 1896 getCalculationList returns a success response")
+        CalculationListStub.stubGetCalculationList(testNino, testTaxYearRange)(CalculationListIntegrationTestConstants.successResponseNonCrystallised.toString())
+
+        IncomeTaxViewChangeFrontend.get(s"/income-sources/manage/your-details?id=$thisTestSelfEmploymentId", clientDetailsWithConfirmation)
+
+        AuditStub.verifyAuditEvent(
+          ManageYourDetailsResponseAuditModel(viewModel = manageIncomeSourceDetailsViewModel)(
+            MtdItUser(
+              mtditid = testMtditid,
+              nino = testNino,
+              userName = None,
+              incomeSources = IncomeSourceDetailsModel(
+                mtdbsa = testMtditid,
+                yearOfMigration = None,
+                businesses = List(business1),
+                properties = List()
+              ),
+              btaNavPartial = None,
+              saUtr = Some(testSaUtr),
+              credId = None,
+              userType = Some(Agent),
+              arn = Some("1")
+            )(
+              FakeRequest()
+            )
+          )
+        )
+      }
+    }
+
+  }
+
 //  s"callingGET $manageUKPropertyShowAgentUrl" should {
 //    "render the Manage UK Property page" when {
 //      "URL contains a valid income source ID and authorised user has no latency information" in {
@@ -379,4 +428,4 @@
 //      }
 //    }
 //  }
-//}
+}
