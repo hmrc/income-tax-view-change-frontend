1cfca85fc00cf02bc0c9adcfdb89f98a9df7c0fb
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2023-10-03 10:07:37 +0100

    WIP

--- app/audit/models/ManageYourDetailsResponseAuditModel.scala
+++ app/audit/models/ManageYourDetailsResponseAuditModel.scala
@@ -16,62 +16,63 @@
 
 package audit.models
 
-import audit.Utilities
+import audit.Utilities.userAuditDetails
 import auth.MtdItUser
 import enums.IncomeSourceJourney.{ForeignProperty, SelfEmployment, UkProperty}
 import models.incomeSourceDetails.viewmodels.ManageIncomeSourceDetailsViewModel
 import play.api.libs.json.{JsValue, Json}
+import utils.Utilities._
 
 case class ManageYourDetailsResponseAuditModel(
                                                 viewModel: ManageIncomeSourceDetailsViewModel
                                               )(implicit user: MtdItUser[_]) extends ExtendedAuditModel {
+
   override val transactionName: String = enums.TransactionName.ManageIncomeSourceDetails
   override val auditType: String = enums.AuditType.ManageIncomeSourceDetails
 
+  val incomeSourceTypeJson = (viewModel.incomeSourceType match {
+    case SelfEmployment => "SE"
+    case UkProperty => "UKPROPERTY"
+    case ForeignProperty => "FOREIGNPROPERTY"
+  })
+
+  val accountingMethodJson = (viewModel.incomeSourceType match {
+    case SelfEmployment => "SE"
+    case UkProperty => "UKPROPERTY"
+    case ForeignProperty => "FOREIGNPROPERTY"
+  })
+
   override val detail: JsValue =
-    Utilities.userAuditDetails(user) ++
-      Json.obj(
-        "journeyType" ->
-          (viewModel.incomeSourceType match {
-            case SelfEmployment => "SE"
-            case UkProperty => "UKPROPERTY"
-            case ForeignProperty => "FOREIGNPROPERTY"
-          }),
-        "businessName" -> viewModel.tradingName,
-        "businessAddressLine1" -> viewModel.address.map(address => address.addressLine1),
-        "businessAddressLine2" -> viewModel.address.flatMap(address => address.addressLine2),
-        "businessAddressLine3" -> viewModel.address.flatMap(address => address.addressLine3),
-        "businessAddressTownOrCity" -> viewModel.address.flatMap(address => address.addressLine4),
-        "businessAddressPostcode" -> viewModel.address.flatMap(address => address.postCode),
-        "businessAddressCountry" -> (if (viewModel.incomeSourceType == SelfEmployment) "United Kingdom" else None),
-        "dateStarted" -> viewModel.tradingStartDate,
-        "accountingMethod" ->
-          (viewModel.businessAccountingMethod match {
-            case Some(value) => if (value) {
-              "Traditional accounting"
-            } else {
-              "Cash basis accounting"
-            }
-            case None => None
-          }),
-        "incomeReportingMethodYear1" -> viewModel.latencyDetails.map(reportingMethod =>
-          Json.obj(
-            "reportingMethod" ->
-              (reportingMethod.latencyIndicator1 match {
-                case "A" => "Annually"
-                case "Q" => "Quarterly"
-              }),
-            "taxYear" -> s"${reportingMethod.taxYear1}-${(reportingMethod.taxYear1.toInt + 1).toString}"
-          )),
-        "incomeReportingMethodYear2" -> viewModel.latencyDetails.map(reportingMethod =>
-          Json.obj(
-            "reportingMethod" ->
-              (reportingMethod.latencyIndicator2 match {
-                case "A" => "Annually"
-                case "Q" => "Quarterly"
-              }),
-            "taxYear" -> s"${reportingMethod.taxYear2}-${(reportingMethod.taxYear2.toInt + 1).toString}"
-          ))
-      )
+    userAuditDetails(user) ++
+      Json.obj("journeyType" -> incomeSourceTypeJson) ++
+      ("businessName", viewModel.tradingName) ++
+      ("businessAddressLine1", viewModel.address.map(address => address.addressLine1)) ++
+      ("businessAddressLine2", viewModel.address.flatMap(address => address.addressLine2)) ++
+      ("businessAddressLine3", viewModel.address.flatMap(address => address.addressLine3)) ++
+      ("businessAddressLine4", viewModel.address.flatMap(address => address.addressLine4)) ++
+      ("businessAddressPostcode", viewModel.address.flatMap(address => address.postCode)) ++
+      Json.obj("businessAddressCountry" -> (if (viewModel.incomeSourceType == SelfEmployment) "United Kingdom" else None)) ++
+      ("accountingMethod", viewModel.businessAccountingMethod.map {
+        case true => "Traditional accounting"
+        case false => "Cash based accounting"
+      }) ++
+      ("incomeReportingMethodYear1", viewModel.latencyDetails.map(reportingMethod =>
+        Json.obj(
+          "reportingMethod" ->
+            (reportingMethod.latencyIndicator1 match {
+              case "A" => "Annually"
+              case "Q" => "Quarterly"
+            }),
+          "taxYear" -> s"${reportingMethod.taxYear1}-${(reportingMethod.taxYear1.toInt + 1).toString}"
+        ))) ++
+      ("incomeReportingMethodYear2", viewModel.latencyDetails.map(reportingMethod =>
+        Json.obj(
+          "reportingMethod" ->
+            (reportingMethod.latencyIndicator2 match {
+              case "A" => "Annually"
+              case "Q" => "Quarterly"
+            }),
+          "taxYear" -> s"${reportingMethod.taxYear2}-${(reportingMethod.taxYear2.toInt + 1).toString}"
+        )))
 
 }
