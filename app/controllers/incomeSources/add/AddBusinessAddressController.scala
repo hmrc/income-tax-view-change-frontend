14c3dddb6d4255118660e7459c2fefb8a28f91ac
AyubAz <110174399+ayubAz@users.noreply.github.com>
2023-11-08 09:58:11 +0000

    remove \n

--- app/controllers/incomeSources/add/AddBusinessAddressController.scala
+++ app/controllers/incomeSources/add/AddBusinessAddressController.scala
@@ -60,7 +60,6 @@ class AddBusinessAddressController @Inject()(authenticate: AuthenticationPredica
       handleRequest(isAgent = false, isChange = isChange)
   }
 
-
   def showAgent(isChange: Boolean): Action[AnyContent] =
     Authenticated.async {
       implicit request =>
