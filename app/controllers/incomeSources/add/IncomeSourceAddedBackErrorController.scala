df5852c887a5b6248af4fe16a748dfbeebaf6d7c
danielmaltz1 <117353102+danielmaltz1@users.noreply.github.com>
2023-11-30 10:48:40 +0000

    Added new files and routes

---
+++ app/controllers/incomeSources/add/IncomeSourceAddedBackErrorController.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.incomeSources.add
+
+import auth.MtdItUser
+import config.{AgentItvcErrorHandler, FrontendAppConfig, ItvcErrorHandler}
+import controllers.agent.predicates.ClientConfirmedController
+import controllers.predicates._
+import enums.IncomeSourceJourney.IncomeSourceType
+import play.api.mvc._
+import services.IncomeSourceDetailsService
+import uk.gov.hmrc.auth.core.AuthorisedFunctions
+import utils.IncomeSourcesUtils
+import views.html.incomeSources.add.IncomeSourceAddedBackError
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class IncomeSourceAddedBackErrorController @Inject()(val checkSessionTimeout: SessionTimeoutPredicate,
+                                                      val authenticate: AuthenticationPredicate,
+                                                      val authorisedFunctions: AuthorisedFunctions,
+                                                      val retrieveNinoWithIncomeSources: IncomeSourceDetailsPredicate,
+                                                      val incomeSourceDetailsService: IncomeSourceDetailsService,
+                                                      val retrieveBtaNavBar: NavBarPredicate,
+                                                      val cannotGoBackError: IncomeSourceAddedBackError)
+                                                     (implicit val appConfig: FrontendAppConfig,
+                                                      mcc: MessagesControllerComponents,
+                                                      val ec: ExecutionContext,
+                                                      val itvcErrorHandler: ItvcErrorHandler,
+                                                      val itvcErrorHandlerAgent: AgentItvcErrorHandler) extends ClientConfirmedController with IncomeSourcesUtils {
+
+
+  def handleRequest(isAgent: Boolean, incomeSourceType: IncomeSourceType)
+                   (implicit user: MtdItUser[_]): Future[Result] = withIncomeSourcesFS {
+    Future.successful(Ok(cannotGoBackError(isAgent, incomeSourceType)))
+  }
+
+  def show(incomeSourceType: IncomeSourceType): Action[AnyContent] = (checkSessionTimeout andThen authenticate
+    andThen retrieveNinoWithIncomeSources andThen retrieveBtaNavBar).async {
+    implicit user =>
+      handleRequest(
+        isAgent = false,
+        incomeSourceType = incomeSourceType
+      )
+  }
+
+  def showAgent(incomeSourceType: IncomeSourceType): Action[AnyContent] = Authenticated.async {
+    implicit request =>
+      implicit user =>
+        getMtdItUserWithIncomeSources(incomeSourceDetailsService) flatMap {
+          implicit mtdItUser =>
+            handleRequest(
+              isAgent = true,
+              incomeSourceType = incomeSourceType
+            )
+        }
+  }
+}
+
