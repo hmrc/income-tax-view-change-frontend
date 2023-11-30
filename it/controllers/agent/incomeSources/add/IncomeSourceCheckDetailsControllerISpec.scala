3c1c13da8773cf4f9d1cee24cbf3278c3ae7ce22
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2023-11-30 12:09:09 +0000

    IncomeSourceCheckDetailsControllerISpec test updates

--- it/controllers/agent/incomeSources/add/IncomeSourceCheckDetailsControllerISpec.scala
+++ it/controllers/agent/incomeSources/add/IncomeSourceCheckDetailsControllerISpec.scala
@@ -42,15 +42,15 @@ class IncomeSourceCheckDetailsControllerISpec extends ComponentSpecBase {
   def checkBusinessDetailsSubmitUrl(incomeSourceType: IncomeSourceType): String = controllers.incomeSources.add.routes.IncomeSourceCheckDetailsController.submitAgent(incomeSourceType).url
 
   val addBusinessReportingMethodUrl: String = {
-    controllers.incomeSources.add.routes.IncomeSourceReportingMethodController.show(isAgent = true, SelfEmployment, testSelfEmploymentId).url
+    controllers.incomeSources.add.routes.IncomeSourceReportingMethodController.show(isAgent = true, SelfEmployment).url
   }
 
   val addForeignPropReportingMethodUrl: String = {
-    controllers.incomeSources.add.routes.IncomeSourceReportingMethodController.show(isAgent = true, ForeignProperty, testSelfEmploymentId).url
+    controllers.incomeSources.add.routes.IncomeSourceReportingMethodController.show(isAgent = true, ForeignProperty).url
   }
 
   val addUkPropReportingMethodUrl: String = {
-    controllers.incomeSources.add.routes.IncomeSourceReportingMethodController.show(isAgent = true, UkProperty, testSelfEmploymentId).url
+    controllers.incomeSources.add.routes.IncomeSourceReportingMethodController.show(isAgent = true, UkProperty).url
   }
 
   def errorPageUrl(incomeSourceType: IncomeSourceType): String = controllers.incomeSources.add.routes.IncomeSourceNotAddedController.showAgent(incomeSourceType).url
