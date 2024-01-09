4b2b3eca2086bb82225887a18987b8ed3989e8f5
avelx <3528155+avelx@users.noreply.github.com>
2024-01-09 15:56:08 +0000

    MISUV-6898:   *  add example applying "case class" rewrite across ViewModels;

--- rules/src/scala/fix/CaseClassRewrite.scala
+++ rules/src/scala/fix/CaseClassRewrite.scala
@@ -8,8 +8,12 @@ class CaseClassRewrite extends SemanticRule("CaseClassRewrite") {
 
   override def fix(implicit doc: SemanticDocument): Patch = {
     doc.tree.collect {
-      case t @ Defn.Class.After_4_6_0(a, className, c, constructorDef, extendParentClassOrTrait) if a.toString() == "List(case)" =>
-        //println(s"$className ==> $extendParentClassOrTrait ===> $constructorDef")
+      // Example of applying specifically to ViewModels across FE
+      //      case t@Defn.Class.After_4_6_0(a, className, c, constructorDef, extendParentClassOrTrait) if a.toString() == "List(case)" && className.toString().contains("ViewModel") =>
+      //      println(s"$className ==> $extendParentClassOrTrait ===> $constructorDef")
+      //      Patch.addLeft(t, "final ")
+      case t@Defn.Class.After_4_6_0(a, className, c, constructorDef, extendParentClassOrTrait) if a.toString() == "List(case)" =>
+        println(s"$className ==> $extendParentClassOrTrait ===> $constructorDef")
         Patch.addLeft(t, "final ")
     }.asPatch
   }
