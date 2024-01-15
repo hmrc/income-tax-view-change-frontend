a5ffa60cfc1a3f33412eb2b985356357edd86a59
avelx <3528155+avelx@users.noreply.github.com>
2024-01-15 13:55:59 +0000

    I

--- rules/ImportRewriteRules.scala
+++ rules/ImportRewriteRules.scala
@@ -7,9 +7,22 @@ import scalafix.patch.Patch
 
 class ImportRewriteRules extends SemanticRule("LocalDate.now") {
 
+
   override def fix(implicit doc: SemanticDocument): Patch = {
-    Patch.replaceSymbols(
-      "scala.concurrent.ExecutionContext.Implicits.global" -> "play.api.libs.concurrent.Execution.Implicits.defaultContext",
-    ) + Patch.addGlobalImport(importer"uk.gov.hmrc.mongo.test.CleanMongoCollectionSupport")
+    doc.tree.collect {
+
+      case t @ importer"scala.concurrent.ExecutionContext.Implicits.global"
+      =>
+        importees.foldLeft(Patch.empty)(_ + Patch.removeImportee(_))
+        Patch.addGlobalImport(importer"uk.gov.hmrc.mongo.test.CleanMongoCollectionSupport")
+      case _ =>
+        Patch.empty
+    }
   }
+
+//  override def fix(implicit doc: SemanticDocument): Patch = {
+//    Patch.replaceSymbols(
+//      "scala.concurrent.ExecutionContext.Implicits.global" -> "play.api.libs.concurrent.Execution.Implicits.defaultContext",
+//    ) + Patch.addGlobalImport(importer"uk.gov.hmrc.mongo.test.CleanMongoCollectionSupport")
+//  }
 }
