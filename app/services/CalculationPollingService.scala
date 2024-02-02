a7ec2789e0178842619bc2fd8525b0b438cc959f
avelx <3528155+avelx@users.noreply.github.com>
2024-02-02 10:29:12 +0000

    MISUV-6871:   * - move delay between attempt and number of attempt into application.conf;

--- app/services/CalculationPollingService.scala
+++ app/services/CalculationPollingService.scala
@@ -62,7 +62,10 @@ class CalculationPollingService @Inject()(val frontendAppConfig: FrontendAppConf
 
           // Ref: https://pekko.apache.org/docs/pekko/current/futures.html
           // V1: apply retry with a fixed delay between calls
-          retry(() => attemptToPollCalc(calcId, nino, taxYear, mtditid, endTimeInMillis), attempts = 10, delay = 500.milliseconds)
+          retry(() =>
+            attemptToPollCalc(calcId, nino, taxYear, mtditid, endTimeInMillis),
+            attempts = frontendAppConfig.calcPollNumberOfAttempts,
+            delay = frontendAppConfig.calcPollDelayBetweenAttempts.millisecond)
 
           // V2: apply retry with with backOff strategy
           //retry(() => futureToAttempt(), attempts = 10, minBackoff = 1.second,  maxBackoff = 10.seconds, randomFactor = 0.5)
