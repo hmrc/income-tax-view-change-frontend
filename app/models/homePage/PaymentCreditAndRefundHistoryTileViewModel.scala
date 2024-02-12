fd7886b34a1644ab870b15d25542f19b5e309796
Edward Dennis <98972365+EdwardDennis@users.noreply.github.com>
2024-02-12 13:31:59 +0000

    MISUV-7063 'Payment and refund history' page heading, title and V&C homepage tile link update (#2103)
    * Create PaymentCreditAndRefundHistoryTileViewModel
    
    * Add title method preserving existing logic to view model
    
    * Fix non-exhaustive match
    
    * Add view model to home page
    
    * Use view model title method in view
    
    * Add availableCredit to view model
    
    * Fix availableCredit logic/tests
    
    * Remove availableCredit param from view
    
    * Add conditional link logic/tests
    
    * Create PaymentCreditAndRefundHistoryViewModel and transfer existing logic
    
    * Add viewModel to view and move boolean logic to viewModel
    
    * Add new titles
    
    * Add tests for new titles
    
    * Delete junk file
    
    * Fix test
    
    * Fix formatting
    
    * Fix IT

---
+++ app/models/homePage/PaymentCreditAndRefundHistoryTileViewModel.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.homePage
+
+import models.financialDetails.{FinancialDetailsModel, FinancialDetailsResponseModel}
+import play.api.i18n.Messages
+
+case class PaymentCreditAndRefundHistoryTileViewModel(unpaidCharges: List[FinancialDetailsResponseModel],
+                                                      creditsRefundsRepayEnabled: Boolean, paymentHistoryRefundsEnabled: Boolean) {
+  val availableCredit: Option[BigDecimal] =
+    if (creditsRefundsRepayEnabled) {
+      Some(unpaidCharges.collectFirst {
+        case fdm: FinancialDetailsModel =>
+          fdm.balanceDetails.getAbsoluteAvailableCreditAmount.getOrElse(BigDecimal(0.00))
+      }.getOrElse(BigDecimal(0.00)))
+    } else None
+
+  def title()(implicit messages: Messages): String = if (paymentHistoryRefundsEnabled) {
+    messages("home.paymentHistoryRefund.heading")
+  } else {
+    messages("home.paymentHistory.heading")
+  }
+
+  def paymentCreditRefundHistoryMessageKey(): String = (creditsRefundsRepayEnabled, paymentHistoryRefundsEnabled) match {
+    case (false, true) => "home.paymentHistoryRefund.view"
+    case (true, false) => "home.paymentCreditHistory.view"
+    case (true, true) => "home.paymentCreditRefundHistory.view"
+    case (false, false) => "home.paymentHistory.view"
+  }
+
+}
