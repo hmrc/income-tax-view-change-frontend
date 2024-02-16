4722048b76bd1589f9a575c8b6798518562abe86
JordHemsworth <88255447+JordHemsworth@users.noreply.github.com>
2024-02-16 14:58:55 +0000

    MISUV-7088-v2- setup view model

--- app/models/creditsandrefunds/CreditAndRefundViewModel.scala
+++ app/models/creditsandrefunds/CreditAndRefundViewModel.scala
@@ -16,9 +16,61 @@
 
 package models.creditsandrefunds
 
+import models.creditDetailModel.{BalancingChargeCreditType, CreditType, CutOverCreditType, MfaCreditType}
 import models.financialDetails.{DocumentDetailWithDueDate, FinancialDetail}
 
 
-case class CreditAndRefundViewModel(creditCharges: List[(DocumentDetailWithDueDate, FinancialDetail)])
+case class CreditAndRefundViewModel(creditCharges: List[(DocumentDetailWithDueDate, FinancialDetail)]) {
+
+
+  private val balancingChargeCredit = "BCC"
+  private val mfaCredit = "MFA"
+  private val cutOverCredit = "CutOver"
+  private val payment = "Payment"
+
+  def sortCreditsByTypeAndMonetaryValue(credits: List[(DocumentDetailWithDueDate, FinancialDetail)])
+  : List[(DocumentDetailWithDueDate, FinancialDetail)] = {
+
+    val creditTypeSortingOrder = Map(
+      balancingChargeCredit -> 1,
+      mfaCredit -> 2,
+      cutOverCredit -> 3,
+      payment -> 4
+    )
+
+    val sortedCredits = credits.groupBy[String] {
+      credits => {
+        getCreditType(credits)
+      }
+    }.toList.sortWith((p1, p2) => creditTypeSortingOrder(p1._1) < creditTypeSortingOrder(p2._1))
+      .map {
+        case (documentDetailWithDueDate, financialDetail) => (documentDetailWithDueDate, sortCreditsByMonetaryValue(financialDetail))
+      }.flatMap {
+      case (_, credits) => credits
+    }
+    sortedCredits
+  }
+
+  def getCreditType(credit: (DocumentDetailWithDueDate, FinancialDetail)): String = {
+    val creditType: Option[CreditType] = credit._2.getCreditType
+    val isPayment: Boolean = credit._1.documentDetail.paymentLot.isDefined
+
+    (creditType, isPayment) match {
+      case (Some(BalancingChargeCreditType), false) => balancingChargeCredit
+      case (Some(MfaCreditType), false) => mfaCredit
+      case (Some(CutOverCreditType), false) => cutOverCredit
+      case (None, true) => payment
+      case (_, _) => "Unknown credit"
+    }
+  }
+
+  def sortCreditsByMonetaryValue(credits: List[(DocumentDetailWithDueDate, FinancialDetail)])
+  : List[(DocumentDetailWithDueDate, FinancialDetail)] = {
+    credits
+      .sortBy(_._1.documentDetail.paymentOrChargeCredit).reverse
+  }
+
+}
+
 
 
