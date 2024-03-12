e48e822f931bbd2cbbd56ca7ffe8a8d9ceaa405e
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2024-03-12 16:22:49 +0000

    move unpaidChargesFuture into for loop

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -99,10 +99,9 @@ class HomeController @Inject()(val homeView: views.html.Home,
     val incomeSourceCurrentTaxYear: Int = dateService.getCurrentTaxYearEnd
     val currentDate = dateService.getCurrentDate
     val nextUpdatesTileViewModel = NextUpdatesTileViewModel(nextUpdatesDueDates, currentDate)
-    val unpaidChargesFuture: Future[List[FinancialDetailsResponseModel]] = financialDetailsService.getAllUnpaidFinancialDetails(isEnabled(CodingOut))
 
     for {
-      unpaidCharges             <- unpaidChargesFuture
+      unpaidCharges             <- financialDetailsService.getAllUnpaidFinancialDetails(isEnabled(CodingOut))
       paymentsDue                = getDueDates(unpaidCharges)
       dunningLockExists          = unpaidCharges.collectFirst { case fdm: FinancialDetailsModel if fdm.dunningLockExists => true }.getOrElse(false)
       outstandingChargesModel   <- getOutstandingChargesModel(unpaidCharges)
