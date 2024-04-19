3bc0aa6387deedaf50cdc0601b36e3ca9570fc9e
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2024-04-19 16:46:08 +0100

    implemented fetch of 1553 data and finding the relevant tax year associated with this WIP attempt 1

--- app/services/ClaimToAdjustService.scala
+++ app/services/ClaimToAdjustService.scala
@@ -16,9 +16,12 @@
 
 package services
 
+import auth.MtdItUser
 import connectors.FinancialDetailsConnector
 import models.core.Nino
+import models.financialDetails.{DocumentDetail, FinancialDetailsModel}
 import models.incomeSourceDetails.TaxYear
+import models.incomeSourceDetails.TaxYear.makeTaxYearWithEndYear
 import uk.gov.hmrc.http.HeaderCarrier
 
 import javax.inject.{Inject, Singleton}
@@ -29,8 +32,33 @@ class ClaimToAdjustService @Inject()(val financialDetailsConnector: FinancialDet
                                      val financialDetailsService: FinancialDetailsService)
                                     (implicit ec: ExecutionContext) {
 
-  def canCustomerClaimToAdjust(nino: String)(implicit hc: HeaderCarrier): Future[Boolean] = ???
+  def canCustomerClaimToAdjust(implicit hc: HeaderCarrier, user: MtdItUser[_]): Future[Boolean] = {
+    maybePoATaxYear.map {
+      case Some(_) => true
+      case None => false
+    }
+  }
 
-  def maybePoATaxYear(nino: String)(implicit hc: HeaderCarrier): Future[Option[TaxYear]] = ???
+  def maybePoATaxYear(implicit hc: HeaderCarrier, user: MtdItUser[_]): Future[Option[TaxYear]] = {
+    val a = financialDetailsService.getAllFinancialDetails map {
+      item => item.collect({ case (_, model: FinancialDetailsModel) => getPoAPayments(model.documentDetails)})
+    }
+    a.map(_.headOption.flatten)
+  }
+
+  def getPoAPayments(documentDetails: List[DocumentDetail]): Option[TaxYear] = {
+    val poa1: List[DocumentDetail] = documentDetails.filter(_.documentDescription.exists(_.equals("ITSA- POA 1")))
+    val poa2: List[DocumentDetail] = documentDetails.filter(_.documentDescription.exists(_.equals("ITSA - POA 2")))
+
+    if ((poa1.length == 1) && (poa2.length == 1)) {
+      if (poa1.head.taxYear == poa2.head.taxYear) {
+        Some(makeTaxYearWithEndYear(poa1.head.taxYear))
+      } else {
+        None
+      }
+    } else {
+      None
+    }
+  }
 
 }
