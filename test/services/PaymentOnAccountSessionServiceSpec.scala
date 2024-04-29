628d54dc4a23c3e4b6d66c04562329ed6db9e7ff
Pavel <3528155+avelx@users.noreply.github.com>
2024-04-29 14:22:06 +0100

    Misuv 7485 mongo repo: WIP (#2250)
    * Created repo, service and data model
    
    * Added UTs for the service, added get by key
    
    * MISUV-7485:
      * - add comments, required index, rename collection to be unique;
    
    * Changed model layout
    
    * Added integration tests for repo, fixed typos
    
    * Added methods to set individual fields, added tests
    
    * Changed methods for more transparency and added tests
    
    ---------
    
    Co-authored-by: danielmaltz1 <117353102+danielmaltz1@users.noreply.github.com>

---
+++ test/services/PaymentOnAccountSessionServiceSpec.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import models.paymentOnAccount.{PoAAmendmentData, PoASessionData}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{mock, when}
+import org.scalactic.Fail
+import repositories.PoAAmendmentDataRepository
+import testUtils.TestSupport
+
+import scala.concurrent.Future
+
+class PaymentOnAccountSessionServiceSpec extends TestSupport {
+
+  val mockRepository: PoAAmendmentDataRepository = mock(classOf[PoAAmendmentDataRepository])
+
+  object TestPaymentOnAccountSessionService extends PaymentOnAccountSessionService(
+    mockRepository)
+
+  val ammendmentData: PoAAmendmentData = PoAAmendmentData(poaAdjustmentReason = Some("reason X"), newPoAAmount = None)
+
+  val sessionData: PoASessionData = PoASessionData(
+    sessionId = "session-123456",
+    poaAmendmentData = Some(ammendmentData)
+  )
+
+  "PaymentOnAccountSessionService.createSession" should {
+    "create the mongo session" in {
+      when(mockRepository.set(any())).thenReturn(Future.successful(true))
+      val result = TestPaymentOnAccountSessionService.createSession
+      result.futureValue shouldBe true
+    }
+  }
+  "PaymentOnAccountSessionService.getMongo" should {
+    "return the correct mongo data" in {
+      when(mockRepository.get(any())).thenReturn(Future.successful(Some(sessionData)))
+      TestPaymentOnAccountSessionService.getMongo(headerCarrier, ec).futureValue shouldBe Right(Some(ammendmentData))
+    }
+  }
+  "PaymentOnAccountSessionService.setMongoData" should {
+    "set the mongo value" in {
+      when(mockRepository.set(any())).thenReturn(Future.successful(true))
+      val result = TestPaymentOnAccountSessionService.setMongoData(Some(ammendmentData))
+      result.futureValue shouldBe true
+    }
+  }
+  "PaymentOnAccountSessionService.setAdjustmentReason" should {
+    "update the adjustment reason" in {
+      when(mockRepository.get(any())).thenReturn(Future.successful(Some(sessionData)))
+      when(mockRepository.set(any())).thenReturn(Future.successful(true))
+      val result = TestPaymentOnAccountSessionService.setAdjustmentReason("Just because")
+      result.futureValue shouldBe Right(())
+    }
+    "return an error" when {
+      "no mongo session can be found" in {
+        when(mockRepository.get(any())).thenReturn(Future.successful(None))
+        val result = TestPaymentOnAccountSessionService.setAdjustmentReason("Just because")
+        result.futureValue match {
+          case Left(ex) => ex.getMessage shouldBe "No active mongo session found"
+          case _ => Fail
+        }
+      }
+    }
+  }
+  "PaymentOnAccountSessionService.setNewPoAAmount" should {
+    "update the PoA amount" in {
+      when(mockRepository.get(any())).thenReturn(Future.successful(Some(sessionData)))
+      when(mockRepository.set(any())).thenReturn(Future.successful(true))
+      val result = TestPaymentOnAccountSessionService.setNewPoAAmount(100.00)
+      result.futureValue shouldBe Right(())
+    }
+    "return an error" when {
+      "no mongo session can be found" in {
+        when(mockRepository.get(any())).thenReturn(Future.successful(None))
+        val result = TestPaymentOnAccountSessionService.setNewPoAAmount(100.00)
+        result.futureValue match {
+          case Left(ex) => ex.getMessage shouldBe "No active mongo session found"
+          case _ => Fail
+        }
+      }
+    }
+  }
+}
