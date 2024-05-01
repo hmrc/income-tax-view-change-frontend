4bd2c46f7a4eab25c2499e0ff4bf2c3246c42787
james-king-hmrc <165788042+james-king-hmrc@users.noreply.github.com>
2024-05-01 21:51:10 +0100

    update

--- app/services/optout/OptOutRulesMain.scala
+++ app/services/optout/OptOutRulesMain.scala
@@ -16,67 +16,24 @@
 
 package services.optout
 
-import services.optout.OptOutRulesService._
+import scala.io.Source
+import scala.util.matching.Regex
 
-/* todo: to be removed */
 object OptOutRulesMain extends App {
 
   val service = new OptOutRulesService()
 
-  val queryStrings: List[(String, String)] = List(
-    (toQuery("N", "M", "M", "V"), "NY"),
-    (toQuery("Y", "M", "M", "V"), "NY"),
+  private val queryLines: List[String] = Source.fromInputStream(getClass.getResourceAsStream("/optout-rules-queries.txt"))
+    .getLines().toList
 
-    (toQuery("Y", "M", "V", "U"), "CY,NY"),
-    (toQuery("N", "M", "V", "U"), "CY,NY"),
-
-    (toQuery("Y", "M", "V", "V"), "CY,NY"),
-    (toQuery("N", "M", "V", "V"), "CY,NY"),
-
-    (toQuery("Y", "M", "V", "A"), "CY"),
-    (toQuery("N", "M", "V", "A"), "CY"),
-
-    (toQuery("Y", "M", "A", "V"), "NY"),
-    (toQuery("N", "M", "A", "V"), "NY"),
-
-    ("N,V,M,M", "PY"),
-    ("N,V,M,U", "PY"),
-
-    ("N,V,M,V", "NY,PY"),
-
-    //("N,V,M,V", "PY"),
-    ("Y,V,M,A", "PY"),
-
-//    (toQuery("N", "V", "U", "U"), "PY"),
-//    (toQuery("N", "U", "V", "U"), "CY,NY"),
-//    (toQuery("N", "U", "U", "V"), "NY"),
-//    (toQuery("N", "U", "V", "V"), ""),
-//
-//    (toQuery("Y", "V", "U", "U"), ""),
-//    (toQuery("Y", "U", "V", "U"), "CY,NY"),
-//    (toQuery("Y", "U", "U", "V"), "NY"),
-//    (toQuery("Y", "U", "V", "V"), ""),
-  )
-
-//  println()
-//  println("Rules In File:")
-//  service.onlyRuleLines.foreach(println)
-
-//  println()
-//  println("Queries:")
-//  queryStrings.foreach(q => println(s"$q"))
-
-//  println()
-//  println("OptOut Options:")
-//  queryStrings.map(q => (q, service.findOptOutOptions(q).mkString(",")))
-//    .map(t => (t._1, if(t._2.isEmpty) "NO" else t._2))
-//    .foreach(t => println(s"${t._1} -> \t${t._2}"))
-
-
-  queryStrings.foreach(t => {
-    val outcome = service.findOptOutOptions(t._1).mkString(",")
-    println(s"${t._1}:-> $outcome")
-    assert(outcome == t._2, s"query: ${t._1} found: $outcome, expected: ${t._2}")
-  })
+  private val optOutQueryVsOptionRegex: Regex = """^(.*?,.*?,.*?,.*?),(.*?)""".r
+  queryLines.foreach {
+    case optOutQueryVsOptionRegex(query, expectedOptOutOption) =>
+      val expectedOptOutOptionFormat = expectedOptOutOption.split("-").sortBy(_.trim).mkString(",")
+      val outcome = service.findOptOutOptions(query).mkString(",")
+      println(s"$query:-> $outcome")
+      assert(outcome == expectedOptOutOptionFormat, s"query: $query found: $outcome, expected: $expectedOptOutOptionFormat")
+    case _ => println("Error")
+  }
 
 }
