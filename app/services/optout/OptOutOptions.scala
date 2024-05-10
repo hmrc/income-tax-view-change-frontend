1798853e58d7352a8e4edad811dc34cf2610f0f1
james-king-hmrc <165788042+james-king-hmrc@users.noreply.github.com>
2024-05-10 15:07:21 +0100

    return the tax years that should be updated for optout explicitly

--- app/services/optout/OptOutOptions.scala
+++ app/services/optout/OptOutOptions.scala
@@ -53,5 +53,18 @@ case class OptOutData(previousTaxYear: PreviousTaxYearOptOut,
     if (countVoluntaryOptOutYears == 1) Some(function(this, availableOptOutYears.head)) else
       None
   }
+
+  def optOutCallsShouldBeMadeForYears(): Seq[TaxYear] = {
+    val isSingleYearOptOutScenario = availableOptOutYears.size == 1
+    val isMultiYearOptOutScenario = availableOptOutYears.size > 1
+    val isNoOptOutScenario = availableOptOutYears.isEmpty
+
+    (isSingleYearOptOutScenario, isMultiYearOptOutScenario, isNoOptOutScenario) match {
+      case (true, _, _) => optOutYears.filter(_.canOptOut).map(_.taxYear)
+      case (_, true, _) => /* TBD */ Seq()
+      case (_, _, true) => Seq()
+    }
+  }
+
 }
 
