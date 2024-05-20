a8b1ad8f700bc4a29df2868e5f79c167b1deaea1
omirostr1 <omiros.trypatsas@mercator.group>
2024-05-20 16:33:59 +0100

    messages added and session data method added to the controller

--- app/controllers/claimToAdjustPoa/ConfirmationForAdjustingPoaController.scala
+++ app/controllers/claimToAdjustPoa/ConfirmationForAdjustingPoaController.scala
@@ -20,13 +20,14 @@ import config.featureswitch.{AdjustPaymentsOnAccount, FeatureSwitching}
 import config.{AgentItvcErrorHandler, FrontendAppConfig, ItvcErrorHandler}
 import controllers.agent.predicates.ClientConfirmedController
 import controllers.routes.HomeController
-import models.claimToAdjustPoa.PaymentOnAccountViewModel
+import models.claimToAdjustPoa.PoAAmendmentData
 import models.core.Nino
 import play.api.Logger
 import play.api.i18n.I18nSupport
 import play.api.mvc._
-import services.ClaimToAdjustService
+import services.{ClaimToAdjustService, PaymentOnAccountSessionService}
 import uk.gov.hmrc.auth.core.AuthorisedFunctions
+import uk.gov.hmrc.http.HeaderCarrier
 import utils.AuthenticatorPredicate
 import views.html.claimToAdjustPoa.ConfirmationForAdjustingPoa
 
@@ -36,6 +37,7 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class ConfirmationForAdjustingPoaController @Inject()(val authorisedFunctions: AuthorisedFunctions,
                                                       claimToAdjustService: ClaimToAdjustService,
+                                                      val sessionService: PaymentOnAccountSessionService,
                                                       val view: ConfirmationForAdjustingPoa,
                                                       implicit val itvcErrorHandler: ItvcErrorHandler,
                                                       auth: AuthenticatorPredicate,
@@ -45,27 +47,26 @@ class ConfirmationForAdjustingPoaController @Inject()(val authorisedFunctions: A
                                                       val ec: ExecutionContext)
   extends ClientConfirmedController with I18nSupport with FeatureSwitching {
 
-  def amountIsZero(poa: PaymentOnAccountViewModel): Boolean = {
-    if (poa.totalAmount > 0) {
-      val isAmountZero: Boolean = true
-      isAmountZero
-    } else {
-      val isAmountZero: Boolean = false
-      isAmountZero
-    }
+  def isAmountZeroFromSession(implicit hc: HeaderCarrier): Future[Boolean] = sessionService.getMongo(hc, ec).flatMap {
+    case Right(Some(newPoaData: PoAAmendmentData)) =>
+      Future.successful(newPoaData.newPoAAmount.contains(BigDecimal(0)))
+    case _ => Future.failed(new Exception(s"failed to retrieve session data."))
   }
 
   def show(isAgent: Boolean): Action[AnyContent] = auth.authenticatedAction(isAgent) {
     implicit user =>
       if (isEnabled(AdjustPaymentsOnAccount)) {
-        claimToAdjustService.getPoaForNonCrystallisedTaxYear(Nino(user.nino)).flatMap {
-          case Right(Some(poa: PaymentOnAccountViewModel)) =>
-            Future.successful(Ok(view(isAgent, poa.taxYear)))
-          case Right(None) =>
-            Logger("application").error(s"Failed to create PaymentOnAccount model")
+        {
+          for {
+            poaMaybe <- claimToAdjustService.getPoaForNonCrystallisedTaxYear(Nino(user.nino))
+            isAmountZero <- isAmountZeroFromSession
+          } yield (poaMaybe, isAmountZero)
+        } flatMap {
+          case (Right(Some(poa)), isAmountZero) => Future.successful(Ok(view(isAgent, poa.taxYear, isAmountZero)))
+          case (Right(None), isAmountZero) => Logger("application").error(s"Failed to create PaymentOnAccount model, isAmountZero: $isAmountZero")
             Future.successful(showInternalServerError(isAgent))
-          case Left(ex) =>
-            Logger("application").error(s"Exception: ${ex.getMessage} - ${ex.getCause}")
+          case (Left(ex), isAmountZero) =>
+            Logger("application").error(s"Exception: ${ex.getMessage} - ${ex.getCause}. isAmountZero: $isAmountZero")
             Future.failed(ex)
         }
       } else {
@@ -81,4 +82,41 @@ class ConfirmationForAdjustingPoaController @Inject()(val authorisedFunctions: A
           showInternalServerError(isAgent)
       }
   }
+
+  //  def show(isAgent: Boolean): Action[AnyContent] = auth.authenticatedAction(isAgent) {
+  //    implicit user =>
+  //      if (isEnabled(AdjustPaymentsOnAccount)) {
+  //        {
+  //          for {
+  //            poaMaybe <- claimToAdjustService.getPoaForNonCrystallisedTaxYear(Nino(user.nino))
+  //            poaSessionData <- sessionService.getMongo.flatMap {
+  //              case Right(Some(newPoaData: PoAAmendmentData)) =>
+  //                val newPoaAmount = newPoaData.newPoAAmount
+  //                Future.successful(newPoaAmount.contains(BigDecimal(0)))
+  //              case _ => Future.failed(new Exception(s"failed to retrieve session data."))
+  //            }
+  //          } yield poaMaybe
+  //        }.value.flatMap {
+  //          case Right(Some(poa: PaymentOnAccountViewModel)) =>
+  //            Future.successful(Ok(view(isAgent, poa.taxYear)))
+  //          case Right(None) =>
+  //            Logger("application").error(s"Failed to create PaymentOnAccount model")
+  //            Future.successful(showInternalServerError(isAgent))
+  //          case Left(ex) =>
+  //            Logger("application").error(s"Exception: ${ex.getMessage} - ${ex.getCause}")
+  //            Future.failed(ex)
+  //        }
+  //      } else {
+  //        Future.successful(
+  //          Redirect(
+  //            if (isAgent) HomeController.showAgent
+  //            else HomeController.show()
+  //          )
+  //        )
+  //      }.recover {
+  //        case ex: Exception =>
+  //          Logger("application").error(s"Unexpected error: ${ex.getMessage} - ${ex.getCause}")
+  //          showInternalServerError(isAgent)
+  //      }
+  //  }
 }
