c6cf0428d5de639d410ed404e2e23e3ed4285117
owen.gilfellon <owen.gilfellon@opencastsoftware.com>
2024-06-05 10:30:42 +0100

    MISUV-7758 | Moved calculation to view model

--- app/models/claimToAdjustPoa/PaymentOnAccountViewModel.scala
+++ app/models/claimToAdjustPoa/PaymentOnAccountViewModel.scala
@@ -36,4 +36,7 @@ case class PaymentOnAccountViewModel(
     totalAmount < poaRelevantAmount
   }
 
+  def partiallyPaidAndTotalAmountLessThanPoa: Boolean =
+    poAPartiallyPaid && totalAmountLessThanPoa
+
 }
