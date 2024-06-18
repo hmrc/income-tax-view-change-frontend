ad9575ff642965a2426082d24b458274321a6fa6
owen.gilfellon <owen.gilfellon@opencastsoftware.com>
2024-06-18 11:57:20 +0100

    MISUV-7766 | Improved test coverage, added tests for adjust paid POAs

--- test/testConstants/claimToAdjustPOA/ClaimToAdjustPOATestConstants.scala
+++ test/testConstants/claimToAdjustPOA/ClaimToAdjustPOATestConstants.scala
@@ -16,11 +16,29 @@
 
 package testConstants.claimToAdjustPoa
 
-import models.financialDetails.{BalanceDetails, DocumentDetail, FinancialDetailsErrorModel, FinancialDetailsModel}
+import models.financialDetails._
 
 import java.time.LocalDate
 
 object ClaimToAdjustPOATestConstants {
+
+  def genericFinancialDetailPOA1(taxYearEnd: Int, outstandingAmount: BigDecimal = 0.0) = FinancialDetail(
+    taxYear = taxYearEnd.toString,
+    mainType = Some("SA Payment on Account 1"),
+    mainTransaction = Some("4920"),
+    outstandingAmount = Some(outstandingAmount),
+    chargeReference = Some("ABCD1234"),
+    items = None
+  )
+
+  def genericFinancialDetailPOA2(taxYearEnd: Int, outstandingAmount: BigDecimal = 0.0) = FinancialDetail(
+    taxYear = taxYearEnd.toString,
+    mainType = Some("SA Payment on Account 2"),
+    mainTransaction = Some("4930"),
+    outstandingAmount = Some(outstandingAmount),
+    chargeReference = Some("ABCD1234"),
+    items = None
+  )
 
   def genericDocumentDetailPOA1(taxYearEnd: Int) = DocumentDetail(
     taxYear = taxYearEnd,
@@ -57,6 +75,11 @@ object ClaimToAdjustPOATestConstants {
     latePaymentInterestAmount = Some(250),
     latePaymentInterestId = None
   )
+
+//  val chargeHistoryModel =
+//
+//  def testChargeHistoryModel(): ChargesHistoryModel = ChargesHistoryModel("NINO", "AB123456C", "ITSA", None)
+
 
   val userPOADetails2024: FinancialDetailsModel = FinancialDetailsModel(
     balanceDetails = BalanceDetails(1.00, 2.00, 3.00, None, None, None, None, None),
@@ -101,5 +124,7 @@ object ClaimToAdjustPOATestConstants {
   )
 
   def financialDetailsErrorModel(errorCode: Int = 404): FinancialDetailsErrorModel = FinancialDetailsErrorModel(errorCode, "There was an error...")
+
+
 
 }
