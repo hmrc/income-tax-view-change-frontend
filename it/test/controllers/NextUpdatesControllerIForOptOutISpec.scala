7d3b6a3f5fdfd8d9114f89dac2b7d912d67a35eb
james-king-hmrc <165788042+james-king-hmrc@users.noreply.github.com>
2024-06-19 12:59:00 +0100

    add it test

---
+++ it/test/controllers/NextUpdatesControllerIForOptOutISpec.scala
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import audit.models.NextUpdatesAuditing.NextUpdatesAuditModel
+import auth.MtdItUser
+import helpers.ComponentSpecBase
+import helpers.servicemocks.ITSAStatusDetailsStub.ITSAYearStatus
+import helpers.servicemocks.{AuditStub, CalculationListStub, ITSAStatusDetailsStub, IncomeTaxViewChangeStub}
+import models.admin.OptOut
+import models.incomeSourceDetails.TaxYear
+import models.itsaStatus.ITSAStatus
+import models.nextUpdates.ObligationsModel
+import play.api.http.Status._
+import play.api.test.FakeRequest
+import testConstants.BaseIntegrationTestConstants._
+import testConstants.CalculationListIntegrationTestConstants
+import testConstants.IncomeSourceIntegrationTestConstants._
+import testConstants.NextUpdatesIntegrationTestConstants._
+import uk.gov.hmrc.auth.core.AffinityGroup.Individual
+
+class NextUpdatesControllerIForOptOutISpec extends ComponentSpecBase {
+
+  "Calling the NextUpdatesController" when {
+
+    val testPropertyOnlyUser: MtdItUser[_] = MtdItUser(
+      testMtditid, testNino, None, ukPropertyOnlyResponse,
+      None, Some("1234567890"), Some("12345-credId"), Some(Individual), None
+    )(FakeRequest())
+
+    "one year opt-out scenarios" when {
+
+      "show opt-out message if the user has Previous Year as Voluntary, Current Year as NoStatus, Next Year as NoStatus" in {
+        enable(OptOut)
+
+        val currentTaxYear = dateService.getCurrentTaxYearEnd
+        val previousYear = currentTaxYear - 1
+
+        IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, propertyOnlyResponse)
+
+        IncomeTaxViewChangeStub.stubGetNextUpdates(testNino, ObligationsModel(Seq(singleObligationQuarterlyModel(testPropertyIncomeId))))
+
+        IncomeTaxViewChangeStub.stubGetFulfilledObligationsNotFound(testNino)
+        val threeYearStatus = ITSAYearStatus(ITSAStatus.Voluntary, ITSAStatus.NoStatus, ITSAStatus.NoStatus)
+        ITSAStatusDetailsStub.stubGetITSAStatusFutureYearsDetailsWithGivenThreeStatus(dateService.getCurrentTaxYearEnd, threeYearStatus)
+        CalculationListStub.stubGetLegacyCalculationList(testNino, previousYear.toString)(CalculationListIntegrationTestConstants.successResponseNotCrystallised.toString())
+
+        optOutService.saveIntent(TaxYear.forYearEnd(2024)).futureValue shouldBe true
+        val savedTaxYearOpn: Option[TaxYear] = optOutService.fetchSavedIntent().futureValue
+        savedTaxYearOpn.isDefined shouldBe true
+        savedTaxYearOpn.get shouldBe TaxYear.forYearEnd(2024)
+
+        val res = IncomeTaxViewChangeFrontend.getNextUpdates
+
+        val afterResetOpn: Option[TaxYear] = optOutService.fetchSavedIntent().futureValue
+        //assert intent tax-year is reset to none
+        afterResetOpn.isDefined shouldBe false
+
+        AuditStub.verifyAuditEvent(NextUpdatesAuditModel(testPropertyOnlyUser))
+
+        verifyIncomeSourceDetailsCall(testMtditid)
+        verifyNextUpdatesCall(testNino)
+        IncomeTaxViewChangeStub.verifyGetObligations(testNino)
+
+        Then("the quarterly updates info sections")
+        res should have(
+          elementTextBySelector("#one-year-opt-out-message")(expectedValue = "You are currently reporting quarterly on a " +
+            "voluntary basis for the 2021 to 2022 tax year. You can choose to opt out of quarterly updates and " +
+            "report annually instead.")
+        )
+
+      }
+
+      "show multi year opt-out message if the user has Previous Year as Voluntary, Current Year as Voluntary, Next Year as Voluntary" in {
+        enable(OptOut)
+
+        val currentTaxYear = dateService.getCurrentTaxYearEnd
+        val previousYear = currentTaxYear - 1
+
+        IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, propertyOnlyResponse)
+
+        IncomeTaxViewChangeStub.stubGetNextUpdates(testNino, ObligationsModel(Seq(singleObligationQuarterlyModel(testPropertyIncomeId))))
+
+        IncomeTaxViewChangeStub.stubGetFulfilledObligationsNotFound(testNino)
+        val threeYearStatus = ITSAYearStatus(ITSAStatus.Voluntary, ITSAStatus.Voluntary, ITSAStatus.Voluntary)
+        ITSAStatusDetailsStub.stubGetITSAStatusFutureYearsDetailsWithGivenThreeStatus(dateService.getCurrentTaxYearEnd, threeYearStatus)
+        CalculationListStub.stubGetLegacyCalculationList(testNino,
+          previousYear.toString)(CalculationListIntegrationTestConstants.successResponseNotCrystallised.toString())
+
+        optOutService.saveIntent(TaxYear.forYearEnd(2024)).futureValue shouldBe true
+        val savedTaxYearOpn: Option[TaxYear] = optOutService.fetchSavedIntent().futureValue
+        savedTaxYearOpn.isDefined shouldBe true
+        savedTaxYearOpn.get shouldBe TaxYear.forYearEnd(2024)
+
+        val res = IncomeTaxViewChangeFrontend.getNextUpdates
+
+        val afterResetOpn: Option[TaxYear] = optOutService.fetchSavedIntent().futureValue
+        //assert intent tax-year is reset to none
+        afterResetOpn.isDefined shouldBe false
+
+        AuditStub.verifyAuditEvent(NextUpdatesAuditModel(testPropertyOnlyUser))
+
+        verifyIncomeSourceDetailsCall(testMtditid)
+        verifyNextUpdatesCall(testNino)
+        IncomeTaxViewChangeStub.verifyGetObligations(testNino)
+
+        Then("the quarterly updates info sections")
+        res should have(
+          elementTextBySelector("#multi-year-opt-out-message")(expectedValue = "You are currently reporting quarterly on a " +
+            "voluntary basis. You can choose to opt out of quarterly updates and report annually instead.")
+        )
+      }
+    }
+
+  }
+
+}
