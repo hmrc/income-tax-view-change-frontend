61a513e87a3487c9ff7f851fa904435d4370963b
owen.gilfellon <owen.gilfellon@opencastsoftware.com>
2024-06-24 07:53:07 +0100

    MISUV-7444 | Removed payment fix (MISUV-7559)

--- test/audit/models/PaymentHistoryResponseAuditModelSpec.scala
+++ test/audit/models/PaymentHistoryResponseAuditModelSpec.scala
@@ -51,7 +51,9 @@ class PaymentHistoryResponseAuditModelSpec extends TestSupport {
       Payment(reference = Some("payment1"), amount = Some(100.00), outstandingAmount = None,
         method = Some("method"), documentDescription = None, lot = Some("lot"), lotItem = Some("lotItem"),
         dueDate = Some(LocalDate.parse("2018-02-01")), documentDate = LocalDate.parse("2018-02-05"), None,
-        mainType = Some("Payment"), mainTransaction = Some("0060")),
+        mainType = Some("Payment"), mainTransaction = Some("4920")),
+      // TODO: MISUV-7559
+      // mainType = Some("Payment"), mainTransaction = Some("0060")),
       Payment(reference = Some("cutover1"), amount = Some(-100.00), outstandingAmount = None,
         method = Some("method"), documentDescription = None, lot = None, lotItem = None, dueDate = Some(LocalDate.parse("2018-02-02")),
         documentDate = LocalDate.parse("2018-02-05"), None, mainType = Some("ITSA Cutover Credits"), mainTransaction = Some("6110")),
