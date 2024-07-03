10cbb30628116aba2e6d613c023c14505bebfe15
simondrugan16 <119412400+simondrugan16@users.noreply.github.com>
2024-07-03 15:17:26 +0100

    changed accepted getCalcList response from 204 -> 404 and added logging in this area

--- app/services/claimToAdjustPoa/ClaimToAdjustHelper.scala
+++ app/services/claimToAdjustPoa/ClaimToAdjustHelper.scala
@@ -26,6 +26,7 @@ import models.core.Nino
 import models.financialDetails.DocumentDetail
 import models.incomeSourceDetails.TaxYear
 import models.incomeSourceDetails.TaxYear.makeTaxYearWithEndYear
+import play.api.Logger
 import services.DateServiceInterface
 import uk.gov.hmrc.http.{HeaderCarrier, InternalServerException}
 
@@ -90,7 +91,9 @@ trait ClaimToAdjustHelper {
         case Some(detailsList) => Right(detailsList.headOption)
         case None => Right(None)
       }
-      case ChargesHistoryErrorModel(code, message) => Left(new Exception(s"Error retrieving charge history code: $code message: $message"))
+      case ChargesHistoryErrorModel(code, message) =>
+        Logger("application").error("chargeHistoryConnector.getChargeHistory returned a non-valid response")
+        Left(new Exception(s"Error retrieving charge history code: $code message: $message"))
     }
   }
 
@@ -102,8 +105,12 @@ trait ClaimToAdjustHelper {
     } else {
       calculationListConnector.getCalculationList(nino, taxYear.formatTaxYearRange).flatMap {
         case res: CalculationListModel => Future.successful(res.crystallised.getOrElse(false))
-        case err: CalculationListErrorModel if err.code == 204 => Future.successful(false)
-        case err: CalculationListErrorModel => Future.failed(new InternalServerException(err.message))
+        case err: CalculationListErrorModel if err.code == 404 =>
+          Logger("application").info("User had no calculations for this tax year, therefore is non-crystallised")
+          Future.successful(false)
+        case err: CalculationListErrorModel =>
+          Logger("application").error("getCalculationList returned a non-valid response")
+          Future.failed(new InternalServerException(err.message))
       }.map(!_)
     }
   }
@@ -164,7 +171,9 @@ trait ClaimToAdjustHelper {
     )
   }) match {
     case Some(model) => Right(model)
-    case None        => Left(new Exception("Failed to create AmendablePoaViewModel"))
+    case None        =>
+      Logger("application").error("Failed to create AmendablePoaViewModel")
+      Left(new Exception("Failed to create AmendablePoaViewModel"))
   }
 
   protected def isSubsequentAdjustment(chargeHistoryConnector: ChargeHistoryConnector, chargeReference: Option[String])
@@ -172,7 +181,9 @@ trait ClaimToAdjustHelper {
     chargeHistoryConnector.getChargeHistory(user.nino, chargeReference) map {
       case ChargesHistoryModel(_, _, _, Some(charges)) if charges.filter(_.isPoA).exists(_.poaAdjustmentReason.isDefined) => Right(true)
       case ChargesHistoryModel(_, _, _, _)                                                                                => Right(false)
-      case ChargesHistoryErrorModel(code, message) => Left(new Exception(s"Error retrieving charge history code: $code message: $message"))
+      case ChargesHistoryErrorModel(code, message) =>
+        Logger("application").error("getChargeHistory returned a non-valid response")
+        Left(new Exception(s"Error retrieving charge history code: $code message: $message"))
     }
   }
 }
