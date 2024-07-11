de2b340ce1eb8ca8e9eddbb6e75ac83f26fa46d5
Aashvin <55795472+Aashvin@users.noreply.github.com>
2024-07-11 14:14:41 +0100

    Review edit: revert to use old message that was previously removed

--- app/forms/incomeSources/cease/IncomeSourceEndDateForm.scala
+++ app/forms/incomeSources/cease/IncomeSourceEndDateForm.scala
@@ -33,9 +33,7 @@ import javax.inject.Inject
 class IncomeSourceEndDateForm @Inject()(val dateService: DateService)(implicit val appConfig: FrontendAppConfig)
   extends CustomConstraints with FeatureSwitching {
 
-  val dateMustBeCompleteSE = "dateForm.error.dayMonthAndYear.required.se"
-  val dateMustBeCompleteUK = "dateForm.error.dayMonthAndYear.required.uk"
-  val dateMustBeCompleteFP = "dateForm.error.dayMonthAndYear.required.fp"
+  val dateMustBeComplete = "dateForm.error.dayMonthAndYear.required"
   val dateMustNotBeMissingDayField = "dateForm.error.day.required"
   val dateMustNotBeMissingMonthField = "dateForm.error.month.required"
   val dateMustNotBeMissingYearField = "dateForm.error.year.required"
@@ -90,7 +88,7 @@ class IncomeSourceEndDateForm @Inject()(val dateService: DateService)(implicit v
         "month" -> default(text(), ""),
         "year" -> default(text(), ""))
         .verifying(firstError(
-          checkRequiredFields(incomeSourceType),
+          checkRequiredFields,
           validDate(dateMustNotBeInvalid(incomeSourceType, newIncomeSourceJourney))
         )).transform[LocalDate](
           {
@@ -102,10 +100,9 @@ class IncomeSourceEndDateForm @Inject()(val dateService: DateService)(implicit v
       )(DateFormElement.apply)(DateFormElement.unapply))
   }
 
-  private def checkRequiredFields(incomeSourceType: IncomeSourceType): Constraint[(String, String, String)] =
-    Constraint("constraints.requiredFields") {
+  private def checkRequiredFields: Constraint[(String, String, String)] = Constraint("constraints.requiredFields") {
     case (day, month, year) if day.trim.isEmpty && month.trim.isEmpty && year.trim.isEmpty =>
-      Invalid(Seq(ValidationError(dateMustBeCompleteKey(incomeSourceType))))
+      Invalid(Seq(ValidationError(dateMustBeComplete)))
     case (day, month, year) if day.trim.isEmpty && month.trim.nonEmpty && year.trim.isEmpty =>
       Invalid(Seq(ValidationError(dateMustNotBeMissingDayAndYearField)))
     case (day, month, year) if day.trim.isEmpty && month.trim.isEmpty && year.trim.nonEmpty =>
@@ -121,11 +118,4 @@ class IncomeSourceEndDateForm @Inject()(val dateService: DateService)(implicit v
     case _ =>
       Valid
   }
-
-  private def dateMustBeCompleteKey(incomeSourceType: IncomeSourceType): String = incomeSourceType match {
-    case SelfEmployment => dateMustBeCompleteSE
-    case UkProperty => dateMustBeCompleteUK
-    case ForeignProperty => dateMustBeCompleteFP
-  }
-
 }
