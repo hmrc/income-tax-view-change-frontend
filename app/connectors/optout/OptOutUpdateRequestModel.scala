c3e9b88ab0b70b4329d380b5f9fc884fc2c64cf9
james-king-hmrc <165788042+james-king-hmrc@users.noreply.github.com>
2024-08-02 13:11:07 +0100

    remove status

--- app/connectors/optout/OptOutUpdateRequestModel.scala
+++ app/connectors/optout/OptOutUpdateRequestModel.scala
@@ -17,21 +17,18 @@
 package connectors.optout
 
 import play.api.libs.json.{Format, Json}
-import play.mvc.Http.Status.{INTERNAL_SERVER_ERROR, NO_CONTENT}
+import play.mvc.Http.Status.NO_CONTENT
 
 object OptOutUpdateRequestModel {
 
   val optOutUpdateReason: String = "10"
 
   case class OptOutUpdateRequest(taxYear: String, updateReason: String)
-  sealed trait OptOutUpdateResponse {
-    val statusCode: Int
-  }
+  sealed trait OptOutUpdateResponse
+
   case class OptOutUpdateResponseSuccess(correlationId: String, statusCode: Int = NO_CONTENT) extends OptOutUpdateResponse
   case class ErrorItem(code: String, reason: String)
-  case class OptOutUpdateResponseFailure(failures: List[ErrorItem]) extends OptOutUpdateResponse {
-    val statusCode: Int = INTERNAL_SERVER_ERROR
-  }
+  case class OptOutUpdateResponseFailure(failures: List[ErrorItem]) extends OptOutUpdateResponse
 
   object OptOutUpdateResponseFailure {
     def defaultFailure(): OptOutUpdateResponseFailure =
