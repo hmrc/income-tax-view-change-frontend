fc4a77a7ae8c027c3a5557a6fbecd7b45cac108f
tharunprasath <65491423+tharunprasath@users.noreply.github.com>
2024-09-04 07:46:46 +0100

    MISUV-8197 Income Source (Manage) - Content changes 'Manage your details' page - Separate reporting frequency URLs for 'manage-your-businesses' to add the test header after income source creation.

--- app/connectors/BusinessDetailsConnector.scala
+++ app/connectors/BusinessDetailsConnector.scala
@@ -86,8 +86,16 @@ class BusinessDetailsConnector @Inject()(val http: HttpClient,
   def getIncomeSources()(
     implicit headerCarrier: HeaderCarrier, mtdItUser: MtdItUserOptionNino[_]): Future[IncomeSourceDetailsResponse] = {
 
-    //Check and add test headers Gov-Test-Scenario for dynamic stub Income Sources Created Scenarios
-    val hc = checkAndAddTestHeader(mtdItUser.path, headerCarrier, appConfig.incomeSourceOverrides(), "afterIncomeSourceCreated")
+    //Check and add test headers Gov-Test-Scenario for dynamic stub Income Sources Created Scenarios for Income Source Journey
+    val manageBusinessesPattern = """.*/manage-your-businesses/.*""".r
+
+    val hc = mtdItUser.path match {
+      case manageBusinessesPattern(_*) =>
+        checkAndAddTestHeader(mtdItUser.path, headerCarrier, appConfig.incomeSourceOverrides(), "afterIncomeSourceCreated")
+      case _ =>
+        headerCarrier
+    }
+
 
     val url = getIncomeSourcesUrl(mtdItUser.mtditid)
     Logger("application").debug(s"GET $url")
