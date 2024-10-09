201858df6ef58e699f6403d6874e404acd9689d4
jmountain-hmrc <31282623+jmountain-hmrc@users.noreply.github.com>
2024-10-09 10:16:52 +0100

    MISUV-8441 WIP

--- app/models/optin/OptInCompletedViewModel.scala
+++ app/models/optin/OptInCompletedViewModel.scala
@@ -23,17 +23,19 @@ case class OptInCompletedViewModel(
                                     optInTaxYear: TaxYear,
                                     isCurrentYear: Boolean,
                                     showAnnualReportingAdvice: Boolean,
-                                    optInIncludedNextYear: Boolean
-                                  ) {
+                                    optInIncludedNextYear: Boolean,
+                                    annualWithFollowingYearMandated: Boolean) {
+
   val startYear: String = optInTaxYear.startYear.toString
   val endYear: String = optInTaxYear.endYear.toString
   val nextYear: String = optInTaxYear.nextYear.endYear.toString
 
   def headingMessageKey: String = {
-    (isCurrentYear, optInIncludedNextYear) match {
-      case (true, true) => "optin.completedOptIn.followingVoluntary.heading.desc"
-      case (true, false) => "optin.completedOptIn.cy.heading.desc"
-      case _ => "optin.completedOptIn.ny.heading.desc"
+    (isCurrentYear, optInIncludedNextYear, annualWithFollowingYearMandated) match {
+      case (true, _, true)    => "optin.completedOptIn.followingVoluntary.heading.desc"
+      case (true, true, _)    => "optin.completedOptIn.followingVoluntary.heading.desc"
+      case (true, false, _)   => "optin.completedOptIn.cy.heading.desc"
+      case _                  => "optin.completedOptIn.ny.heading.desc"
     }
   }
 
