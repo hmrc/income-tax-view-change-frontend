d6ea1b9d383206cdf5c18bb51968bc0685fc5265
WozCapGem <63247226+WozCapGem@users.noreply.github.com>
2024-10-18 13:42:45 +0100

    adding clientName to CDM

--- app/models/clientData/ClientDataModel.scala
+++ app/models/clientData/ClientDataModel.scala
@@ -4,11 +4,12 @@ package models.clientData
 import org.checkerframework.checker.units.qual.A
 import play.api.libs.json.{Json, OFormat}
 import play.api.mvc.{Request, WrappedRequest}
+import uk.gov.hmrc.auth.core.retrieve.Name
 
 
 case class ClientDataModel (clientMTDID: String,
-                            clientFirstName: String,
-                            clientLastName: String,
+                            clientName: Option[Name],
+                            clientNino: String,
                             clientUTR: String)(implicit request: Request[A]) extends WrappedRequest[A](request) {
 
 
