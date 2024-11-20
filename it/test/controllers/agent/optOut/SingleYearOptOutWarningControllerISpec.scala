30321ad6e3cf9b60b911aaecc53a6e26a2707a67
Michael Yau <47248104+mywyau@users.noreply.github.com>
2024-11-20 15:44:35 +0000

    MISUV-8792 Create new OptOutCancelled View, Controller and Navigation (#2640)
    * MISUV-8792 Create new OptOutCancelled View, Controller and Navigation
    
    * remove unneeded feature switch in test
    
    * fix up ispecs and add validation for only single tax year voluntary can be cancelled
    
    * fix title checks in OptOutCancelledControllerISpec
    
    * remove println
    
    * update for agent routes
    
    * fix unit tests and it tests
    
    * revert ispec to original stubbing
    
    * remove unused stubs
    
    * wip
    
    * refactor stubGetITSAStatusFutureYearsDetails
    
    * add full stops to all paragraphs

--- it/test/controllers/agent/optOut/SingleYearOptOutWarningControllerISpec.scala
+++ it/test/controllers/agent/optOut/SingleYearOptOutWarningControllerISpec.scala
@@ -28,22 +28,12 @@ import testConstants.BaseIntegrationTestConstants.{clientDetailsWithConfirmation
 import testConstants.IncomeSourceIntegrationTestConstants.propertyOnlyResponse
 
 class SingleYearOptOutWarningControllerISpec extends ComponentSpecBase {
-  private val isAgent: Boolean = true
-  private val singleYearOptOutWarningPageGETUrl = controllers.optOut.routes.SingleYearOptOutWarningController.show(isAgent).url
-  private val singleYearOptOutWarningPagePOSTUrl = controllers.optOut.routes.SingleYearOptOutWarningController.submit(isAgent).url
+
   private val validYesForm = ConfirmOptOutSingleTaxYearForm(Some(true), "")
   private val validNoForm = ConfirmOptOutSingleTaxYearForm(Some(false), "")
   private val inValidForm = ConfirmOptOutSingleTaxYearForm(None, "")
-  private val confirmOptOutPageUrl = controllers.optOut.routes.ConfirmOptOutController.show(isAgent).url
-  private val nextUpdatesPageUrl = controllers.routes.NextUpdatesController.showAgent.url
 
   private val currentTaxYear = TaxYear.forYearEnd(dateService.getCurrentTaxYearEnd)
-  private val previousYear = currentTaxYear.addYears(-1)
-
-  private val expectedInsetText = s"If you continue, from 6 April ${previousYear.endYear}. you’ll be required to send quarterly updates again through software."
-  private val expectedDetailText = s"You can only opt out for the ${previousYear.startYear} to ${previousYear.endYear} tax year."
-  private val expectedFormTitle = s"Do you still want to opt out for the ${previousYear.startYear} to ${previousYear.endYear} tax year?"
-  private val expectedErrorText = s"Select yes to opt out for the ${previousYear.startYear.toString} to ${previousYear.endYear.toString} tax year"
 
   private val repository: UIJourneySessionDataRepository = app.injector.instanceOf[UIJourneySessionDataRepository]
   private val helper = new OptOutSessionRepositoryHelper(repository)
@@ -53,103 +43,119 @@ class SingleYearOptOutWarningControllerISpec extends ComponentSpecBase {
     repository.clearSession(testSessionId).futureValue shouldBe true
   }
 
-  s"calling GET $singleYearOptOutWarningPageGETUrl" should {
-    "render single tax year opt out confirmation pager" when {
-      "User is authorised" in {
-        stubAuthorisedAgentUser(authorised = true)
-        IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, propertyOnlyResponse)
+  "SingleYearOptOutWarningController" when {
 
-        helper.stubOptOutInitialState(currentTaxYear,
-          previousYearCrystallised = false,
-          previousYearStatus = Voluntary,
-          currentYearStatus = NoStatus,
-          nextYearStatus = NoStatus)
+    "is Agent Journey" when {
 
-        val result = IncomeTaxViewChangeFrontend.getSingleYearOptOutWarning(clientDetailsWithConfirmation)
+      "calling GET - /optout/single-taxyear-warning" should {
 
-        result should have(
-          httpStatus(OK),
-          pageTitleAgent("optOut.confirmSingleYearOptOut.title"),
-          elementTextByID("detail-text")(expectedDetailText),
-          elementTextByID("warning-inset")(expectedInsetText),
-          elementTextBySelector(".govuk-fieldset__legend--m")(expectedFormTitle)
-        )
+        "render single tax year opt out confirmation pager" when {
 
+          "Agent User is authorised" in {
+
+            stubAuthorisedAgentUser(authorised = true)
+            IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, propertyOnlyResponse)
+
+            helper.stubOptOutInitialState(currentTaxYear,
+              previousYearCrystallised = false,
+              previousYearStatus = Voluntary,
+              currentYearStatus = NoStatus,
+              nextYearStatus = NoStatus)
+
+            val result = IncomeTaxViewChangeFrontend.getSingleYearOptOutWarning(clientDetailsWithConfirmation)
+
+            result should have(
+              httpStatus(OK),
+              pageTitleAgent("optOut.confirmSingleYearOptOut.title")
+            )
+
+          }
+        }
+      }
+
+      "calling POST - /agent/optout/single-taxyear-warning" should {
+
+        "return status BAD_REQUEST - and render single tax year opt out confirmation pager with error message - 400 " when {
+
+          "invalid data is sent" in {
+
+            stubAuthorisedAgentUser(authorised = true)
+            IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, propertyOnlyResponse)
+
+            helper.stubOptOutInitialState(currentTaxYear,
+              previousYearCrystallised = false,
+              previousYearStatus = Voluntary,
+              currentYearStatus = NoStatus,
+              nextYearStatus = NoStatus)
+
+            val result = IncomeTaxViewChangeFrontend.postSingleYearOptOutWarning(clientDetailsWithConfirmation)(inValidForm)
+
+            verifyIncomeSourceDetailsCall(testMtditid)
+
+            result should have(
+              httpStatus(BAD_REQUEST),
+              pageTitleAgent("optOut.confirmSingleYearOptOut.title")
+            )
+
+          }
+        }
+
+        "redirect to ConfirmOptOutPage - /agents/optout/review-confirm-taxyear - SEE_OTHER - 303" when {
+
+          "Yes response is sent" in {
+
+            val isAgent: Boolean = true
+            val confirmOptOutPageUrl = controllers.optOut.routes.ConfirmOptOutController.show(isAgent).url
+
+            stubAuthorisedAgentUser(authorised = true)
+            IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, propertyOnlyResponse)
+
+            helper.stubOptOutInitialState(currentTaxYear,
+              previousYearCrystallised = false,
+              previousYearStatus = Voluntary,
+              currentYearStatus = NoStatus,
+              nextYearStatus = NoStatus)
+
+            val result = IncomeTaxViewChangeFrontend.postSingleYearOptOutWarning(clientDetailsWithConfirmation)(validYesForm)
+
+            verifyIncomeSourceDetailsCall(testMtditid)
+
+            result should have(
+              httpStatus(SEE_OTHER),
+              redirectURI(confirmOptOutPageUrl)
+            )
+
+          }
+        }
+
+        "redirect to OptOutCancelledPage - /agents/optout/cancelled - SEE_OTHER - 303" when {
+
+          "user answers No" in {
+
+            val optOutCancelledAgentUrl = controllers.optOut.routes.OptOutCancelledController.showAgent().url
+
+            stubAuthorisedAgentUser(authorised = true)
+            IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, propertyOnlyResponse)
+
+            helper.stubOptOutInitialState(currentTaxYear,
+              previousYearCrystallised = false,
+              previousYearStatus = Voluntary,
+              currentYearStatus = NoStatus,
+              nextYearStatus = NoStatus)
+
+            val result = IncomeTaxViewChangeFrontend.postSingleYearOptOutWarning(clientDetailsWithConfirmation)(validNoForm)
+
+            verifyIncomeSourceDetailsCall(testMtditid)
+
+            result should have(
+              httpStatus(SEE_OTHER),
+              redirectURI(optOutCancelledAgentUrl)
+            )
+          }
+        }
       }
     }
   }
 
-  s"calling POST $singleYearOptOutWarningPagePOSTUrl" should {
-    s"return status $BAD_REQUEST and render single tax year opt out confirmation pager with error message - $BAD_REQUEST " when {
-      "invalid data is sent" in {
-        stubAuthorisedAgentUser(authorised = true)
-        IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, propertyOnlyResponse)
-
-        helper.stubOptOutInitialState(currentTaxYear,
-          previousYearCrystallised = false,
-          previousYearStatus = Voluntary,
-          currentYearStatus = NoStatus,
-          nextYearStatus = NoStatus)
-
-        val result = IncomeTaxViewChangeFrontend.postSingleYearOptOutWarning(clientDetailsWithConfirmation)(inValidForm)
-
-        verifyIncomeSourceDetailsCall(testMtditid)
-
-        result should have(
-          httpStatus(BAD_REQUEST),
-          pageTitleAgent("optOut.confirmSingleYearOptOut.title"),
-          elementTextByID("detail-text")(expectedDetailText),
-          elementTextByID("warning-inset")(expectedInsetText),
-          elementTextBySelector(".govuk-fieldset__legend--m")(expectedFormTitle),
-          elementTextBySelector(".govuk-error-summary__body")(expectedErrorText)
-        )
-
-      }
-    }
-    s"return status $SEE_OTHER with location $confirmOptOutPageUrl" when {
-      "Yes response is sent" in {
-        stubAuthorisedAgentUser(authorised = true)
-        IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, propertyOnlyResponse)
-
-        helper.stubOptOutInitialState(currentTaxYear,
-          previousYearCrystallised = false,
-          previousYearStatus = Voluntary,
-          currentYearStatus = NoStatus,
-          nextYearStatus = NoStatus)
-
-        val result = IncomeTaxViewChangeFrontend.postSingleYearOptOutWarning(clientDetailsWithConfirmation)(validYesForm)
-
-        verifyIncomeSourceDetailsCall(testMtditid)
-
-        result should have(
-          httpStatus(SEE_OTHER),
-          redirectURI(confirmOptOutPageUrl)
-        )
-
-      }
-    }
-
-    s"return status $SEE_OTHER with location $nextUpdatesPageUrl" when {
-      "Yes response is sent" in {
-        stubAuthorisedAgentUser(authorised = true)
-        IncomeTaxViewChangeStub.stubGetIncomeSourceDetailsResponse(testMtditid)(OK, propertyOnlyResponse)
-
-        helper.stubOptOutInitialState(currentTaxYear,
-          previousYearCrystallised = false,
-          previousYearStatus = Voluntary,
-          currentYearStatus = NoStatus,
-          nextYearStatus = NoStatus)
-
-        val result = IncomeTaxViewChangeFrontend.postSingleYearOptOutWarning(clientDetailsWithConfirmation)(validNoForm)
-
-        verifyIncomeSourceDetailsCall(testMtditid)
-
-        result should have(
-          httpStatus(SEE_OTHER),
-          redirectURI(nextUpdatesPageUrl)
-        )
-      }
-    }
-  }
 
 }
