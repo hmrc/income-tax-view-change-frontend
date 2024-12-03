9be523384b706553ef89dc3248182941c049b982
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2024-12-03 15:02:17 +0000

    WIP ITs

--- it/test/connectors/agent/CitizenDetailsConnectorISpec.scala
+++ it/test/connectors/agent/CitizenDetailsConnectorISpec.scala
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package connectors.agent
 
 import com.github.tomakehurst.wiremock.client.WireMock
-import helpers.{ComponentSpecBase, WiremockHelper}
-import helpers.servicemocks.AuditStub
+import _root_.helpers.{ComponentSpecBase, WiremockHelper}
+import _root_.helpers.servicemocks.AuditStub
 import org.scalatest.wordspec.AnyWordSpec
-import play.api.http.Status.OK
+import play.api.http.Status.{INTERNAL_SERVER_ERROR, OK}
 import play.api.test.Injecting
 
 class CitizenDetailsConnectorISpec extends AnyWordSpec with ComponentSpecBase with Injecting {
@@ -28,7 +44,11 @@ class CitizenDetailsConnectorISpec extends AnyWordSpec with ComponentSpecBase wi
         result shouldBe ""
       }
       "return an error when the request fails" in {
+        WiremockHelper.stubGet(s"/citizen-details/sautr/$saUtr", INTERNAL_SERVER_ERROR, "{}")
 
+        val result = connector.getCitizenDetailsBySaUtr(saUtr).futureValue
+
+        result shouldBe ""
       }
     }
   }
