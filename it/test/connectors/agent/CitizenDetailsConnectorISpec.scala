de5e80218145fcd01d52a4a053783aaa831432c9
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2024-12-04 09:05:50 +0000

    Added success response to ITs

--- it/test/connectors/agent/CitizenDetailsConnectorISpec.scala
+++ it/test/connectors/agent/CitizenDetailsConnectorISpec.scala
@@ -20,7 +20,7 @@ import com.github.tomakehurst.wiremock.client.WireMock
 import _root_.helpers.agent.ComponentSpecBase
 import _root_.helpers.WiremockHelper
 import _root_.helpers.servicemocks.AuditStub
-import models.citizenDetails.CitizenDetailsErrorModel
+import models.citizenDetails.{CitizenDetailsErrorModel, CitizenDetailsModel}
 import org.scalatest.wordspec.AnyWordSpec
 import play.api.http.Status.{INTERNAL_SERVER_ERROR, OK}
 import play.api.test.Injecting
@@ -42,16 +42,22 @@ class CitizenDetailsConnectorISpec extends AnyWordSpec with ComponentSpecBase wi
         val responseBody =
           """
             |{
-            |   "firstName": "John",
-            |   "lastName": "Doe",
-            |   "nino": "AA123456A"
+            |  "name": {
+            |    "current": {
+            |      "firstName": "John",
+            |      "lastName": "Doe"
+            |    }
+            |  },
+            |  "ids": {
+            |    "nino": "AA123456A"
+            |  }
             |}
             |""".stripMargin
         WiremockHelper.stubGet(s"/citizen-details/sautr/$saUtr", OK, responseBody)
 
         val result = connector.getCitizenDetailsBySaUtr(saUtr).futureValue
 
-        result shouldBe ""
+        result shouldBe CitizenDetailsModel(Some("John"), Some("Doe"), Some("AA123456A"))
       }
       "return an error when the request fails" in {
         WiremockHelper.stubGet(s"/citizen-details/sautr/$saUtr", INTERNAL_SERVER_ERROR, "{}")
