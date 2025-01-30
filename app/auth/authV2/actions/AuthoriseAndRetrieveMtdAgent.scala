95d57ea33f89aa94782a0f1626c26a62417714b7
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2025-01-30 08:15:03 +0000

    MISUV-9040 added addition log

--- app/auth/authV2/actions/AuthoriseAndRetrieveMtdAgent.scala
+++ app/auth/authV2/actions/AuthoriseAndRetrieveMtdAgent.scala
@@ -109,7 +109,7 @@ class AuthoriseAndRetrieveMtdAgent @Inject()(authorisedFunctions: AuthorisedFunc
         logger.error(s"Unauthorised request: ${authorisationException.reason}. Redirect to Sign In.")
         Future.successful(Left(Redirect(controllers.routes.SignInController.signIn)))
       case ex =>
-        logger.error(s"Unexpected error from Auth.")
+        logger.error(s"Unexpected error from Auth. Error message = ${ex.getMessage}")
         Future.successful(Left(errorHandler.showInternalServerError()))
     }
   }
