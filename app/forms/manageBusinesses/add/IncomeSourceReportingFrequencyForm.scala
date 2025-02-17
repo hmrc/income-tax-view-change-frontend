94bb5296d58ce86bb3d6a4d29578815f74468de4
jmountain-hmrc <31282623+jmountain-hmrc@users.noreply.github.com>
2025-02-17 22:21:49 +0000

    MISUV-8495 WIP

---
+++ app/forms/manageBusinesses/add/IncomeSourceReportingFrequencyForm.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.manageBusinesses.add
+
+import models.incomeSourceDetails.TaxYear
+import play.api.data.Form
+import play.api.data.Forms.{boolean, mapping, optional, text}
+import play.api.i18n.Messages
+
+case class IncomeSourceReportingFrequencyForm(reportingFrequencyQuarterly: Option[Boolean], csrfToken: String)
+
+object IncomeSourceReportingFrequencyForm {
+  val reportQuarterlyField: String = "report-quarterly"
+  val noResponseErrorMessageKey: String = "optOut.confirmSingleYearOptOut.form.no-select.error"
+  val csrfToken: String = "csrfToken"
+
+  def apply()(implicit messages: Messages): Form[IncomeSourceReportingFrequencyForm] = {
+    val noSelectionErrorMessage: String =
+      messages(noResponseErrorMessageKey)
+
+    Form(
+      mapping(
+        reportQuarterlyField -> optional(boolean).verifying(
+          noSelectionErrorMessage, response => response.isInstanceOf[Some[Boolean]]),
+        csrfToken -> text
+      )(IncomeSourceReportingFrequencyForm.apply)(IncomeSourceReportingFrequencyForm.unapply)
+    )
+  }
+}
