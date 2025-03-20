047d60bae48401547fe4bb44a44f41e17131d1c4
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-03-20 10:51:02 +0000

    update response to take TaxYearRange

--- app/services/FinancialDetailsService.scala
+++ app/services/FinancialDetailsService.scala
@@ -90,7 +90,8 @@ class FinancialDetailsService @Inject()(val financialDetailsConnector: Financial
       Logger("application").debug(s"Getting financial details for TaxYears: ${from} - ${to}")
 
       for {
-        response <- financialDetailsConnector.getFinancialDetails(TaxYear.forYearEnd(from), TaxYear.forYearEnd(to), user.nino)
+        response <- financialDetailsConnector.getFinancialDetailsByTaxYearRange(TaxYearRange(TaxYear.forYearEnd(from), TaxYear.forYearEnd(to)), user.nino)
+
       } yield response match {
         case financialDetails: FinancialDetailsModel => Some(financialDetails)
         case error: FinancialDetailsErrorModel if error.code != NOT_FOUND => Some(error)
