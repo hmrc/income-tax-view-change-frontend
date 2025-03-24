04cd530054611fea2e2997e8eb546ee37510b9f1
AyubAz <110174399+ayubAz@users.noreply.github.com>
2025-03-24 09:24:17 +0000

    empty commit

--- app/models/financialDetails/ChargeItem.scala
+++ app/models/financialDetails/ChargeItem.scala
@@ -138,8 +138,6 @@ case class ChargeItem (
 
   val isBalancingCharge: Boolean = transactionType == BalancingCharge
 
-  val isPaymentOnAccount: Boolean = transactionType == PoaOneDebit || transactionType == PoaTwoDebit
-
   val isPoaReconciliationCredit: Boolean = transactionType == PoaOneReconciliationCredit ||
     transactionType == PoaTwoReconciliationCredit
 
