e330685dfc37030fbe4775b6f777d0e8e9b4e499
mywyau <47248104+mywyau@users.noreply.github.com>
2025-04-07 09:36:00 +0100

    remove accidental form changes

--- app/forms/incomeSources/add/AddIncomeSourceStartDateFormProvider.scala
+++ app/forms/incomeSources/add/AddIncomeSourceStartDateFormProvider.scala
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http:
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,11 +28,8 @@ class AddIncomeSourceStartDateFormProvider extends Mappings {
 
   def apply(messagesPrefix: String)(implicit messages: Messages, dateService: DateServiceInterface, dateFormatter: ImplicitDateFormatter): Form[LocalDate] = {
 
-    // We tested max date to validation, allowing users to submit a date 6 days in advance/future
-    val maximumAllowableDate: LocalDate = dateService.getCurrentDate.plusDays(6) // tested 11-4-2025
+    val maximumAllowableDate: LocalDate = dateService.getCurrentDate.plusDays(6)
     val earliestInvalidDate: LocalDate = maximumAllowableDate.plusDays(1)
-
-    val earliestDatePossible = LocalDate.of(1900, 1, 1)
 
     val earliestLongInvalidDate: String = dateFormatter.longDate(earliestInvalidDate).toLongDate
     val invalidFutureDateErrorMessage: String = messages(s"$messagesPrefix.error.future", earliestLongInvalidDate)
@@ -47,12 +44,8 @@ class AddIncomeSourceStartDateFormProvider extends Mappings {
         twoRequiredKey = s"$dateFormErrorPrefix.required.two",
         requiredKey = s"$dateFormErrorPrefix.required"
       ).verifying(
-        maxDate(maximumAllowableDate, invalidFutureDateErrorMessage), // max date in future error handling / form validation
-        fourDigitValidYear(invalidMessage), // where is earliest day/year?
-        minDate(
-          minimum = LocalDate.of(1900, 1, 1), // new validation code/date to be added
-          errorKey = "The earliest date must be after 1-1-1900"  //  we would use a message key here, with content driven from the UCD team
-        )
+        maxDate(maximumAllowableDate, invalidFutureDateErrorMessage),
+        fourDigitValidYear(invalidMessage),
       )
     )
   }
