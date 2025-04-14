cfa6fbb66cf443e9862afa2f094e602638965a1b
nemanjatomanovic <nemanja.tomanovic@gmail.com>
2025-04-14 15:30:59 +0100

    MISUV-9554 - moved exception handling from views to model classes

--- app/models/paymentAllocationCharges/PaymentAllocationViewModel.scala
+++ app/models/paymentAllocationCharges/PaymentAllocationViewModel.scala
@@ -24,7 +24,11 @@ import implicits.ImplicitCurrencyFormatter._
 
 import java.time.LocalDate
 
-case class AllocationDetailWithClearingDate(allocationDetail: Option[AllocationDetail], clearingDate: Option[LocalDate])
+case class AllocationDetailWithClearingDate(allocationDetail: Option[AllocationDetail], clearingDate: Option[LocalDate]) {
+  def validatedAllocationDetail: AllocationDetail = allocationDetail.getOrElse(throw MissingFieldException("Payment Allocation Detail"))
+  def validatedClearingDate: LocalDate = clearingDate.getOrElse(throw MissingFieldException("Payment Clearing Date"))
+
+}
 
 case class LatePaymentInterestPaymentAllocationDetails(documentDetail: DocumentDetail, amount: BigDecimal)
 
@@ -37,7 +41,7 @@ case class PaymentAllocationViewModel(paymentAllocationChargeModel: FinancialDet
     paymentAllocationChargeModel.documentDetails.exists(_.credit.isDefined)
 
   def getEffectiveDateOfPayment: LocalDate =
-      paymentAllocationChargeModel.documentDetails.head.effectiveDateOfPayment.getOrElse(throw MissingFieldException("Effective Date Of Payment"))
+    paymentAllocationChargeModel.documentDetails.head.effectiveDateOfPayment.getOrElse(throw MissingFieldException("Effective Date Of Payment"))
 
   def getOriginalAmount: String =
     paymentAllocationChargeModel.filteredDocumentDetails.head.originalAmount.abs.toCurrencyString
