c84963663a6842b38a12579fc767239039f319df
AyubAz <110174399+ayubAz@users.noreply.github.com>
2025-04-23 13:30:25 +0100

    fix unreachable code

--- app/models/financialDetails/ChargeItem.scala
+++ app/models/financialDetails/ChargeItem.scala
@@ -133,10 +133,10 @@ case class ChargeItem (
     val validCharge = (transactionType, codedOutStatus) match {
       case (BalancingCharge,      Some(Nics2)) => true
       case (BalancingCharge,             None) => true
-      case (PoaOneDebit,                    _) => true
       case (PoaOneDebit, Some(FullyCollected)) => true
-      case (PoaTwoDebit,                    _) => true
+      case (PoaOneDebit,                    _) => true
       case (PoaTwoDebit, Some(FullyCollected)) => true
+      case (PoaTwoDebit,                    _) => true
       case (LateSubmissionPenalty,          _) => true
       case (FirstLatePaymentPenalty,        _) => true
       case _                                   => false
