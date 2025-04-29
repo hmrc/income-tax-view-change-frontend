1a3d81161b92cc197ae7bf1898c22af12ebb1279
dnyuar <60218795+dnyuar@users.noreply.github.com>
2025-04-29 09:02:27 +0100

    rebase

--- app/models/financialDetails/CodedOutStatusType.scala
+++ app/models/financialDetails/CodedOutStatusType.scala
@@ -60,12 +60,11 @@ object CodedOutStatusType {
       case (Some(CODING_OUT_ACCEPTED.name),        _) | (_, Some(CODING_OUT_ACCEPTED.code))        => Some(Accepted)
       case (Some(CODING_OUT_CANCELLED.name),       _) | (_, Some(CODING_OUT_CANCELLED.code))       => Some(Cancelled)
       case (Some(CODING_OUT_FULLY_COLLECTED.name), _) | (_, Some(CODING_OUT_FULLY_COLLECTED.code)) => Some(FullyCollected)
-      case _                                                                                       => None
+      case (Some(CODING_OUT_NOT_COLLECTED.name), _) | (_, Some(CODING_OUT_NOT_COLLECTED.code)) => Some(Cancelled)
+      case (Some(CODING_OUT_PARTLY_COLLECTED.name), _) | (_, Some(CODING_OUT_PARTLY_COLLECTED.code)) => Some(Cancelled)
       case (Some(CODING_OUT_CLASS2_NICS.name), _) => Some(Nics2)
       case (Some(CODING_OUT_ACCEPTED.name), _) | (_, Some(CODING_OUT_ACCEPTED.code)) => Some(Accepted)
       case (Some(CODING_OUT_CANCELLED.name), _) | (_, Some(CODING_OUT_CANCELLED.code)) => Some(Cancelled)
-      case (Some(CODING_OUT_NOT_COLLECTED.name), _) | (_, Some(CODING_OUT_NOT_COLLECTED.code)) => Some(Cancelled)
-      case (Some(CODING_OUT_PARTLY_COLLECTED.name), _) | (_, Some(CODING_OUT_PARTLY_COLLECTED.code)) => Some(Cancelled)
       case _ => None
     }
   }
