492593d111f1a9c359350aae26bae59fc8b2bc5a
eloiseyaz <79723491+eloiseyaz@users.noreply.github.com>
2025-07-29 09:32:35 +0100

    remove review and reconcile from WhatYouOweViewSpec

--- test/views/WhatYouOweViewSpec.scala
+++ test/views/WhatYouOweViewSpec.scala
@@ -134,7 +134,6 @@ class WhatYouOweViewSpec extends TestSupport with FeatureSwitching with Implicit
                   dunningLock: Boolean = false,
                   taxYear: Int = fixedDate.getYear,
                   migrationYear: Int = fixedDate.getYear - 1,
-                  reviewAndReconcileEnabled: Boolean = false,
                   adjustPaymentsOnAccountFSEnabled: Boolean = false,
                   claimToAdjustViewModel: Option[WYOClaimToAdjustViewModel] = None,
                   LPP2Url: String = ""
@@ -186,7 +185,6 @@ class WhatYouOweViewSpec extends TestSupport with FeatureSwitching with Implicit
   class AgentTestSetup(charges: WhatYouOweChargesList,
                        currentTaxYear: Int = fixedDate.getYear,
                        migrationYear: Int = fixedDate.getYear - 1,
-                       reviewAndReconcileEnabled: Boolean = false,
                        dunningLock: Boolean = false,
                        taxYear: Int = fixedDate.getYear,
                        hasLpiWithDunningLock: Boolean = false,
@@ -468,8 +466,7 @@ class WhatYouOweViewSpec extends TestSupport with FeatureSwitching with Implicit
 
         }
         "have review and reconcile extra payments with Accrues Interest tags in the same table" in new TestSetup(
-          charges = whatYouOweWithReviewReconcileDataNotYetDue,
-          reviewAndReconcileEnabled  = true) {
+          charges = whatYouOweWithReviewReconcileDataNotYetDue) {
           val poaExtra1Table: Element = pageDocument.getElementsByClass("govuk-table__row").get(1)
           poaExtra1Table.select("td").first().text() shouldBe fixedDate.plusYears(100).minusDays(1).toLongDateShort
           poaExtra1Table.select("td").get(1).text() shouldBe "Accrues interest " + poaExtra1Text + " 1"
@@ -484,8 +481,7 @@ class WhatYouOweViewSpec extends TestSupport with FeatureSwitching with Implicit
 
           poa2ExtraTable.select("td").last().text() shouldBe "£75.00"
         }
-        "have interest charges for paid reconciliation charges in the same table" in new TestSetup(charges = whatYouOweReconciliationInterestData,
-          reviewAndReconcileEnabled = true) {
+        "have interest charges for paid reconciliation charges in the same table" in new TestSetup(charges = whatYouOweReconciliationInterestData) {
           val poaExtra1Table: Element = pageDocument.getElementsByClass("govuk-table__row").get(1)
           poaExtra1Table.select("td").first().text() shouldBe interestEndDateFuture.toLongDateShort
           poaExtra1Table.select("td").get(1).text() shouldBe poa1ReconcileInterest + " 1"
