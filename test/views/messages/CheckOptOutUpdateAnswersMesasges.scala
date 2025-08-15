ca5dce6b821df3d978f0f8a0b1be13d0c0f95926
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2025-08-15 13:09:44 +0100

    MISUV-9975 - basic test setup (a lot still awaiting the journey completion and link up)

---
+++ test/views/messages/CheckOptOutUpdateAnswersMesasges.scala
@@ -0,0 +1,10 @@
+package views.messages
+
+object CheckOptOutUpdateAnswersMesasges {
+  val title: String = "Check your answers - Manage your Self Assessment - GOV.UK"
+  val heading: String = "Confirm and opt out for the 2025 to 2026 tax year"
+  val p1: String = "If you opt out, you would no longer need software compatible with Making Tax Digital for Income Tax."
+  val inset: String = "You have 2 quarterly updates submitted for this tax year. If you continue, these updates will be deleted from our records. You will need to include any income from these updates in your tax return."
+  val confirmButton: String = "Confirm and save"
+  val cancelButton: String = "Cancel"
+}
