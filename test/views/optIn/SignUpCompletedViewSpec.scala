5befacd8aff571d5ce1147d80c64f5cd2ec0346b
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2025-08-19 12:55:54 +0100

    Frontend changes finished, WIP for tests

---
+++ test/views/optIn/SignUpCompletedViewSpec.scala
@@ -0,0 +1,21 @@
+package views.optIn
+
+import testUtils.TestSupport
+
+class SignUpCompletedViewSpec extends TestSupport {
+
+  object SignUpCompletedViewMessages {
+    val currentYearCompletedHeading = "Sign up completed"
+    val currentYearCompletedTitle = "Sign up completed - Income Tax - GOV.UK"
+    val currentYearCompletedPanelDesc = "You need to use Making Tax Digital for Income Tax from now on"
+    val currentYearCompletedDesc = "You may have overdue updates for the 2024 to 2025 tax year. You must submit these updates with all required income and expenses through your compatible software."
+
+    val currentYearYourRevisedDeadlinesHeading = "Your revised deadlines"
+    val currentYearYourRevisedDeadlinesDesc = "Your revised deadlines will be available in the next few minutes."
+    val currentYearYourRevisedDeadlinesDesc2 = "Even if they are not displayed right away on the updates and deadlines page, your account has been updated."
+    val currentYearYourRevisedDeadlinesDesc3 = "You can decide at any time to opt out of Making Tax Digital for Income Tax for all your businesses on your reporting obligations page."
+
+    val currentYearSubmitUpdatesHeading = "Submit updates in software"
+
+  }
+}
