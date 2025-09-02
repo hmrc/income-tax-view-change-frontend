3ef953be638671200eb94c703d83366789654a0f
AyubAz <110174399+ayubAz@users.noreply.github.com>
2025-09-02 09:43:40 +0100

    undo changes to NrsUtils.scala

--- it/test/testConstants/NrsUtils.scala
+++ it/test/testConstants/NrsUtils.scala
@@ -16,8 +16,7 @@
 
 package testConstants
 
-import cats.implicits.catsSyntaxOptionId
-import models.nrs.{NrsMetadata, NrsSubmission, NrsSuccessResponse, RawPayload, SearchKeys}
+import models.nrs.{IdentityData, NrsMetadata, NrsSubmission, NrsSuccessResponse, RawPayload}
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.Headers
 import play.api.test.FakeRequest
@@ -36,6 +35,51 @@ object NrsUtils {
     NrsSuccessResponse("submissionId")
   ).toString()
 
+  val identityData: IdentityData = IdentityData(
+    internalId = Some("int-id"),
+    externalId = Some("ext-id"),
+    agentCode = None,
+    credentials = Some(Credentials("12345-credId", "GovernmmentGateway")),
+    confidenceLevel = ConfidenceLevel.L200,
+    nino = None,
+    saUtr = None,
+    name = Some(Name(Some("mickey"), Some("mouse"))),
+    dateOfBirth = Some(LocalDate.parse("1985-01-01")),
+    email = Some("test@test.com"),
+    agentInformation = AgentInformation(
+      agentCode = Some("TZRXXV"),
+      agentFriendlyName = Some("Bodgitt & Legget LLP"),
+      agentId = Some("BDGL")
+    ),
+    groupIdentifier = Some("GroupId"),
+    credentialRole = Some(User),
+    mdtpInformation = Some(MdtpInformation("DeviceId", "SessionId")),
+    itmpName = Some(
+      ItmpName(
+        Some("michael"),
+        Some("h"),
+        Some("mouse")
+      )),
+    itmpDateOfBirth = Some(LocalDate.parse("1985-01-01")),
+    itmpAddress = Some(
+      ItmpAddress(
+        line1 = Some("Line 1"),
+        line2 = None,
+        line3 = None,
+        line4 = None,
+        line5 = None,
+        postCode = Some("NW94HD"),
+        countryName = Some("United Kingdom"),
+        countryCode = Some("UK")
+      )),
+    affinityGroup = Some(AffinityGroup.Individual),
+    credentialStrength = Some("strong"),
+    loginTimes = LoginTimes(
+      Instant.parse("2016-11-27T09:00:00.000Z"),
+      Some(Instant.parse("2016-11-01T12:00:00.000Z"))
+    )
+  )
+
   val nrsMetadata: NrsMetadata = {
     val request =
       FakeRequest()
@@ -48,9 +92,10 @@ object NrsUtils {
         .withBody(nrsMetadataBody)
 
     NrsMetadata(
-      request,
       Instant.parse("2018-04-07T12:13:25.000Z"),
-      SearchKeys("credId".some, "saUtr".some, "nino"),
+      "3216783621-123873821-12332",
+      identityData,
+      request,
       request.body.calculateSha256)
   }
 
@@ -65,6 +110,51 @@ object NrsUtils {
          |    "payloadContentType": "application/xml",
          |    "payloadSha256Checksum":"${nrsMetadataBody.calculateSha256}",
          |    "userSubmissionTimestamp": "2018-04-07T12:13:25.000Z",
+         |    "identityData": {
+         |      "internalId": "int-id",
+         |      "externalId": "ext-id",
+         |      "credentials": {
+         |        "providerId": "12345-credId",
+         |        "providerType": "GovernmmentGateway"
+         |      },
+         |      "confidenceLevel": 200,
+         |      "name": {
+         |        "name": "mickey",
+         |        "lastName": "mouse"
+         |      },
+         |      "dateOfBirth": "1985-01-01",
+         |      "email": "test@test.com",
+         |      "agentInformation": {
+         |        "agentCode": "TZRXXV",
+         |        "agentFriendlyName": "Bodgitt & Legget LLP",
+         |        "agentId": "BDGL"
+         |      },
+         |      "groupIdentifier": "GroupId",
+         |      "credentialRole": "User",
+         |      "mdtpInformation": {
+         |        "deviceId": "DeviceId",
+         |        "sessionId": "SessionId"
+         |      },
+         |      "itmpName": {
+         |        "givenName": "michael",
+         |        "middleName": "h",
+         |        "familyName": "mouse"
+         |      },
+         |      "itmpDateOfBirth": "1985-01-01",
+         |      "itmpAddress": {
+         |        "line1": "Line 1",
+         |        "postCode": "NW94HD",
+         |        "countryName": "United Kingdom",
+         |        "countryCode": "UK"
+         |      },
+         |      "affinityGroup": "Individual",
+         |      "credentialStrength": "strong",
+         |      "enrolments":{"enrolments":[]},
+         |      "loginTimes": {
+         |        "currentLogin": "2016-11-27T09:00:00.000Z",
+         |        "previousLogin": "2016-11-01T12:00:00.000Z"
+         |      }
+         |    },
          |    "userAuthToken": "Bearer AbCdEf123456",
          |    "headerData": {
          |      "Authorization": "Bearer AbCdEf123456",
