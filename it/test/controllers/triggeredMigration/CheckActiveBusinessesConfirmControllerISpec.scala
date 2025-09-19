2e788d6be01907188f0d1d779c6c4654da1838ab
IoannisLazaridis-oc <128503072+IoannisLazaridis-oc@users.noreply.github.com>
2025-09-19 11:34:12 +0100

    MISUV-10154: fix integration test

--- it/test/controllers/triggeredMigration/CheckActiveBusinessesConfirmControllerISpec.scala
+++ it/test/controllers/triggeredMigration/CheckActiveBusinessesConfirmControllerISpec.scala
@@ -36,7 +36,7 @@ class CheckActiveBusinessesConfirmControllerISpec extends ControllerISpecHelper 
     val title = "Have you checked that HMRC records only list your active businesses?"
     val continueText = "Continue"
     val errorSummaryHeading = "There is a problem"
-    val errorMessage = "Select yes if you've checked that HMRC records only list your active businesses"
+    val errorMessage = "Select yes if you’ve checked that HMRC records only list your active businesses"
   }
 
   private def checkPageContent(result: WSResponse, mtdRole: MTDUserRole): Unit = {
