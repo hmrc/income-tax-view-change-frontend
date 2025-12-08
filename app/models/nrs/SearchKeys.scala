e72a3789334f81209b91d24ded1eaa1e704b850c
nemanjatomanovic <nemanja.tomanovic@gmail.com>
2025-12-08 16:45:22 +0000

    MISUV-10591 - Fixed notableEvent and search keys fields.

--- app/models/nrs/SearchKeys.scala
+++ app/models/nrs/SearchKeys.scala
@@ -18,7 +18,7 @@ package models.nrs
 
 import play.api.libs.json.{Json, Writes}
 
-case class SearchKeys(credId: Option[String], saUtr: Option[String], nino: String)
+case class SearchKeys(credId: Option[String], utr: Option[String], nino: String)
 
 object SearchKeys {
   implicit val writes: Writes[SearchKeys] = Json.writes[SearchKeys]
