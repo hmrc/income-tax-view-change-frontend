22bd24ccbb4402ab22a0083db399f831f447a600
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2025-12-30 11:53:32 +0000

    Update tests in IncomeSourceEndDateFormSpec

--- test/forms/manageBusinesses/cease/IncomeSourceEndDateFormSpec.scala
+++ test/forms/manageBusinesses/cease/IncomeSourceEndDateFormSpec.scala
@@ -65,7 +65,7 @@ class IncomeSourceEndDateFormSpec extends AnyWordSpec with Matchers with TestSup
     completedForm.data.get("income-source-end-date.day") shouldBe Some("20")
     completedForm.data.get("income-source-end-date.month") shouldBe Some("12")
     completedForm.data.get("income-source-end-date.year") shouldBe Some(s"$futureYear")
-    completedForm.errors shouldBe List(FormError("income-source-end-date", List(s"incomeSources.cease.endDate.${incomeSourceType.messagesCamel}.future"), List()))
+    completedForm.errors shouldBe List()
   }
 
   def setupBindBeforeStartDateTest(incomeSourceType: IncomeSourceType): Unit = {
