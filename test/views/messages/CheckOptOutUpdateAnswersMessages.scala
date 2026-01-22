919effad79808e65e446b27c8c47938ab4b43f51
IoannisLazaridis-oc <128503072+IoannisLazaridis-oc@users.noreply.github.com>
2026-01-22 15:39:46 +0000

    MISUV-10629: update text for CYA page - waiting for Welsh translation

--- test/views/messages/CheckOptOutUpdateAnswersMessages.scala
+++ test/views/messages/CheckOptOutUpdateAnswersMessages.scala
@@ -19,7 +19,7 @@ package views.messages
 object CheckOptOutUpdateAnswersMessages {
   val title: String = "Check your answers - Manage your Self Assessment - GOV.UK"
   val heading: String = "Confirm and opt out for the 2025 to 2026 tax year"
-  val p1: String = "If you opt out, you would no longer need software compatible with Making Tax Digital for Income Tax."
+  val p1: String = "For any tax years you opt out of, you no longer need to use software compatible with Making Tax Digital for Income Tax, but you can keep using it if you prefer."
   val inset: String = "You have 2 quarterly updates submitted for this tax year. If you continue, these updates will be deleted from our records. You will need to include any income from these updates in your tax return."
   val confirmButton: String = "Confirm and save"
   val cancelButton: String = "Cancel"
