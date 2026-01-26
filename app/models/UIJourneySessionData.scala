f3c5f206fe6bc35b323d268c35798ac362829acd
nemanjatomanovic <nemanja.tomanovic@gmail.com>
2026-01-26 16:35:40 +0000

    Fixed date serialisation for SensitiveUIJourneySessionData

--- app/models/UIJourneySessionData.scala
+++ app/models/UIJourneySessionData.scala
@@ -151,7 +151,59 @@ case class SensitiveUIJourneySessionData(
 
 object SensitiveUIJourneySessionData {
 
-  implicit def format(implicit crypto: Encrypter with Decrypter): OFormat[SensitiveUIJourneySessionData] =
-    Json.format[SensitiveUIJourneySessionData]
-
+  implicit def format(using crypto: Encrypter & Decrypter): OFormat[SensitiveUIJourneySessionData] =
+    (
+      (__ \ "sessionId").format[String]
+        ~ (__ \ "journeyType").format[String]
+        ~ (__ \ "addIncomeSourceData").formatNullable[SensitiveAddIncomeSourceData]
+        ~ (__ \ "manageIncomeSourceData").formatNullable[ManageIncomeSourceData]
+        ~ (__ \ "ceaseIncomeSourceData").formatNullable[CeaseIncomeSourceData]
+        ~ (__ \ "optOutSessionData").formatNullable[OptOutSessionData]
+        ~ (__ \ "optInSessionData").formatNullable[OptInSessionData]
+        ~ (__ \ "incomeSourceReportingFrequencyData").formatNullable[IncomeSourceReportingFrequencySourceData]
+        ~ (__ \ "triggeredMigrationSessionData").formatNullable[TriggeredMigrationSessionData]
+        ~ (__ \ "lastUpdated").format(MongoJavatimeFormats.instantFormat)
+        ~ (__ \ "journeyIsComplete").formatNullable[Boolean]
+      ).apply(
+      (
+        sessionId,
+        journeyType,
+        addIncomeSourceData,
+        manageIncomeSourceData,
+        ceaseIncomeSourceData,
+        optOutSessionData,
+        optInSessionData,
+        incomeSourceReportingFrequencyData,
+        triggeredMigrationSessionData,
+        lastUpdated,
+        journeyIsComplete
+      ) =>
+        SensitiveUIJourneySessionData(
+          sessionId,
+          journeyType,
+          addIncomeSourceData,
+          manageIncomeSourceData,
+          ceaseIncomeSourceData,
+          optOutSessionData,
+          optInSessionData,
+          incomeSourceReportingFrequencyData,
+          triggeredMigrationSessionData,
+          lastUpdated,
+          journeyIsComplete
+        ),
+      s =>
+        (
+          s.sessionId,
+          s.journeyType,
+          s.addIncomeSourceData,
+          s.manageIncomeSourceData,
+          s.ceaseIncomeSourceData,
+          s.optOutSessionData,
+          s.optInSessionData,
+          s.incomeSourceReportingFrequencyData,
+          s.triggeredMigrationSessionData,
+          s.lastUpdated,
+          s.journeyIsComplete
+        )
+    )
 }
