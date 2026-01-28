98a22ae5073d2db456016f0cc40a8db13e64235d
taradhesi14 <233078744+taradhesi14@users.noreply.github.com>
2026-01-28 12:39:57 +0000

    Corrected spelling of oom to Room

--- it/test/testConstants/NewCalcBreakdownItTestConstants.scala
+++ it/test/testConstants/NewCalcBreakdownItTestConstants.scala
@@ -568,7 +568,7 @@ object NewCalcBreakdownItTestConstants {
     errors = Some(List(
       Message("C55012", "the update must align to the accounting period end date of 05/01/2023."),
       Message("C15507", "you’ve claimed £2000 in Property Income Allowance but this is more than turnover for your UK property."),
-      Message("C15510", "the Rent-a-oom relief claimed for a jointly let property cannot be more than 10% of the Rent-a-Room limit."),
+      Message("C15510", "the Rent-a-Room relief claimed for a jointly let property cannot be more than 10% of the Rent-a-Room limit."),
       Message("C55010", "updates cannot include overlaps.")
     ))
   )))
