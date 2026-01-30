957db68999c5a3b61454908e1a40146c7db883df
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2026-01-30 15:05:27 +0000

    Remapping uk dividends to totalUkDividends (#3230)

--- app/models/liabilitycalculation/Calculation.scala
+++ app/models/liabilitycalculation/Calculation.scala
@@ -54,7 +54,7 @@ object ChargeableEventGainsIncome {
   implicit val format: OFormat[ChargeableEventGainsIncome] = Json.format[ChargeableEventGainsIncome]
 }
 
-case class DividendsIncome(chargeableForeignDividends: Option[Int] = None)
+case class DividendsIncome(totalUkDividends: Option[Int] = None, chargeableForeignDividends: Option[Int] = None)
 
 object DividendsIncome {
   implicit val format: OFormat[DividendsIncome] = Json.format[DividendsIncome]
