4e9b425af67c09ccd165db06d80042e9219d0a22
Nemanja Tomanovic <nemanja.tomanovic@gmail.com>
2026-03-09 17:49:54 +0000

    Misuv 10929 (#3279)

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -320,9 +320,10 @@ class HomeController @Inject()(val homeView: views.html.HomeView,
         ctaViewModel <- whatYouOweService.claimToAdjustViewModel(Nino(user.nino))
         credits <- creditService.getAllCredits()
         unpaidCharges <- financialDetailsService.getAllUnpaidFinancialDetails()
+        chargeItem = getChargeList(unpaidCharges, isEnabled(FilterCodedOutPoas), isEnabled(PenaltiesAndAppeals))
       }
       yield {
-        Ok(newHomeOverviewView(origin, isAgent, user.isSupportingAgent, dateService.getCurrentTaxYear, yourTasksUrl(origin, isAgent), recentActivityUrl(origin, isAgent), overviewUrl(origin, isAgent), helpUrl(origin, isAgent), unpaidCharges.isEmpty, credits.availableCreditInAccount, ctaViewModel))
+        Ok(newHomeOverviewView(origin, isAgent, user.isSupportingAgent, dateService.getCurrentTaxYear, yourTasksUrl(origin, isAgent), recentActivityUrl(origin, isAgent), overviewUrl(origin, isAgent), helpUrl(origin, isAgent), unpaidCharges.isEmpty, credits.availableCreditInAccount, ctaViewModel, chargeItem))
       }
     }
   }
@@ -337,4 +338,17 @@ class HomeController @Inject()(val homeView: views.html.HomeView,
   def overviewUrl(origin: Option[String] = None, isAgent: Boolean): String = controllers.routes.HomeController.handleOverview(origin, isAgent).url
   def helpUrl(origin: Option[String] = None, isAgent: Boolean): String = controllers.routes.HomeController.handleHelp(origin, isAgent).url
 
+
+  private def getChargeList(unpaidCharges: List[FinancialDetailsResponseModel], isFilterOutCodedPoasEnabled: Boolean, penaltiesEnabled: Boolean): List[ChargeItem] = {
+
+    val chargesList =
+      unpaidCharges.collect {
+        case fdm: FinancialDetailsModel => fdm
+      }
+    whatYouOweService.getFilteredChargesList(
+      financialDetailsList = chargesList,
+      isFilterCodedOutPoasEnabled = isFilterOutCodedPoasEnabled,
+      isPenaltiesEnabled = penaltiesEnabled,
+      remainingToPayByChargeOrInterestWhenChargeIsPaidOrNot = mainChargeIsNotPaidFilter)
+  }
 }
