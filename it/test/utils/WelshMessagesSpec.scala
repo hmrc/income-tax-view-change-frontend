e853f9cc41a10f9e04bedd98e25cdf6fe7ba56c8
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2026-03-31 14:26:35 +0100

    Changed welsh messages file to only check local conf

--- it/test/utils/WelshMessagesSpec.scala
+++ it/test/utils/WelshMessagesSpec.scala
@@ -1,36 +1,43 @@
-/*
- * Copyright 2017 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package utils
 
 import helpers.ComponentSpecBase
+import play.api.Environment
+import play.api.i18n.MessagesApi
+
+import scala.io.Source
 
 class WelshMessagesSpec extends ComponentSpecBase {
 
-  val realMessages: Map[String, Map[String, String]] = messagesAPI.messages
-  val englishMessages: Map[String, String] = realMessages("default")
-  val welshMessages: Map[String, String] = realMessages("cy")
+  private val env: Environment = app.injector.instanceOf[Environment]
+  private val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
+
+  private def loadMessagesFile(path: String): Map[String, String] = {
+    env.resourceAsStream(path)
+      .map(Source.fromInputStream)
+      .map(_.getLines().toList)
+      .map { lines =>
+        lines
+          .filterNot(_.trim.startsWith("#"))
+          .filter(_.contains("="))
+          .map { line =>
+            val Array(key, value) = line.split("=", 2)
+            key.trim -> value.trim
+          }.toMap
+      }
+      .getOrElse(Map.empty)
+  }
+
+  private val englishMessages = loadMessagesFile("messages")
+  private val welshMessages   = loadMessagesFile("messages.cy")
 
   "all English messages should have a Welsh translation" in {
     val missingWelshKeys = englishMessages.keySet.diff(welshMessages.keySet)
 
     if (missingWelshKeys.nonEmpty) {
-      val failureText = missingWelshKeys.foldLeft(s"There are ${missingWelshKeys.size} missing Welsh translations:") {
-        case (failureString, key) =>
-          failureString + s"\n$key:${englishMessages(key)}"
+      val failureText = missingWelshKeys.foldLeft(
+        s"There are ${missingWelshKeys.size} missing Welsh translations:"
+      ) {
+        case (out, key) => out + s"\n$key: ${englishMessages(key)}"
       }
       fail(failureText)
     }
@@ -40,12 +47,12 @@ class WelshMessagesSpec extends ComponentSpecBase {
     val missingEnglishKeys = welshMessages.keySet.diff(englishMessages.keySet)
 
     if (missingEnglishKeys.nonEmpty) {
-      val failureText = missingEnglishKeys.foldLeft(s"There are ${missingEnglishKeys.size} missing English translations:") {
-        case (failureString, key) =>
-          failureString + s"\n$key:${welshMessages(key)}"
+      val failureText = missingEnglishKeys.foldLeft(
+        s"There are ${missingEnglishKeys.size} missing English translations:"
+      ) {
+        case (out, key) => out + s"\n$key: ${welshMessages(key)}"
       }
       fail(failureText)
     }
   }
 }
-
