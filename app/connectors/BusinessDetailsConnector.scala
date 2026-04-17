156d12ebe6a7c9b15bbeeed65740d5651e23ea86
antonytitley-digital <antony.titley@digital.hmrc.gov.uk>
2026-04-17 10:33:39 +0100

    Reapply "MIPR-2520"
    This reverts commit ba51a70c43a4041aab34eeaf8775d57792c0a54e.

--- app/connectors/BusinessDetailsConnector.scala
+++ app/connectors/BusinessDetailsConnector.scala
@@ -40,15 +40,15 @@ class BusinessDetailsConnector @Inject()(
                                         )(implicit val ec: ExecutionContext) {
 
   private[connectors] def getBusinessDetailsUrl(nino: String): String = {
-    s"${appConfig.itvcProtectedService}/income-tax-view-change/get-business-details/nino/$nino"
+    s"${appConfig.incomeTaxBusinessDetailsBaseUrl}/income-tax-business-details/get-business-details/nino/$nino"
   }
 
   private[connectors] def getIncomeSourcesUrl(mtditid: String): String = {
-    s"${appConfig.itvcProtectedService}/income-tax-view-change/income-sources/$mtditid"
+    s"${appConfig.incomeTaxBusinessDetailsBaseUrl}/income-tax-business-details/income-sources/$mtditid"
   }
 
   private[connectors] def getNinoLookupUrl(mtdRef: String): String = {
-    s"${appConfig.itvcProtectedService}/income-tax-view-change/nino-lookup/$mtdRef"
+    s"${appConfig.incomeTaxBusinessDetailsBaseUrl}/income-tax-business-details/nino-lookup/$mtdRef"
   }
 
   def getBusinessDetails(nino: String)(implicit headerCarrier: HeaderCarrier): Future[IncomeSourceDetailsResponse] = {
