e62099f9de8690b52278223a49de8bb34480d6e9
antonytitley-digital <antony.titley@digital.hmrc.gov.uk>
2026-04-22 14:36:17 +0100

    fix: removed unused imports

--- test/businessDetails/controllers/manageBusinesses/add/AddBusinessTradeControllerSpec.scala
+++ test/businessDetails/controllers/manageBusinesses/add/AddBusinessTradeControllerSpec.scala
@@ -19,7 +19,7 @@ package businessDetails.controllers.manageBusinesses.add
 import businessDetails.controllers.manageBusinesses.add.AddBusinessTradeController
 import connectors.{BusinessDetailsConnector, ITSAStatusConnector}
 import enums.IncomeSourceJourney.SelfEmployment
-import enums.JourneyType.{Add, IncomeSourceJourneyType, JourneyType, Manage}
+import enums.JourneyType.{Add, IncomeSourceJourneyType, JourneyType}
 import enums.{MTDIndividual, MTDSupportingAgent, MTDUserRole}
 import forms.manageBusinesses.add.BusinessTradeForm
 import mocks.auth.MockAuthActions
@@ -34,7 +34,7 @@ import play.api.http.Status
 import play.api.mvc.{Action, AnyContent, Result}
 import play.api.test.Helpers.*
 import services.{DateServiceInterface, SessionService}
-import testConstants.BusinessDetailsTestConstants.{address, business1}
+import testConstants.BusinessDetailsTestConstants.business1
 import testConstants.incomeSources.IncomeSourceDetailsTestConstants.*
 
 import scala.concurrent.Future
