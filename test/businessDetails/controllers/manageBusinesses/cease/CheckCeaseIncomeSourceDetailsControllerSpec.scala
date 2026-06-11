1498b125e237835d69303bf584d21637c3ca0700
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2026-06-11 15:44:27 +0100

    MIPR-2622-h moving code to correct folders

--- test/businessDetails/controllers/manageBusinesses/cease/CheckCeaseIncomeSourceDetailsControllerSpec.scala
+++ test/businessDetails/controllers/manageBusinesses/cease/CheckCeaseIncomeSourceDetailsControllerSpec.scala
@@ -18,7 +18,7 @@ package businessDetails.controllers.manageBusinesses.cease
 
 import businessDetails.controllers.manageBusinesses.cease.routes as ceaseBusinessRoutes
 import businessDetails.controllers.triggeredMigration.routes as triggeredMigrationRoutes
-import businessDetails.services.{IncomeSourceDetailsService, UpdateIncomeSourceService, UpdateIncomeSourceSuccess}
+import businessDetails.services.{IncomeSourceDetailsService, UpdateIncomeSourceService, UpdateIncomeSourceSuccess, SessionService}
 import common.connectors.ITSAStatusConnector
 import common.enums.IncomeSourceJourney.{ForeignProperty, IncomeSourceType, SelfEmployment, UkProperty}
 import common.enums.JourneyType.{Cease, IncomeSourceJourneyType}
@@ -27,7 +27,7 @@ import common.enums.{MTDIndividual, MTDSupportingAgent}
 import common.mocks.auth.MockAuthActions
 import common.mocks.services.MockSessionService
 import common.models.core.IncomeSourceId
-import common.services.{DateServiceInterface, SessionService}
+import common.services.{DateServiceInterface}
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.mockito.ArgumentMatchers.any
