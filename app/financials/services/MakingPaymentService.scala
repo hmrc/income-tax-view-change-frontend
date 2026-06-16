6e5a26940aa4472ce94a7e2ddcc612d94978cbed
BAmiriDev <126995732+BAmiriDev@users.noreply.github.com>
2026-06-16 07:05:18 +0100

    MISUV-11499 Add making payment service

---
+++ app/financials/services/MakingPaymentService.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package financials.services
+
+import common.auth.MtdItUser
+import common.services.DateServiceInterface
+import models.financialDetails.{DocumentDetail, FinancialDetailsErrorModel, FinancialDetailsModel, MakingPaymentViewModel}
+import uk.gov.hmrc.http.HeaderCarrier
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class MakingPaymentService @Inject()(financialDetailsService: FinancialDetailsService,
+                                     implicit val dateService: DateServiceInterface) {
+
+  def createViewModel(backUrl: String,
+                      paymentHandoffUrl: String,
+                      whatYouOweUrl: String,
+                      moneyInYourAccountUrl: String,
+                      payPenaltyUrl: String)
+                     (implicit user: MtdItUser[_],
+                      hc: HeaderCarrier,
+                      ec: ExecutionContext): Future[Option[MakingPaymentViewModel]] = {
+    financialDetailsService.getAllFinancialDetails.map { financialDetails =>
+      if (financialDetails.exists(_._2.isInstanceOf[FinancialDetailsErrorModel])) {
+        None
+      } else {
+        val financialDetailsModels = financialDetails.collect { case (_, model: FinancialDetailsModel) => model }
+        Some(MakingPaymentViewModel(
+          backUrl = backUrl,
+          paymentHandoffUrl = paymentHandoffUrl,
+          whatYouOweUrl = whatYouOweUrl,
+          moneyInYourAccountUrl = moneyInYourAccountUrl,
+          payPenaltyUrl = payPenaltyUrl,
+          hasInterest = financialDetailsModels.flatMap(_.documentDetails).exists(hasInterest),
+          hasPenalty = financialDetailsModels.flatMap(_.toChargeItem).exists(charge => charge.isPenalty && charge.remainingToPayByChargeOrInterest > 0),
+          unallocatedCredit = financialDetailsModels.flatMap(_.balanceDetails.unallocatedCredit).find(_ > 0)
+        ))
+      }
+    }
+  }
+
+  private def hasInterest(documentDetail: DocumentDetail): Boolean =
+    documentDetail.accruingInterestAmount.exists(_ > 0) ||
+      documentDetail.interestOutstandingAmount.exists(_ > 0) ||
+      documentDetail.latePaymentInterestAmount.exists(_ > 0)
+}
