4386328071e0707e77dd9ea60c143cefd17993f1
Tom Burt-Gray <72973046+tbg2003@users.noreply.github.com>
2026-06-22 13:05:09 +0100

    MISUV-11505: Updates IncomeSummaryTotals to include 'totalUKOtherProp… (#3502)
    * MISUV-11505: Updates IncomeSummaryTotals and IncomeBreakdownViewModel to include 'totalUKOtherPropertyProfit' field

--- test/models/liabilitycalculation/viewmodels/IncomeBreakdownViewModelSpec.scala
+++ test/models/liabilitycalculation/viewmodels/IncomeBreakdownViewModelSpec.scala
@@ -26,7 +26,7 @@ class IncomeBreakdownViewModelSpec extends UnitSpec {
   "IncomeBreakdownViewModel model" when {
     "create a minimal IncomeBreakdownViewModel when there is a minimal Calculation" in {
       IncomeBreakdownViewModel(Some(Calculation())) shouldBe Some(
-        IncomeBreakdownViewModel(None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None))
+        IncomeBreakdownViewModel(None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None))
     }
 
     "create a empty IncomeBreakdownViewModel when there is a no Calculation" in {
@@ -42,6 +42,7 @@ class IncomeBreakdownViewModelSpec extends UnitSpec {
           totalEmploymentExpenses = Some(5000.99),
           totalSelfEmploymentProfit = Some(12500),
           totalPropertyProfit = Some(12500),
+          totalUKOtherPropertyProfit = Some(12500),
           totalFHLPropertyProfit = Some(12500),
           totalForeignPropertyProfit = Some(12500),
           totalEeaFhlProfit = Some(12500),
