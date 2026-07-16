71aa134b359ef12efe86eb4c92b647e3e0fb403b
Deng Nyuar <60218795+dnyuar@users.noreply.github.com>
2026-07-16 11:22:21 +0100

    MISUV-11653 Re-enable commented-out MoneyInYourAccountController tests (#3564)
    * Uncommented and updated MoneyInYourAccount ISpec tests
    
    * uncommenting MIYA unit tests, updated and now passing

--- test/financials/controllers/MoneyInYourAccountControllerSpec.scala
+++ test/financials/controllers/MoneyInYourAccountControllerSpec.scala
@@ -93,78 +93,82 @@ class MoneyInYourAccountControllerSpec extends MockAuthActions with MockCreditSe
               status(result) shouldBe Status.OK
               title shouldBe "Money in your account - Manage your Self Assessment - GOV.UK"
             }
-  //TODO: Re-enable these tests as part of MISUV-10631
-//            "credit charges are returned" in {
-//
-//              setupMockSuccess(mtdUserRole, false, List(CreditsRefundsRepay))
-//              mockItsaStatusRetrievalAction()
-//              mockSingleBISWithCurrentYearAsMigrationYear()
-//
-//              when(mockCreditService.getAllCredits(any(), any())).thenReturn(Future.successful(
-//                ANewCreditAndRefundModel()
-//                  .withBalancingChargeCredit(LocalDate.parse("2022-08-16"), 100.0)
-//                  .get()))
-//
-//              val result = action(fakeRequest)
-//              status(result) shouldBe Status.OK
-//            }
-//
-//            "credit charges are returned in sorted order of credits" in {
-//
-//              setupMockSuccess(mtdUserRole, false, List(CreditsRefundsRepay))
-//              mockItsaStatusRetrievalAction()
-//              mockSingleBISWithCurrentYearAsMigrationYear()
-//
-//              when(mockCreditService.getAllCredits(any(), any())).thenReturn(Future.successful(
-//                ANewCreditAndRefundModel()
-//                  .withBalancingChargeCredit(LocalDate.of(2019, 5, 15), 250.0)
-//                  .withBalancingChargeCredit(LocalDate.of(2019, 5, 15), 125.0)
-//                  .withPayment(LocalDate.parse("2022-08-16"), 100.0)
-//                  .withPayment(LocalDate.parse("2022-08-16"), 500.0)
-//                  .withPayment(LocalDate.parse("2022-08-16"), 300.0)
-//                  .withMfaCredit(LocalDate.of(2019, 5, 15), 100.0)
-//                  .withMfaCredit(LocalDate.of(2019, 5, 15), 1000.0)
-//                  .withMfaCredit(LocalDate.of(2019, 5, 15), 800.0)
-//                  .withCutoverCredit(LocalDate.of(2019, 5, 15), 200.0)
-//                  .withCutoverCredit(LocalDate.of(2019, 5, 15), 2000.0)
-//                  .withCutoverCredit(LocalDate.of(2019, 5, 15), 700.0)
-//                  .withFirstRefund(4.0)
-//                  .withSecondRefund(2.0)
-//                  .get()))
-//
-//              val result = action(fakeRequest)
-//              status(result) shouldBe Status.OK
-//
-//              val doc: Document = Jsoup.parse(contentAsString(result))
-//              doc.select("#main-content").select("li:nth-child(1)")
-//                .select("p").first().text().contains(messages("credit-and-refund.payment") + " 15 June 2018")
-//              doc.select("#main-content").select("li:nth-child(2)")
-//                .select("p").first().text().contains(messages("credit-and-refund.payment") + " 15 June 2018")
-//              doc.select("#main-content").select("li:nth-child(3)")
-//                .select("p").first().text().contains(messages("credit-and-refund.payment") + " 15 June 2018")
-//              doc.select("#main-content").select("li:nth-child(4)")
-//                .select("p").first().text().contains(messages("credit-and-refund.credit-from-earlier-tax-year") + " " + s"$testTaxYearTo")
-//              doc.select("#main-content").select("li:nth-child(5)")
-//                .select("p").first().text().contains(messages("credit-and-refund.credit-from-adjustment-prt-1") + " " + s"$testTaxYearTo")
-//              doc.select("#main-content").select("li:nth-child(6)")
-//                .select("p").first().text().contains(messages("credit-and-refund.credit-from-earlier-tax-year") + " " + s"$testTaxYearTo")
-//              doc.select("#main-content").select("li:nth-child(7)")
-//                .select("p").first().text().contains(messages("credit-and-refund.credit-from-earlier-tax-year") + " " + s"$testTaxYearTo")
-//              doc.select("#main-content").select("li:nth-child(8)")
-//                .select("p").first().text().contains(messages("credit-and-refund.credit-from-adjustment-prt-1") + " " + s"$testTaxYearTo")
-//              doc.select("#main-content").select("li:nth-child(9)")
-//                .select("p").first().text().contains(messages("credit-and-refund.credit-from-balancing-charge-prt-1") + " " +
-//                  messages("credit-and-refund.credit-from-balancing-charge-prt-2") + s"$testTaxYearTo")
-//              doc.select("#main-content").select("li:nth-child(10)")
-//                .select("p").first().text().contains(messages("credit-and-refund.credit-from-balancing-charge-prt-1") + " " +
-//                  messages("credit-and-refund.credit-from-balancing-charge-prt-2") + s"$testTaxYearTo")
-//              doc.select("#main-content").select("li:nth-child(11)")
-//                .select("p").first().text().contains(messages("credit-and-refund.credit-from-adjustment-prt-1") + " " + s"$testTaxYearTo")
-//              doc.select("#main-content").select("li:nth-child(12)")
-//                .select("p").first().text() shouldBe "£4.00 " + messages("credit-and-refund.refundProgress-prt-2")
-//              doc.select("#main-content").select("li:nth-child(13)")
-//                .select("p").first().text() shouldBe "£2.00 " + messages("credit-and-refund.refundProgress-prt-2")
-//            }
+
+            "credit charges are returned" in {
+
+              setupMockSuccess(mtdUserRole, false, List(CreditsRefundsRepay))
+              mockItsaStatusRetrievalAction()
+              mockSingleBISWithCurrentYearAsMigrationYear()
+
+              when(mockCreditService.getAllCredits(any(), any())).thenReturn(Future.successful(
+                ANewCreditAndRefundModel()
+                  .withBalancingChargeCredit(LocalDate.parse("2022-08-16"), 100.0)
+                  .get()))
+
+              val result = action(fakeRequest)
+              status(result) shouldBe Status.OK
+            }
+
+            "credit charges are returned in sorted order of credits" in {
+
+              setupMockSuccess(mtdUserRole, false, List(CreditsRefundsRepay))
+              mockItsaStatusRetrievalAction()
+              mockSingleBISWithCurrentYearAsMigrationYear()
+
+              val testPreviousTaxYear = 2019
+              val testTaxYear = 2020
+              val testTaxYear2023 = 2023
+
+              when(mockCreditService.getAllCredits(any(), any())).thenReturn(Future.successful(
+                ANewCreditAndRefundModel()
+                  .withAvailableCredit(5.0)
+                  .withTotalCredit(5.0)
+                  .withBalancingChargeCredit(LocalDate.of(2019, 5, 15), 250.0)
+                  .withBalancingChargeCredit(LocalDate.of(2019, 5, 15), 125.0)
+                  .withPayment(LocalDate.parse("2022-08-16"), 100.0, Some(LocalDate.parse("2021-06-15")))
+                  .withPayment(LocalDate.parse("2022-08-16"), 500.0, Some(LocalDate.parse("2021-06-15")))
+                  .withPayment(LocalDate.parse("2022-08-16"), 300.0, Some(LocalDate.parse("2021-06-15")))
+                  .withMfaCredit(LocalDate.of(2019, 5, 15), 100.0)
+                  .withMfaCredit(LocalDate.of(2019, 5, 15), 1000.0)
+                  .withMfaCredit(LocalDate.of(2019, 5, 15), 800.0)
+                  .withCutoverCredit(LocalDate.of(2019, 5, 15), 200.0)
+                  .withCutoverCredit(LocalDate.of(2019, 5, 15), 2000.0)
+                  .withCutoverCredit(LocalDate.of(2019, 5, 15), 700.0)
+                  .withFirstRefund(4.0)
+                  .withSecondRefund(2.0)
+                  .get()))
+
+              val result = action(fakeRequest)
+              status(result) shouldBe Status.OK
+
+              val doc: Document = Jsoup.parse(contentAsString(result))
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(1)")
+                .text() shouldBe s"15 May $testPreviousTaxYear " + messages("money-in-your-account.where-from.credit-row.description") + s" $testPreviousTaxYear to $testTaxYear " + "£250.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(2)")
+                .text() shouldBe s"15 May $testPreviousTaxYear " + messages("money-in-your-account.where-from.credit-row.description") + s" $testPreviousTaxYear to $testTaxYear " + "£125.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(3)")
+                .text() shouldBe s"15 May $testPreviousTaxYear " + messages("money-in-your-account.where-from.credit-row.description") + s" $testPreviousTaxYear to $testTaxYear " + "£100.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(4)")
+                .text() shouldBe s"15 May $testPreviousTaxYear " + messages("money-in-your-account.where-from.credit-row.description") + s" $testPreviousTaxYear to $testTaxYear " + "£1,000.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(5)")
+                .text() shouldBe s"15 May $testPreviousTaxYear " + messages("money-in-your-account.where-from.credit-row.description") + s" $testPreviousTaxYear to $testTaxYear " + "£800.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(6)")
+                .text() shouldBe s"15 May $testPreviousTaxYear " + messages("money-in-your-account.where-from.credit-row.description") + s" $testPreviousTaxYear to $testTaxYear " + "£200.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(7)")
+                .text() shouldBe s"15 May $testPreviousTaxYear " + messages("money-in-your-account.where-from.credit-row.description") + s" $testPreviousTaxYear to $testTaxYear " + "£2,000.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(8)")
+                .text() shouldBe s"15 May $testPreviousTaxYear " + messages("money-in-your-account.where-from.credit-row.description") + s" $testPreviousTaxYear to $testTaxYear " + "£700.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(9)")
+                .text() shouldBe s"16 Aug ${testTaxYear2023 - 1} " + messages("money-in-your-account.where-from.payment-row.description", "15 Jun 2021") + s" ${testTaxYear2023 - 1} to $testTaxYear2023 " + "£100.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(10)")
+                .text() shouldBe s"16 Aug ${testTaxYear2023 - 1} " + messages("money-in-your-account.where-from.payment-row.description", "15 Jun 2021") + s" ${testTaxYear2023 - 1} to $testTaxYear2023 " + "£500.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(11)")
+                .text() shouldBe s"16 Aug ${testTaxYear2023 - 1} " + messages("money-in-your-account.where-from.payment-row.description", "15 Jun 2021") + s" ${testTaxYear2023 - 1} to $testTaxYear2023 " + "£300.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(12)")
+                .text() shouldBe messages("chargeSummary.noData") + " " + messages("money-in-your-account.where-from.refund-row.description") + " " + messages("chargeSummary.noData") + " −£4.00"
+              doc.select("#main-content").select("#where-the-money-came-from-table tbody tr:nth-child(13)")
+                .text() shouldBe messages("chargeSummary.noData") + " " + messages("money-in-your-account.where-from.refund-row.description") + " " + messages("chargeSummary.noData") + " −£2.00"
+            }
           }
 
           "render the custom not found error page" when {
