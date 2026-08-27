f6d51e513d9e652f1ccd3f6a5d165858465cd61a
tbg2003 <72973046+tbg2003@users.noreply.github.com>
2026-08-27 12:13:14 +0100

    simplify SensitiveUIJourneySessionData implicit format

--- app/businessDetails/models/SensitiveUIJourneySessionData.scala
+++ app/businessDetails/models/SensitiveUIJourneySessionData.scala
@@ -20,13 +20,13 @@ import businessDetails.models.incomeSourceDetails.{CeaseIncomeSourceData, Income
 import businessDetails.models.triggeredMigration.TriggeredMigrationSessionData
 import obligations.models.reportingObligations.optOut.OptOutSessionData
 import obligations.models.reportingObligations.signUp.SignUpSessionData
-import play.api.libs.functional.syntax.toFunctionalBuilderOps
 import play.api.libs.json.*
 import shared.models.UIJourneySessionData
 import uk.gov.hmrc.crypto.{Decrypter, Encrypter}
 import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats
 
 import java.time.Instant
+import scala.language.implicitConversions
 
 case class SensitiveUIJourneySessionData(
                                           sessionId: String,
@@ -41,7 +41,7 @@ case class SensitiveUIJourneySessionData(
                                           lastUpdated: Instant = Instant.now,
                                           journeyIsComplete: Option[Boolean] = None
                                         ) {
-  
+
   def this(uiJourneySessionData: UIJourneySessionData) = {
     this(
       uiJourneySessionData.sessionId,
@@ -77,58 +77,8 @@ case class SensitiveUIJourneySessionData(
 object SensitiveUIJourneySessionData {
 
   implicit def format(using crypto: Encrypter & Decrypter): OFormat[SensitiveUIJourneySessionData] =
-    (
-      (__ \ "sessionId").format[String]
-        ~ (__ \ "journeyType").format[String]
-        ~ (__ \ "addIncomeSourceData").formatNullable[SensitiveAddIncomeSourceData]
-        ~ (__ \ "manageIncomeSourceData").formatNullable[ManageIncomeSourceData]
-        ~ (__ \ "ceaseIncomeSourceData").formatNullable[CeaseIncomeSourceData]
-        ~ (__ \ "optOutSessionData").formatNullable[OptOutSessionData]
-        ~ (__ \ "signUpSessionData").formatNullable[SignUpSessionData]
-        ~ (__ \ "incomeSourceReportingFrequencyData").formatNullable[IncomeSourceReportingFrequencySourceData]
-        ~ (__ \ "triggeredMigrationData").formatNullable[TriggeredMigrationSessionData]
-        ~ (__ \ "lastUpdated").format(MongoJavatimeFormats.instantFormat)
-        ~ (__ \ "journeyIsComplete").formatNullable[Boolean]
-      ).apply(
-      (
-        sessionId,
-        journeyType,
-        addIncomeSourceData,
-        manageIncomeSourceData,
-        ceaseIncomeSourceData,
-        optOutSessionData,
-        signUpSessionData,
-        incomeSourceReportingFrequencyData,
-        triggeredMigrationData,
-        lastUpdated,
-        journeyIsComplete
-      ) =>
-        SensitiveUIJourneySessionData(
-          sessionId,
-          journeyType,
-          addIncomeSourceData,
-          manageIncomeSourceData,
-          ceaseIncomeSourceData,
-          optOutSessionData,
-          signUpSessionData,
-          incomeSourceReportingFrequencyData,
-          triggeredMigrationData,
-          lastUpdated,
-          journeyIsComplete
-        ),
-      s =>
-        (
-          s.sessionId,
-          s.journeyType,
-          s.addIncomeSourceData,
-          s.manageIncomeSourceData,
-          s.ceaseIncomeSourceData,
-          s.optOutSessionData,
-          s.signUpSessionData,
-          s.incomeSourceReportingFrequencyData,
-          s.triggeredMigrationData,
-          s.lastUpdated,
-          s.journeyIsComplete
-        )
-    )
+    {
+      implicit val mongoInstantFormat: Format[Instant] = MongoJavatimeFormats.instantFormat
+      Json.format[SensitiveUIJourneySessionData]
+    }
 }
